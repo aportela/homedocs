c0c48729985fd6d0d7a3eea67bb2630d99090002
alex <766f6964+github@gmail.com>
2019-08-11 17:17:30 +0200

    struct of main tables changed

--- src/Database/Version.php
+++ src/Database/Version.php
@@ -75,6 +75,69 @@
                             PRIMARY KEY (`document_id`, `tag`)
                         )
                     '
+                ),
+                "1.03" => array(
+                    '
+                        CREATE INDEX idx_user_email ON USER (email);
+                    ',
+                    '
+                        DROP TABLE IF EXISTS `DOCUMENT`;
+                    ',
+                    '
+                        CREATE TABLE `DOCUMENT` (
+                            `id` VARCHAR(36) NOT NULL,
+                            `title` VARCHAR(128) NOT NULL,
+                            `description` VARCHAR(4096) NULL,
+                            `created_by_user_id` VARCHAR(36) NOT NULL,
+                            `created_on_timestamp` INTEGER NOT NULL,
+                            PRIMARY KEY (`id`),
+                            FOREIGN KEY("created_by_user_id") REFERENCES "USER"("id")
+                        );
+                    ',
+                    '
+                        CREATE INDEX idx_document_title ON DOCUMENT (title);
+                    ',
+                    '
+                        CREATE INDEX idx_document_description ON DOCUMENT (description);
+                    ',
+                    '
+                        CREATE INDEX idx_document_creator ON DOCUMENT (created_by_user_id);
+                    ',
+                    '
+                        CREATE INDEX idx_document_created_on ON DOCUMENT (created_on_timestamp);
+                    ',
+                    '
+                        DROP TABLE IF EXISTS `FILE`;
+                    ',
+                    '
+                        CREATE TABLE `FILE` (
+                            `id` VARCHAR(36) NOT NULL,
+                            `sha1_hash` VARCHAR(40) NOT NULL,
+                            `name` VARCHAR(256) NOT NULL,
+                            `size` INTEGER NOT NULL,
+                            `uploaded_by_user_id` VARCHAR(36) NOT NULL,
+                            `uploaded_on_timestamp` INTEGER NOT NULL,
+                            PRIMARY KEY (`id`)
+                        );
+                    ',
+                    '
+                        CREATE INDEX idx_file_hash ON FILE (sha1_hash);
+                    ',
+                    '
+                        CREATE INDEX idx_file_name ON FILE (name);
+                    ',
+                    '
+                        CREATE INDEX idx_file_size ON FILE (size);
+                    ',
+                    '
+                        CREATE INDEX idx_file_uploader ON FILE (uploaded_by_user_id);
+                    ',
+                    '
+                        CREATE INDEX idx_file_upload_on ON FILE (uploaded_on_timestamp);
+                    ',
+                    '
+                        CREATE INDEX idx_tag_name ON DOCUMENT_TAG (tag);
+                    ',
                 )
             )
         );
