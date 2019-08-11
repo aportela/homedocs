aac6452b761ef16a609df0c20cd4a5fa0717bfb8
alex <766f6964+github@gmail.com>
2019-08-11 17:16:11 +0200

    new method for parsing unit file-length's

--- public/scripts/modules/utils.js
+++ public/scripts/modules/utils.js
@@ -6,3 +6,26 @@
 export function uuid() {
     return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
 }
+
+/**
+ * get human file size
+ *
+ * (mpen) https://stackoverflow.com/a/14919494
+ * @param {*} bytes
+ * @param {*} si
+ */
+export function humanFileSize(bytes, si) {
+    var thresh = si ? 1000 : 1024;
+    if(Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+    var units = si
+        ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+        : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+    var u = -1;
+    do {
+        bytes /= thresh;
+        ++u;
+    } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1)+' '+units[u];
+}
