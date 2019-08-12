1a5b4c3c5b63590703c5d7856af99731de6f1cff
alex <766f6964+github@gmail.com>
2019-08-12 21:39:46 +0200

    fixed style class name

--- public/scripts/modules/control-table-header-sortable.js
+++ public/scripts/modules/control-table-header-sortable.js
@@ -1,5 +1,5 @@
 const template = `
-    <th class="f-cursor-pointer" v-on:click.prevent="$emit('sortClicked')">
+    <th class="cursor-pointer" v-on:click.prevent="$emit('sortClicked')">
         <i v-if="isSorted" class="fas" v-bind:class="{ 'fa-sort-alpha-up': sortOrder == 'ASC',  'fa-sort-alpha-down': sortOrder == 'DESC' }"></i>
         {{ name }}
     </th>
