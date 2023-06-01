1b0598c383fc90e1c941fb3fe8178befcfc9be57
alex <766f6964+github@gmail.com>
2023-06-01 11:51:48 +0200

    fixed end tag & readonly prop used for index

--- src-frontend/scripts/vue-components/modal-document-file-preview.js
+++ src-frontend/scripts/vue-components/modal-document-file-preview.js
@@ -1,39 +1,38 @@
 const template = `
-    <div class="modal is-active" v-if="previewIndex >= 0">
+    <div class="modal is-active" v-if="index >= 0">
         <div class="modal-background"></div>
-            <div class="modal-card">
-                <header class="modal-card-head has-text-centered">
-                    <p class="modal-card-title">{{ fileName }} ({{ $utils.humanFileSize(fileSize, true) }})</span></p>
-                    <button class="delete" aria-label="close" v-on:click.prevent="onClose"></button>
-                </header>
-                <section class="modal-card-body" v-if="hasPreviewSupport">
-                    <div class="notification" v-if="error">
-                        <h1 class="title"><i class="fas fa-exclamation-triangle"></i> Error loading preview</h1>
-                        <h2 class="sub-title">URL: <strong>{{ 'api2/file/' + fileId }}</strong></h2>
-                    </div>
-                    <p class="image" v-else>
-                        <img v-bind:src="'api2/file/' + fileId" alt="image preview" v-on:error="error = true;">
-                    </p>
-                </section>
-                <section class="modal-card-body" v-else>
-                    <p class="has-text-centered">Preview not available</p>
-                </section>
-                <footer class="modal-card-foot">
-                    <button class="button" v-on:click.prevent="onPrevious" v-bind:disabled="isPreviousButtonDisabled">
-                        <span class="icon is-small">
-                            <i class="fas fa-caret-left"></i>
-                        </span>
-                        <span>Previous</span>
-                    </button>
-                    <button class="button is-static">File {{ previewIndex + 1 }} of {{ totalFiles }}</button>
-                    <button class="button" v-on:click.prevent="onNext" v-bind:disabled="isNextButtonDisabled">
-                        <span class="icon is-small">
-                            <i class="fas fa-caret-right"></i>
-                        </span>
-                        <span>Next</span>
-                    </button>
-                </footer>
-            </div>
+        <div class="modal-card">
+            <header class="modal-card-head has-text-centered">
+                <p class="modal-card-title">{{ fileName }} ({{ $utils.humanFileSize(fileSize, true) }})</p>
+                <button class="delete" aria-label="close" v-on:click.prevent="onClose"></button>
+            </header>
+            <section class="modal-card-body" v-if="hasPreviewSupport">
+                <div class="notification" v-if="error">
+                    <h1 class="title"><i class="fas fa-exclamation-triangle"></i> Error loading preview</h1>
+                    <h2 class="sub-title">URL: <strong>{{ 'api2/file/' + fileId }}</strong></h2>
+                </div>
+                <p class="image" v-else>
+                    <img v-bind:src="'api2/file/' + fileId" alt="image preview" v-on:error="error = true;">
+                </p>
+            </section>
+            <section class="modal-card-body" v-else>
+                <p class="has-text-centered">Preview not available</p>
+            </section>
+            <footer class="modal-card-foot">
+                <button class="button" v-on:click.prevent="onPrevious" v-bind:disabled="isPreviousButtonDisabled">
+                    <span class="icon is-small">
+                        <i class="fas fa-caret-left"></i>
+                    </span>
+                    <span>Previous</span>
+                </button>
+                <button class="button is-static">File {{ index + 1 }} of {{ totalFiles }}</button>
+                <button class="button" v-on:click.prevent="onNext" v-bind:disabled="isNextButtonDisabled">
+                    <span class="icon is-small">
+                        <i class="fas fa-caret-right"></i>
+                    </span>
+                    <span>Next</span>
+                </button>
+            </footer>
         </div>
         <button class="modal-close is-large" aria-label="close" v-on:click.prevent="onClose"></button>
     </div>
@@ -44,7 +43,8 @@ export default {
     template: template,
     data: function () {
         return ({
-            error: false
+            error: false,
+            index: 0
         });
     },
     props: [
@@ -52,39 +52,40 @@ export default {
     ],
     created: function () {
         window.addEventListener('keydown', this.onKeyPress);
+        this.index = this.previewIndex;
     },
     computed: {
         totalFiles: function () {
             return (this.files.length);
         },
         fileId: function () {
-            return (this.files[this.previewIndex].id);
+            return (this.files[this.index].id);
         },
         fileName: function () {
-            return (this.files[this.previewIndex].name);
+            return (this.files[this.index].name);
         },
         fileSize: function () {
-            return (this.files[this.previewIndex].size);
+            return (this.files[this.index].size);
         },
         isPreviousButtonDisabled: function () {
-            return (this.previewIndex < 1);
+            return (this.index < 1);
         },
         isNextButtonDisabled: function () {
-            return (this.previewIndex >= this.files.length - 1);
+            return (this.index >= this.files.length - 1);
         },
         hasPreviewSupport: function () {
-            return (this.$utils.isImage(this.files[this.previewIndex].name));
+            return (this.$utils.isImage(this.files[this.index].name));
         }
     },
     methods: {
         onPrevious: function () {
-            if (this.previewIndex > 0) {
-                this.previewIndex--;
+            if (this.index > 0) {
+                this.index--;
             }
         },
         onNext: function () {
-            if (this.previewIndex < this.files.length - 1) {
-                this.previewIndex++;
+            if (this.index < this.files.length - 1) {
+                this.index++;
             }
         },
         onClose: function () {
