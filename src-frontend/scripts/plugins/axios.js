1e478c760ca64b5eaace0de07d0aeafac7db814e
alex <766f6964+github@gmail.com>
2023-06-09 11:50:26 +0200

    new axios interceptor for fatal api errors

--- src-frontend/scripts/plugins/axios.js
+++ src-frontend/scripts/plugins/axios.js
@@ -1,34 +1,50 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    install: (app, options) => {
-        let axiosInstance = axios.create(options);
-        const jwt = app.config.globalProperties.$localStorage.get('jwt');
-        axiosInstance.interceptors.request.use((config) => {
-            if (jwt) {
-                config.headers["HOMEDOCS-JWT"] = jwt;
-                config.withCredentials = true;
-            }
-            return (config);
-        }, (error) => {
-            return Promise.reject(error);
-        });
-        axiosInstance.interceptors.response.use((response) => {
-            // warning: axios lowercase received header names
-            const apiResponseJWT = response.headers["homedocs-jwt"] || null;
-            if (apiResponseJWT) {
-                if (apiResponseJWT && apiResponseJWT != jwt) {
-                    app.config.globalProperties.$localStorage.set("jwt", apiResponseJWT);
-                }
-            }
-            return response;
-        }, (error) => {
-            // helper for checking invalid fields on api response
-            error.isFieldInvalid = function (fieldName) {
-                return (error.response.data.invalidOrMissingParams.indexOf(fieldName) > -1);
-            }
-            return Promise.reject(error);
-        });
-        app.config.globalProperties.$axios = axiosInstance;
-    }
-}
+  install: (app, options) => {
+    let axiosInstance = axios.create(options);
+    const jwt = app.config.globalProperties.$localStorage.get("jwt");
+    axiosInstance.interceptors.request.use(
+      (config) => {
+        if (jwt) {
+          config.headers["HOMEDOCS-JWT"] = jwt;
+          config.withCredentials = true;
+        }
+        return config;
+      },
+      (error) => {
+        return Promise.reject(error);
+      }
+    );
+    axiosInstance.interceptors.response.use(
+      (response) => {
+        // warning: axios lowercase received header names
+        const apiResponseJWT = response.headers["homedocs-jwt"] || null;
+        if (apiResponseJWT) {
+          if (apiResponseJWT && apiResponseJWT != jwt) {
+            app.config.globalProperties.$localStorage.set(
+              "jwt",
+              apiResponseJWT
+            );
+          }
+        }
+        return response;
+      },
+      (error) => {
+        // helper for checking invalid fields on api response
+        error.isFieldInvalid = function (fieldName) {
+          return (
+            error.response.data.invalidOrMissingParams.indexOf(fieldName) > -1
+          );
+        };
+
+        error.response.getApiErrorData = function () {
+          return JSON.stringify(error.response, null, "\t");
+        };
+
+        return Promise.reject(error);
+      }
+    );
+    app.config.globalProperties.$axios = axiosInstance;
+  },
+};
