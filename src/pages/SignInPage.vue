ce93b242d9dacf107a68508a9cb1a4b6eb5727ef
alex <766f6964+github@gmail.com>
2023-06-16 11:52:57 +0200

    added autofocus/disabled/loading state, fixed validation rules

--- src/pages/SignInPage.vue
+++ src/pages/SignInPage.vue
@@ -1,14 +1,8 @@
 <template>
   <q-page class="flex flex-center bg-grey-2">
     <q-card class="q-pa-md shadow-2 my_card" bordered>
-      <form
-        @submit.prevent.stop="onSubmit"
-        @reset.prevent.stop="onReset"
-        autocorrect="off"
-        autocapitalize="off"
-        autocomplete="off"
-        spellcheck="false"
-      >
+      <form @submit.prevent.stop="onSubmit" @reset.prevent.stop="onReset" autocorrect="off" autocapitalize="off"
+        autocomplete="off" spellcheck="false">
         <q-card-section class="text-center">
           <h3>Homedocs</h3>
           <h5>I ASSURE YOU; WE'RE OPEN</h5>
@@ -16,59 +10,33 @@
           <div class="text-grey-8">Sign in below to access your account</div>
         </q-card-section>
         <q-card-section>
-          <q-input
-            dense
-            outlined
-            ref="emailRef"
-            v-model="email"
-            type="email"
-            name="email"
-            label="Email Address"
-            :rules="emailRules"
-          >
+          <q-input dense outlined ref="emailRef" v-model="email" type="email" name="email" label="Email Address"
+            :disable="loading" :autofocus="true" :rules="emailRules">
             <template v-slot:prepend>
-              <q-icon name="mail" />
+              <q-icon name="alternate_email" />
             </template>
           </q-input>
-          <q-input
-            dense
-            outlined
-            class="q-mt-md"
-            ref="passwordRef"
-            v-model="password"
-            name="password"
-            type="password"
-            label="Password"
-            :rules="passwordRules"
-          >
+          <q-input dense outlined class="q-mt-md" ref="passwordRef" v-model="password" name="password" type="password"
+            label="Password" :disable="loading" :rules="passwordRules">
             <template v-slot:prepend>
               <q-icon name="key" />
             </template>
           </q-input>
         </q-card-section>
         <q-card-section>
-          <q-btn
-            style="border-radius: 8px"
-            color="dark"
-            rounded
-            size="md"
-            label="Sign in"
-            no-caps
-            class="full-width"
-            @click="onSubmit"
-            type="submit"
-            :rules="passwordRules"
-          ></q-btn>
+          <q-btn color="dark" size="md" label="Sign in" no-caps class="full-width" icon="account_circle"
+            :disable="loading || (!(email && password))" :loading="loading" @click="onSubmit" type="submit">
+            <template v-slot:loading>
+              <q-spinner-hourglass class="on-left" />
+              Loading...
+            </template>
+          </q-btn>
         </q-card-section>
         <q-card-section class="text-center q-pt-none">
           <div class="text-grey-8">
             Don't have an account yet?
             <router-link :to="{ name: 'signUp' }">
-              <span
-                class="text-dark text-weight-bold"
-                style="text-decoration: none"
-                >Sign up.</span
-              >
+              <span class="text-dark text-weight-bold" style="text-decoration: none">Sign up.</span>
             </router-link>
           </div>
         </q-card-section>
@@ -77,71 +45,73 @@
   </q-page>
 </template>
 
-<script>
-import { ref, defineComponent, getCurrentInstance } from "vue";
+<script setup>
+
+import { ref, getCurrentInstance } from "vue";
 import { useQuasar } from "quasar";
 import { useRouter } from "vue-router";
 import { useSessionStore } from "stores/session";
 
-export default defineComponent({
-  name: "SignInPage",
-  setup() {
-    const $q = useQuasar();
+const $q = useQuasar();
 
-    const router = useRouter();
-    const app = getCurrentInstance();
+const router = useRouter();
+const app = getCurrentInstance();
 
-    const email = ref(null);
-    const emailRef = ref(null);
-    const passwordRef = ref(null);
-    const password = ref(null);
+const loading = ref(false);
 
-    return {
-      email,
-      emailRef,
-      password,
-      passwordRef,
-      emailRules: [[(val) => !!val || "Field is required"]],
-      passwordRules: [[(val) => !!val || "Field is required"]],
+const email = ref("");
+const emailRef = ref(null);
+const password = ref("");
+const passwordRef = ref(null);
 
-      onSubmit() {
-        emailRef.value.validate();
-        passwordRef.value.validate();
-        if (!(emailRef.value.hasError || passwordRef.value.hasError)) {
-          app.appContext.config.globalProperties.$api.user
-            .signIn(email, password)
-            .then((success) => {
-              $q.notify({
-                color: "positive",
-                icon: "announcement",
-                message: "Ok",
-              });
-              let session = useSessionStore();
-              session.signIn("jwttest");
-              router.push({
-                name: "index",
-              });
-            })
-            .catch((error) => {
-              $q.notify({
-                color: "negative",
-                icon: "announcement",
-                message: "API Error",
-              });
-            });
-        } else {
-          $q.notify({
-            color: "negative",
-            icon: "announcement",
-            message: "Validation error",
-          });
-        }
-      },
-      onReset() {
-        emailRef.value.resetValidation();
-        passwordRef.value.resetValidation();
-      },
-    };
-  },
-});
+const emailRules = [val => !!val || 'Field is required'];
+const passwordRules = [val => !!val || 'Field is required'];
+
+function onSubmit() {
+  emailRef.value.validate();
+  passwordRef.value.validate();
+  if (!(emailRef.value.hasError || passwordRef.value.hasError)) {
+    loading.value = true;
+    app.appContext.config.globalProperties.$api.user
+      .signIn(email.value, password.value)
+      .then((success) => {
+        $q.notify({
+          color: "positive",
+          icon: "announcement",
+          message: "Ok",
+        });
+        loading.value = false;
+        /*
+        let session = useSessionStore();
+        session.signIn("jwttest");
+        router.push({
+          name: "index",
+        });
+        */
+      })
+      .catch((error) => {
+        $q.notify({
+          color: "negative",
+          icon: "announcement",
+          message: "API Error",
+        });
+        loading.value = false;
+      });
+  } else {
+    $q.notify({
+      color: "negative",
+      icon: "announcement",
+      message: "Validation error",
+    });
+  }
+}
+
+function onReset() {
+  emailRef.value.resetValidation();
+  passwordRef.value.resetValidation();
+}
+
+
+
+
 </script>
