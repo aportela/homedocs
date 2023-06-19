1edeb795395f69455e9991ef7685430644968932
alex <766f6964+github@gmail.com>
2023-06-19 13:02:48 +0200

    set legacy to false (required by composition api)

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -5,6 +5,7 @@ export default ({ app }) => {
   // Create I18n instance
   const i18n = createI18n({
     locale: "en-US",
+    legacy: false, // you must set `false`, to use Composition API
     globalInjection: true,
     messages,
   });
