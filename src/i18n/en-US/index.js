ab90a59ce81030d4fdaf7bd82d9fda0fcbfeed6d
alex <766f6964+github@gmail.com>
2023-06-20 00:26:46 +0200

    new i18n message format

--- src/i18n/en-US/index.js
+++ src/i18n/en-US/index.js
@@ -2,6 +2,27 @@
 // so you can safely delete all default props below
 
 export default {
+  Homedocs: "Homedocs",
+  '"I ASSURE YOU; WE\'RE OPEN"': '"I ASSURE YOU; WE\'RE OPEN"',
+  "Sign in": "Sign in",
+  "Sign in below to access your account":
+    "Sign in below to access your account",
+  Email: "Email",
+  Password: "Password",
+  "Loading...": "Loading...",
+  "Don't have an account yet ?": "Don't have an account yet ?",
+  "Click here to sign up": "Click here to sign up",
+  "Field is required": "Field is required",
+  "Email not registered": "Email not registered",
+  "Invalid password": "Invalid password",
+  "Sign up": "Sign up",
+  "Sign up below to create your account":
+    "Sign up below to create your account",
+  "Email already used": "Email already used",
+  "Already have an account ?": "Already have an account ?",
+  "Click here to sign in": "Click here to sign in",
+  "Your account has been created": "Your account has been created",
+
   pages: {
     common: {
       labels: {
