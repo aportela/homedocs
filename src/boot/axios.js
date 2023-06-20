31b76951bf86065a609ad96d1361718f484177a5
alex <766f6964+github@gmail.com>
2023-06-20 10:32:59 +0200

    added jwt interceptors

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,6 +1,53 @@
 import { boot } from "quasar/wrappers";
 
 import axios from "axios";
+import { useSessionStore } from "stores/session";
+
+const session = useSessionStore();
+const jwt = session.getJWT;
+
+axios.interceptors.request.use(
+  (config) => {
+    if (jwt) {
+      config.headers["HOMEDOCS-JWT"] = jwt;
+      config.withCredentials = true;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    // warning: axios lowercase received header names
+    const apiResponseJWT = response.headers["homedocs-jwt"] || null;
+    if (apiResponseJWT) {
+      if (apiResponseJWT && apiResponseJWT != jwt) {
+        session.signIn(apiResponseJWT);
+      }
+    }
+    return response;
+  },
+  (error) => {
+    // helper for checking invalid fields on api response
+    error.isFieldInvalid = function (fieldName) {
+      return error.response.data.invalidOrMissingParams.indexOf(fieldName) > -1;
+    };
+    error.response.getApiErrorData = function () {
+      return JSON.stringify(
+        {
+          url: error.request.responseURL,
+          response: error.response,
+        },
+        null,
+        "\t"
+      );
+    };
+    return Promise.reject(error);
+  }
+);
 
 const api = {
   common: {
