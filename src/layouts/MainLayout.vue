49a07b3debb866f1f591b2864932668eb46ca7b9
alex <766f6964+github@gmail.com>
2023-06-20 10:31:30 +0200

    added router links

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,49 +1,19 @@
 <template>
   <q-layout class="bg-grey-1">
-    <q-header
-      elevated
-      class="text-white"
-      style="background: #24292e"
-      height-hint="61.59"
-    >
+    <q-header elevated class="text-white" style="background: #24292e" height-hint="61.59">
       <q-toolbar class="q-py-sm q-px-md">
-        <q-btn
-          round2
-          dense
-          flat
-          :ripple="false"
-          size="19px"
-          color="white"
-          class="q-mr-sm"
-          no-caps
-        >
+        <q-btn round2 dense flat :ripple="false" size="19px" color="white" class="q-mr-sm" no-caps>
           <q-avatar square size="42px">
             <img src="icons/favicon-128x128.png" />
           </q-avatar>
         </q-btn>
         HomeDocs
 
-        <q-select
-          ref="search"
-          dark
-          dense
-          standout
-          use-input
-          hide-selected
-          class="GL__toolbar-select q-mx-md"
-          color="black"
-          :stack-label="false"
-          label="Search or jump to..."
-          v-model="text"
-          :options="filteredOptions"
-          @filter="filter"
-          style="width: 300px"
-          v-if="session.isLogged"
-        >
+        <q-select ref="search" dark dense standout use-input hide-selected class="GL__toolbar-select q-mx-md"
+          color="black" :stack-label="false" label="Search..." v-model="text" :options="filteredOptions" @filter="filter"
+          style="width: 300px" v-if="session.isLogged">
           <template v-slot:append>
-            <img
-              src="https://cdn.quasar.dev/img/layout-gallery/img-github-search-key-slash.svg"
-            />
+            <img src="https://cdn.quasar.dev/img/layout-gallery/img-github-search-key-slash.svg" />
           </template>
 
           <template v-slot:no-option>
@@ -57,30 +27,19 @@
           </template>
         </q-select>
 
-        <div
-          v-if="session.isLogged && $q.screen.gt.sm"
-          class="GL__toolbar-link q-ml-xs q-gutter-md text-body2 text-weight-bold row items-center no-wrap"
-        >
-          <a href="javascript:void(0)" class="text-white">New document </a>
-          <a href="javascript:void(0)" class="text-white">Advanced search </a>
+        <div v-if="session.isLogged && $q.screen.gt.sm"
+          class="GL__toolbar-link q-ml-xs q-gutter-md text-body2 text-weight-bold row items-center no-wrap">
+          <router-link class="text-white text-weight-bold" style="text-decoration: none" :to="{ name: 'newDocument' }">New
+            document</router-link>
+          <router-link class="text-white text-weight-bold" style="text-decoration: none"
+            :to="{ name: 'advancedSearch' }">Advanced
+            search</router-link>
         </div>
         <q-space />
 
-        <a
-          v-if="session.isLogged"
-          href="javascript:void(0)"
-          class="text-white text-weight-bold"
-          style="text-decoration: none"
-          @click="signOut"
-          >Sign Out
+        <a v-if="session.isLogged" href="javascript:void(0)" class="text-white text-weight-bold"
+          style="text-decoration: none" @click="signOut">Sign Out
         </a>
-        <!--
-        <q-btn dense flat no-wrap>
-          <q-avatar rounded>
-            <q-icon name="matRemoveModerator" />
-          </q-avatar>
-        </q-btn>
-          -->
       </q-toolbar>
     </q-header>
     <q-page-container>
@@ -89,33 +48,22 @@
   </q-layout>
 </template>
 
-<script>
-import { defineComponent, ref } from "vue";
+<script setup>
+import { ref } from "vue";
 
 import { useSessionStore } from "stores/session";
 import { useRouter } from "vue-router";
 
-export default defineComponent({
-  name: "MainLayout",
+const text = ref("");
+const filteredOptions = ref([]);
+const filter = ref(null);
+const session = useSessionStore();
+const router = useRouter();
 
-  setup() {
-    const text = ref("");
-    const session = useSessionStore();
-    const router = useRouter();
-
-    function signOut() {
-      session.signOut();
-      router.push({
-        name: "signIn",
-      });
-    }
-    return {
-      session,
-      signOut,
-      text: null,
-      filter: null,
-      filteredOptions: null,
-    };
-  },
-});
+function signOut() {
+  session.signOut();
+  router.push({
+    name: "signIn",
+  });
+}
 </script>
