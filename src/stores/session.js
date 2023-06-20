2b871d1b2c233e3a916baa81f1730ce9f04da026
alex <766f6964+github@gmail.com>
2023-06-20 10:32:43 +0200

    load / save methods from localStorage

--- src/stores/session.js
+++ src/stores/session.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import { default as useBasil } from "basil.js";
 
 export const useSessionStore = defineStore("session", {
   state: () => ({
@@ -7,13 +8,27 @@ export const useSessionStore = defineStore("session", {
 
   getters: {
     isLogged: (state) => state.jwt != null,
+    getJWT: (state) => state.jwt,
   },
   actions: {
+    load() {
+      const basil = useBasil();
+      const jwt = basil.get("jwt");
+      if (jwt) {
+        this.jwt = jwt;
+      }
+    },
+    save(jwt) {
+      const basil = useBasil();
+      basil.set("jwt", jwt);
+    },
     signIn(jwt) {
       this.jwt = jwt;
+      this.save(jwt);
     },
     signOut() {
       this.jwt = null;
+      this.save(null);
     },
   },
 });
