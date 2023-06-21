d7f73a5142ee811b832f64addf576cbc753cbad9
alex <766f6964+github@gmail.com>
2023-06-21 10:34:42 +0200

    added autocomplete input tags & preview images dialog

--- src/pages/DocumentPage.vue
+++ src/pages/DocumentPage.vue
@@ -14,11 +14,9 @@
           label="Descripción del documento" :disable="loading" :autofocus="true">
         </q-input>
       </q-card-section>
-      <q-card-section class="text-left">
-        <h5>Tags</h5>
-        <q-chip square color="dark" text-color="white" v-for="tag in document.tags" :key="tag" removable>
-          {{ tag }}
-        </q-chip>
+      <q-card-section>
+        <q-select label="Tags" outlined v-model="document.tags" use-input use-chips multiple hide-dropdown-icon
+          input-debounce="0" new-value-mode="add-unique" clearable />
       </q-card-section>
       <q-card-section class="text-left">
         <h5>Files</h5>
@@ -32,20 +30,56 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="file in document.files" :key="file.id">
+            <tr v-for="file in   document.files  " :key="file.id">
               <td class="text-left">{{ file.uploadedOn }}</td>
               <td class="text-left">{{ file.name }}</td>
               <td class="text-right">{{ file.humanSize }}</td>
               <td class="text-center">
                 <q-btn-group>
-                  <q-btn label="Open/Preview" icon="preview" />
-                  <q-btn label="Download" icon="download" />
-                  <q-btn label="Remove" icon="delete" />
+                  <q-btn label="Open/Preview" icon="preview" @click.prevent="onPreviewFile(file)"
+                    :disable="loading || !isImage(file.name)" />
+                  <q-btn label="Download" icon="download" :href="'api2/file/' + file.id" :disable="loading" />
+                  <q-btn label="Remove" icon="delete" :disable="loading" />
                 </q-btn-group>
               </td>
             </tr>
           </tbody>
         </q-markup-table>
+        <q-dialog>
+          <q-card style="width: 700px; max-width: 80vw;">
+            <q-card-section class="row items-center q-pb-none">
+              <div class="text-h6">{{ previewFile.name }} ({{ previewFile.humanSize }})</div>
+              <q-space />
+              <q-btn icon="close" flat round dense v-close-popup />
+            </q-card-section>
+            <q-img :src="'api2/file/' + previewFile.id" />
+          </q-card>
+        </q-dialog>
+        <q-dialog v-model="showPreviewDialog">
+          <q-card style="width: 700px; max-width: 80vw;">
+            <q-card-section class="row items-center q-pb-none">
+              <div class="text-h6">File preview</div>
+              <q-space />
+              <q-btn icon="close" flat round dense v-close-popup />
+            </q-card-section>
+            <q-card-section class="q-pt-none">
+              <q-img :src="'api2/file/' + previewFile.id" loading="lazy" spinner-color="white">
+                <div class="absolute-bottom text-subtitle1 text-center">
+                  {{ previewFile.name }} ({{ previewFile.humanSize }})
+                </div>
+              </q-img>
+              <q-card-actions align="right">
+                <q-btn outline :href="'api2/file/' + previewFile.id" label="Download" icon="download" />
+                <q-btn outline v-close-popup label="Close" icon="close" />
+              </q-card-actions>
+            </q-card-section>
+            <!--
+            <q-card-actions align="right">
+              <q-btn flat label="Close" color="primary" v-close-popup />
+            </q-card-actions>
+            -->
+          </q-card>
+        </q-dialog>
       </q-card-section>
     </form>
 
@@ -59,7 +93,12 @@ import { useRoute, useRouter } from "vue-router";
 import { api } from 'boot/axios'
 import { date } from 'quasar'
 import { format } from "quasar";
+
+
 const { humanStorageSize } = format
+
+const showPreviewDialog = ref(false);
+const previewFile = ref(null);
 
 const loading = ref(false);
 
@@ -73,6 +112,33 @@ const document = ref({
   files: [],
   tags: []
 });
+
+const availableTags = ref([]);
+
+function onLoadAvailableTags() {
+  loading.value = true;
+  api.tag.search().then((response) => {
+    availableTags.value = response.data.tags;
+    loading.value = false;
+  }).catch((error) => {
+    loading.value = false;
+    switch (error.response.status) {
+      case 401:
+        this.$router.push({
+          name: "signIn",
+        });
+        break;
+      default:
+        /*
+        this.$emit("showAPIError", {
+          httpCode: error.response.status,
+          data: error.response.getApiErrorData(),
+        });
+        */
+        break;
+    }
+  });
+}
 
 function onRefresh() {
   loading.value = true;
@@ -112,12 +178,21 @@ function onRefresh() {
 function onSubmitForm() {
 }
 
+function onPreviewFile(file) {
+  previewFile.value = file;
+  showPreviewDialog.value = true;
+}
+
+function isImage(filename) {
+  return (filename.match(/.(jpg|jpeg|png|gif)$/i));
+}
 const route = useRoute();
 //const router = useRouter();
 
 
 document.value.id = route.params.id || null;
 
+onLoadAvailableTags();
 if (document.value.id) {
   onRefresh();
 }
