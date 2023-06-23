898b4dc3eecbcc5c7202a8503c03ab460de0b40d
alex <766f6964+github@gmail.com>
2023-06-23 11:28:47 +0200

    added model

--- src/components/TagSelector.vue
+++ src/components/TagSelector.vue
@@ -1,11 +1,11 @@
 <template>
-  <q-select ref="selectRef" :label="t('Tags')" dense outlined v-model="currentTags" use-input use-chips multiple
+  <q-select ref="selectRef" :label="t('Tags')" v-model="currentTags" dense outlined use-input use-chips multiple
     hide-dropdown-icon :options="filteredTags" input-debounce="0" new-value-mode="add-unique" clearable
     :disable="disabled || loading || loadingError" :loading="loading" :error="loadingError"
     :errorMessage="t('Error loading available tags')" @filter="onFilterTags" @add="onAddTag">
     <template v-slot:selected>
-      <q-chip removable v-for="tag, index in currentTags" :model="tag" :key="tag" @remove="removeTagAtIndex(index)"
-        color="dark" text-color="white" icon="label_important">
+      <q-chip removable v-for="tag, index in currentTags" :key="tag" @remove="removeTagAtIndex(index)" color="dark"
+        text-color="white" icon="label_important">
         {{ tag }}
       </q-chip>
     </template>
@@ -14,18 +14,18 @@
 
 <script setup>
 
-import { defineProps, defineEmits, ref, watch } from "vue";
+import { defineProps, defineEmits, ref, watch, computed } from "vue";
 import { api } from 'boot/axios'
 import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n();
 
 const props = defineProps({
-  disabled: Boolean,
-  selectedTags: Array
+  modelValue: Array,
+  disabled: Boolean
 });
 
-const emit = defineEmits(['change', 'error']);
+const emit = defineEmits(['update:modelValue', 'error']);
 
 const selectRef = ref('');
 
@@ -33,10 +33,15 @@ const loading = ref(false);
 const loadingError = ref(false);
 const availableTags = ref([]);
 const filteredTags = ref([]);
-const currentTags = ref(props.selectedTags || []);
+const currentTags = ref([]);
+const selectedTagsProp = computed(() => props.modelValue);
+
+watch(selectedTagsProp, (newValue) => {
+  currentTags.value = newValue;
+});
 
 watch(currentTags, (newValue) => {
-  emit('change', newValue);
+  emit('update:modelValue', newValue);
 });
 
 
@@ -58,7 +63,7 @@ function loadAvailableTags() {
   loading.value = true;
   api.tag.search().then((response) => {
     availableTags.value = response.data.tags;
-    currentTags.value = props.selectedTags || [];
+    console.log("Al cargar tiene " + currentTags.value.length);
     loading.value = false;
   }).catch((error) => {
     loadingError.value = true;
@@ -67,7 +72,7 @@ function loadAvailableTags() {
   });
 }
 
-function onAddTag() {
+function onAddTag(tag) {
   // TODO: hide select popup after adding element not working
   selectRef.value.hidePopup()
 }
@@ -75,6 +80,7 @@ function onAddTag() {
 function removeTagAtIndex(index) {
   currentTags.value.splice(index, 1);
 }
+
 loadAvailableTags();
 
 </script>
