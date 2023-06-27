840acb939accea22494af8b7bad7cd148b207960
alex <766f6964+github@gmail.com>
2023-06-27 11:47:55 +0200

    added expansion items for dashboard blocks (collapsed on mobile)

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,47 +4,46 @@
       <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 flex">
         <q-card class="my-card fit" flat bordered>
           <q-card-section>
-            <div class="text-h6"><q-icon name="work_history" class="q-mr-sm" />Recent documents</div>
-          </q-card-section>
-          <q-card-section>
-            <q-markup-table>
-              <thead>
-                <tr>
-                  <th class="text-left">Title</th>
-                  <th class="text-left">Created on</th>
-                  <th class="text-right">Files</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="recentDocument in recentDocuments" :key="recentDocument.id">
-                  <td class="text-left"><router-link :to="{ name: 'document', params: { id: recentDocument.id } }">{{
-                    recentDocument.title }}</router-link>
-                  </td>
-                  <td class="text-left">{{ recentDocument.createdOn }}</td>
-                  <td class="text-right">{{ recentDocument.fileCount }}</td>
-                </tr>
-              </tbody>
-            </q-markup-table>
+            <q-expansion-item expand-separator icon="work_history" label="Recent documents"
+              caption="Last 32 documents sort by creation date desc" :model-value="!$q.screen.lt.md">
+              <q-markup-table>
+                <thead>
+                  <tr>
+                    <th class="text-left">Title</th>
+                    <th class="text-left">Created on</th>
+                    <th class="text-right">Files</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="recentDocument in recentDocuments" :key="recentDocument.id">
+                    <td class="text-left"><router-link :to="{ name: 'document', params: { id: recentDocument.id } }">{{
+                      recentDocument.title }}</router-link>
+                    </td>
+                    <td class="text-left">{{ recentDocument.createdOn }}</td>
+                    <td class="text-right">{{ recentDocument.fileCount }}</td>
+                  </tr>
+                </tbody>
+              </q-markup-table>
+            </q-expansion-item>
           </q-card-section>
         </q-card>
       </div>
       <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 flex">
         <q-card class="my-card fit" flat bordered>
           <q-card-section>
-            <div class="text-h6"><q-icon name="bookmark" class="q-mr-sm" />Browse by tags</div>
-          </q-card-section>
-          <q-card-section>
-            <q-chip square outline text-color="dark" v-for="tag in tags" :key="tag">
-              <q-avatar color="grey-9" text-color="white">{{ tag.total }}</q-avatar>
-              <router-link :to="{ name: 'advancedSearchByTag', params: { tag: tag.tag } }" style="text-decoration: none"
-                class="text-dark">
-                {{ tag.tag }}</router-link>
-            </q-chip>
+            <q-expansion-item expand-separator icon="bookmark" label="Browse by tags"
+              caption="Last 32 documents sort by creation date desc" :model-value="!$q.screen.lt.md">
+              <q-chip square outline text-color="dark" v-for="tag in tags" :key="tag">
+                <q-avatar color="grey-9" text-color="white">{{ tag.total }}</q-avatar>
+                <router-link :to="{ name: 'advancedSearchByTag', params: { tag: tag.tag } }" style="text-decoration: none"
+                  class="text-dark">
+                  {{ tag.tag }}</router-link>
+              </q-chip>
+            </q-expansion-item>
           </q-card-section>
         </q-card>
       </div>
     </div>
-
   </q-page>
 </template>
 
@@ -53,7 +52,9 @@
 import { ref } from "vue";
 import { date } from 'quasar'
 import { api } from 'boot/axios'
+import { useQuasar } from 'quasar'
 
+const $q = useQuasar();
 const loading = ref(false);
 const recentDocuments = ref([]);
 const tags = ref([]);
