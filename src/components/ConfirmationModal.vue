8b5935564cf5699050353000f8a86a3c67e2b0f5
alex <766f6964+github@gmail.com>
2023-07-05 11:06:08 +0200

    style confirmation modal

--- src/components/ConfirmationModal.vue
+++ src/components/ConfirmationModal.vue
@@ -1,15 +1,17 @@
 <template>
   <q-dialog v-model="visible" @hide="emit('close')">
     <q-card>
-      <q-card-section>
+      <q-card-section class="bg-grey-8 text-white q-p-none">
         <slot name="header"></slot>
       </q-card-section>
-      <q-card-section class="q-pb-none">
+      <q-card-section class="q-p-none">
         <slot name="body"></slot>
       </q-card-section>
       <q-card-actions align="right">
-        <q-btn outline v-close-popup><q-icon left name="close" />{{ t("Cancel") }}</q-btn>
-        <q-btn outline @click.stop="emit('ok')"><q-icon left name="done" />{{ t("Ok") }}</q-btn>
+        <q-btn outline v-close-popup class="bg-grey-6 text-white"><q-icon left name="close" />{{ t("Cancel")
+        }}</q-btn>
+        <q-btn outline @click.stop="emit('ok')" class="bg-dark text-white"><q-icon left name="done" />{{ t("Ok")
+        }}</q-btn>
       </q-card-actions>
     </q-card>
   </q-dialog>
