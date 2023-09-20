75d7c1e38d2f1d4a2b77e65d01417982a4f6fd43
alex <766f6964+github@gmail.com>
2023-09-20 23:34:10 +0200

    removed basil (not used)

--- quasar-frontend/src/stores/initialState.js
+++ quasar-frontend/src/stores/initialState.js
@@ -1,13 +1,10 @@
 import { defineStore } from "pinia";
-import { default as useBasil } from "basil.js";
 import { api } from "boot/axios";
 
-const localStorageBasilOptions = {
-  namespace: "homedocs",
-  storages: ["local", "cookie", "session", "memory"],
-  storage: "local",
-  expireDays: 3650,
-};
+const hashedSite = Array.from(window.location.host).reduce(
+  (hash, char) => 0 | (31 * hash + char.charCodeAt(0)),
+  0
+);
 
 export const useInitialStateStore = defineStore("initialState", {
   state: () => ({
@@ -32,17 +29,6 @@ export const useInitialStateStore = defineStore("initialState", {
         .catch((error) => {
           console.error(error.response);
         });
-      /*
-      const basil = useBasil(localStorageBasilOptions);
-      const initialState = basil.get("initialState");
-      if (initialState) {
-        this.initialState = initialState;
-      }
-      */
-    },
-    save(initialState) {
-      const basil = useBasil(localStorageBasilOptions);
-      basil.set("initialState", initialState);
     },
   },
 });
