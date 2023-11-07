4c40dfc3e3f139b32ac66d9401215276051b15ca
alex <766f6964+github@gmail.com>
2023-11-07 12:03:21 +0100

    php-stan: fixed warning

--- phpslim-api/homedocs/Utils.php
+++ phpslim-api/homedocs/Utils.php
@@ -89,10 +89,10 @@ class Utils
     /**
      * https://stackoverflow.com/a/25370978
      */
-    private static function parseIniSize($size): float
+    private static function parseIniSize(string $iniSize): float
     {
-        $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
-        $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
+        $unit = preg_replace('/[^bkmgtpezy]/i', '', $iniSize); // Remove the non-unit characters from the size.
+        $size = intval(preg_replace('/[^0-9\.]/', '', $iniSize)); // Remove the non-numeric characters from the size.
         if ($unit) {
             // Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
             return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
