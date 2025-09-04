f2efbc7cce7bb5c12324ba4af62838182400e7f8
alex <766f6964+github@gmail.com>
2025-09-04 12:07:53 +0200

    code cleanups

--- quasar-frontend/src/pages/SignUpPage.vue
+++ quasar-frontend/src/pages/SignUpPage.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-page class="flex flex-center _bg-grey-2">
-    <q-card class="q-pa-md shadow-2 my_card" bordered>
+  <q-page class="flex flex-center">
+    <q-card class="q-pa-md shadow-2" bordered>
       <form @submit.prevent.stop="onValidateForm" autocorrect="off" autocapitalize="off" autocomplete="off"
         spellcheck="false">
         <q-card-section class="text-center">
