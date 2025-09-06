d84773882c420b13d5cdc5c579c6be1d894158be
alex <766f6964+github@gmail.com>
2025-09-06 16:30:30 +0200

    splitted activity heatmap on external component

---
+++ quasar-frontend/src/components/ActivityHeatMap.vue
@@ -0,0 +1,121 @@
+<template>
+  <div id="cal-heatmap"></div>
+  <!--
+    <q-btn-group flat dense class="q-mt-sm">
+      <q-btn icon="arrow_left" @click.prevent="cal.previous()">Previous</q-btn>
+      <q-btn icon-right="arrow_right" @click.prevent="cal.next()">Next</q-btn>
+    </q-btn-group>
+    -->
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+
+import { api } from "boot/axios";
+
+import CalHeatmap from "cal-heatmap";
+import "cal-heatmap/cal-heatmap.css";
+import Tooltip from 'cal-heatmap/plugins/Tooltip';
+import CalendarLabel from 'cal-heatmap/plugins/CalendarLabel';
+
+const router = useRouter();
+
+const activityHeatMapData = ref([]);
+
+const cal = new CalHeatmap();
+
+const loadingError = ref(false);
+const loading = ref(false);
+
+cal.on('click', (event, timestamp, value) => {
+  if (value > 0) {
+    router.push({
+      name: "advancedSearchByFixedDate",
+      params: {
+        fixedDate: new Date(timestamp).toISOString().split('T')[0]
+      }
+    });
+  }
+});
+
+function refreshActivityHeatmapData() {
+  activityHeatMapData.value = [];
+  loading.value = true;
+  loadingError.value = false;
+  api.stats.getActivityHeatMapData()
+    .then((success) => {
+      activityHeatMapData.value = success.data.heatmap;
+      const maxValue = activityHeatMapData.value.reduce((max, obj) => (obj.count > max ? obj.count : max), -Infinity)
+      cal.paint(
+        {
+          data: {
+            source: activityHeatMapData.value,
+            x: "date",
+            y: "count",
+            type: "json",
+          },
+          date: {
+            start: new Date(new Date().setFullYear(new Date().getFullYear() - 1)),
+            end: new Date(),
+            max: new Date()
+          },
+          range: 12,
+          scale: {
+            color: {
+              scheme: 'Greens',
+              type: "linear",
+              domain: [0, maxValue],
+            },
+          },
+          domain: {
+            type: 'month',
+            gutter: 4,
+            label: { text: 'MMM', textAlign: 'start', position: 'top' },
+          },
+          subDomain: { type: 'ghDay', radius: 2, width: 11, height: 11, gutter: 4 },
+          itemSelector: '#cal-heatmap',
+        },
+        [
+          [
+            Tooltip,
+            {
+              text: function (date, value, dayjsDate) {
+                return (
+                  (value ? value : 'No') +
+                  ' changes on ' +
+                  dayjsDate.format('dddd, MMMM D, YYYY')
+                );
+              },
+            },
+          ],
+          [
+            CalendarLabel,
+            {
+              width: 30,
+              textAlign: 'start',
+              text: () => dayjs.weekdaysShort().map((d, i) => (i % 2 == 0 ? '' : d)),
+              padding: [25, 0, 0, 0],
+            },
+          ],
+        ]
+      );
+
+      loading.value = false;
+    })
+    .catch((error) => {
+      loading.value = false;
+      loadingError.value = true;
+      $q.notify({
+        type: "negative",
+        message: t("API Error: fatal error"),
+        caption: t("API Error: fatal error details", { status: error.response.status, statusText: error.response.statusText })
+      });
+    });
+}
+
+onMounted(() => {
+  refreshActivityHeatmapData();
+});
+
+</script>
--- quasar-frontend/src/components/SystemStats.vue
+++ quasar-frontend/src/components/SystemStats.vue
@@ -9,6 +9,7 @@
             <q-spinner-pie v-if="loading" color="grey-5" size="md" />
           </p>
           <div v-else>
+            <ActivityHeatMap class="q-mb-sm"></ActivityHeatMap>
             <div class="row q-gutter-sm">
               <div class="col-2">
                 <q-card class="bg-grey-3">
@@ -37,13 +38,6 @@
             </div>
           </div>
         </q-expansion-item>
-        <div id="cal-heatmap"></div>
-        <!--
-        <q-btn-group flat dense class="q-mt-sm">
-          <q-btn icon="arrow_left" @click.prevent="cal.previous()">Previous</q-btn>
-          <q-btn icon-right="arrow_right" @click.prevent="cal.next()">Next</q-btn>
-        </q-btn-group>
-        -->
       </q-card-section>
     </q-card>
   </div>
@@ -51,16 +45,13 @@
 
 <script setup>
 
-import { ref } from "vue";
-import { useRouter } from "vue-router";
+import { ref, onMounted } from "vue";
+
 import { useI18n } from "vue-i18n";
 import { format, useQuasar } from "quasar"
 import { api } from "boot/axios";
 
-import CalHeatmap from "cal-heatmap";
-import "cal-heatmap/cal-heatmap.css";
-import Tooltip from 'cal-heatmap/plugins/Tooltip';
-import CalendarLabel from 'cal-heatmap/plugins/CalendarLabel';
+import ActivityHeatMap from 'components/ActivityHeatMap.vue';
 
 const { t } = useI18n();
 const $q = useQuasar();
@@ -71,9 +62,6 @@ let expanded = !$q.screen.lt.md;
 const totalDocuments = ref(0);
 const totalAttachments = ref(0);
 const totalAttachmentsDiskUsage = ref(0);
-const activityHeatMapData = ref([]);
-
-const router = useRouter();
 
 function refreshTotalDocuments() {
   totalDocuments.value = 0;
@@ -135,97 +123,10 @@ function refreshTotalAttachmentsDiskUsage() {
     });
 }
 
-const cal = new CalHeatmap();
-
-cal.on('click', (event, timestamp, value) => {
-  if (value > 0) {
-    router.push({
-      name: "advancedSearchByFixedDate",
-      params: {
-        fixedDate: new Date(timestamp).toISOString().split('T')[0]
-      }
-    });
-  }
+onMounted(() => {
+  refreshTotalDocuments();
+  refreshTotalAttachments();
+  refreshTotalAttachmentsDiskUsage();
 });
-
-function refreshActivityHeatmapData() {
-  activityHeatMapData.value = [];
-  loading.value = true;
-  loadingError.value = false;
-  api.stats.getActivityHeatMapData()
-    .then((success) => {
-      activityHeatMapData.value = success.data.heatmap;
-      const maxValue = activityHeatMapData.value.reduce((max, obj) => (obj.count > max ? obj.count : max), -Infinity)
-      cal.paint(
-        {
-          data: {
-            source: activityHeatMapData.value,
-            x: "date",
-            y: "count",
-            type: "json",
-          },
-          date: {
-            start: new Date(new Date().setFullYear(new Date().getFullYear() - 1)),
-            end: new Date(),
-            max: new Date()
-          },
-          range: 12,
-          scale: {
-            color: {
-              scheme: 'Greens',
-              type: "linear",
-              domain: [0, maxValue],
-            },
-          },
-          domain: {
-            type: 'month',
-            gutter: 4,
-            label: { text: 'MMM', textAlign: 'start', position: 'top' },
-          },
-          subDomain: { type: 'ghDay', radius: 2, width: 11, height: 11, gutter: 4 },
-          itemSelector: '#cal-heatmap',
-        },
-        [
-          [
-            Tooltip,
-            {
-              text: function (date, value, dayjsDate) {
-                return (
-                  (value ? value : 'No') +
-                  ' changes on ' +
-                  dayjsDate.format('dddd, MMMM D, YYYY')
-                );
-              },
-            },
-          ],
-          [
-            CalendarLabel,
-            {
-              width: 30,
-              textAlign: 'start',
-              text: () => dayjs.weekdaysShort().map((d, i) => (i % 2 == 0 ? '' : d)),
-              padding: [25, 0, 0, 0],
-            },
-          ],
-        ]
-      );
-
-      loading.value = false;
-    })
-    .catch((error) => {
-      loading.value = false;
-      loadingError.value = true;
-      $q.notify({
-        type: "negative",
-        message: t("API Error: fatal error"),
-        caption: t("API Error: fatal error details", { status: error.response.status, statusText: error.response.statusText })
-      });
-    });
-}
-
-refreshTotalDocuments();
-refreshTotalAttachments();
-refreshTotalAttachmentsDiskUsage();
-refreshActivityHeatmapData();
 
 </script>
