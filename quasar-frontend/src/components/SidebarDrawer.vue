f51d0e064983b8799b3a0c5b2a638fde4211c18a
alex <766f6964+github@gmail.com>
2025-09-12 10:02:24 +0200

    not required tags removed

--- quasar-frontend/src/components/SidebarDrawer.vue
+++ quasar-frontend/src/components/SidebarDrawer.vue
@@ -1,68 +1,60 @@
 <template>
-  <q-drawer v-bind="attrs" show-if-above bordered :width="240" :mini="mini" @click.capture="drawerClick"
-    class="fixed bg-grey-1">
-    <q-scroll-area class="fit">
-      <q-list padding>
-        <q-item class="cursor-pointer non-selectable rounded-borders q-ma-sm sidebar_item">
-          <q-item-section avatar>
-            <q-avatar square size="24px">
-              <img src="icons/favicon-128x128.png" />
-            </q-avatar>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label class="text-weight-bold text-uppercase">HomeDocs</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item v-for="link in menuItems" :key="link.text" v-ripple clickable :to="{ name: link.routeName }"
-          class="rounded-borders q-ma-sm sidebar_item" :active="$route.name === link.routeName"
-          active-class="bg-grey-4 text-grey-8">
-          <q-item-section avatar>
-            <q-icon color="grey" :name="link.icon" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>{{ t(link.text) }}</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item v-ripple clickable @click="signOut" class="rounded-borders q-ma-sm sidebar_item">
-          <q-item-section avatar>
-            <q-icon color="grey" name="logout" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>{{ t("Sign out") }}</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-scroll-area>
-    <!--
-    <div class="q-mini-drawer-hide absolute" style="top: 22px; right: -17px">
-      <q-btn dense round unelevated color="accent" icon="chevron_left" @click="mini = true"
-        style="background-color: rgb(105, 108, 255); color: white; border: 6px solid rgb(242, 242, 247);" />
-    </div>
-    -->
+  <q-drawer v-bind="attrs" show-if-above bordered :width="240" :mini="mini" @click.capture="onDrawerClick"
+    class="bg-grey-1 fit">
+    <q-list>
+      <q-item class="cursor-pointer non-selectable rounded-borders q-ma-sm q-item-bordered">
+        <q-item-section avatar>
+          <q-avatar square size="24px">
+            <img src="icons/favicon-128x128.png" />
+          </q-avatar>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label class="text-weight-bold text-uppercase">HomeDocs</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item v-for="link in menuItems" :key="link.text" v-ripple clickable :to="{ name: link.routeName }"
+        class="rounded-borders q-ma-sm q-item-bordered" :active="$route.name === link.routeName"
+        active-class="bg-grey-4 text-dark">
+        <q-item-section avatar>
+          <q-icon color="grey" :name="link.icon" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ t(link.text) }}</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item v-ripple clickable @click="signOut" class="rounded-borders q-ma-sm q-item-bordered">
+        <q-item-section avatar>
+          <q-icon color="grey" name="logout" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ t("Sign out") }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-drawer>
 </template>
 
 <script setup>
 import { computed, useAttrs } from "vue";
-import { api } from "boot/axios";
-import { useSessionStore } from "stores/session";
 import { useRouter } from "vue-router";
 import { useI18n } from "vue-i18n";
 import { useQuasar } from "quasar";
-
-const attrs = useAttrs();
-
-const { t } = useI18n();
-const $q = useQuasar();
-const session = useSessionStore();
-if (!session.isLoaded) {
-  session.load();
-}
-const router = useRouter();
+import { api } from "boot/axios";
+import { useSessionStore } from "stores/session";
 
 const props = defineProps({
   mini: Boolean
 });
+
+const attrs = useAttrs();
+const $q = useQuasar();
+const { t } = useI18n();
+const router = useRouter();
+
+const session = useSessionStore();
+if (!session.isLoaded) {
+  session.load();
+}
 
 const mini = computed(() => props.mini);
 
@@ -72,7 +64,7 @@ const menuItems = [
   { icon: 'find_in_page', text: "Advanced search", routeName: 'advancedSearch' }
 ];
 
-function drawerClick(e) {
+function onDrawerClick(e) {
   if (mini.value) {
     mini.value = false
     // notice we have registered an event with capture flag;
@@ -103,7 +95,7 @@ function signOut() {
 </script>
 
 <style scoped>
-.sidebar_item {
+.q-item-bordered {
   border: 1px solid #eee;
 }
 </style>
