63e66685083ec7dfb44bf87d076877102034a799
alex <766f6964+github@gmail.com>
2025-09-15 16:51:41 +0200

    styles cleaned

--- quasar-frontend/src/layouts/AuthRegisterLayout.vue
+++ quasar-frontend/src/layouts/AuthRegisterLayout.vue
@@ -1,7 +1,7 @@
 <template>
-  <q-layout class="q-layout-themed-signin-signup">
+  <q-layout>
     <div class="row" style="height: 100vh">
-      <div class="col-12 col-lg-8 col-md-12 col-sm-12 slogan-image-cover flex flex-center">
+      <div class="col-12 col-lg-8 col-md-12 col-sm-12 slogan-background-image-cover flex flex-center">
         <div class="q-pa-xl text-center text-white">
           <h2 class="text-h2 text-weight-bolder q-mb-md">{{ t("Welcome to HomeDocs") }}</h2>
           <h4 class="text-grey-4 text-h4">{{ t("Where your documents find their place.") }}</h4>
@@ -23,26 +23,10 @@ const { t } = useI18n();
 </script>
 
 <style scoped>
-.slogan-image-cover {
+.slogan-background-image-cover {
   background-image: linear-gradient(135deg, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.8) 100%), url('https://images.pexels.com/photos/3184460/pexels-photo-3184460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
   background-size: cover;
   background-position: center;
   filter: grayscale(100%);
-}
-</style>
-
-<style>
-.body--light {
-  .q-layout-themed-signin-signup {
-    background-color: var(--color-zinc-100);
-  }
-
-}
-
-.body--dark {
-  .q-layout-themed-signin-signup {
-    background-color: var(--color-zinc-800);
-  }
-
 }
 </style>
