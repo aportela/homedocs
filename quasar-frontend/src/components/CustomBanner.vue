03a4b273486b30f61b3387eb19a7c0dd75120e7c
alex <766f6964+github@gmail.com>
2025-09-18 11:38:15 +0200

    new custom banner component

---
+++ quasar-frontend/src/components/CustomBanner.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-banner dense rounded class="theme-default-q-banner"
+    :class="{ 'theme-default-q-banner-success': success, 'theme-default-q-banner-error': error }">
+    <template v-slot:avatar v-if="success || error">
+      <q-icon v-if="success" name="check" class="text-green" />
+      <q-icon v-else-if="error" name="error" class="text-red" />
+    </template>
+    {{ t(text) }}
+  </q-banner>
+</template>
+
+<script setup>
+
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+
+const props = defineProps({
+  text: String,
+  success: Boolean,
+  error: Boolean,
+});
+
+</script>
