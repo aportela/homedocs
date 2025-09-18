5f27edd5bce6d346a619411e87a4fdb154349cfd
alex <766f6964+github@gmail.com>
2025-09-18 23:35:38 +0200

    new profile form widget

---
+++ quasar-frontend/src/components/ProfileFormWidget.vue
@@ -0,0 +1,146 @@
+<template>
+  <CustomWidget title="Personal information" caption="Update your data" icon="contact_mail">
+    <template v-slot:content>
+      <form @submit.prevent.stop="onValidateForm" autocorrect="off" autocapitalize="off" autocomplete="off"
+        spellcheck="false">
+        <q-input class="q-my-md" dense outlined v-model="email" type="email" name="email" :label="t('Email')" readonly
+          :error="false">
+          <template v-slot:prepend>
+            <q-icon name="alternate_email" />
+          </template>
+        </q-input>
+        <CustomInputPassword class="q-my-md" dense outlined v-model="password" name="password"
+          :label="t('New password')" :error="validator.hasErrors" ref="passwordRef"
+          :errorMessage="validator.message ? t(validator.message) : ''" :disable="loading" :autofocus="true"
+          :rules="formUtils.requiredFieldRules" lazy-rules>
+        </CustomInputPassword>
+        <q-btn color="primary" size="md" :label="$t('Update profile')" no-caps class="full-width" icon="account_circle"
+          :disable="loading || !password" :loading="loading" type="submit">
+          <template v-slot:loading>
+            <q-spinner-hourglass class="on-left" />
+            {{ t('Update profile') }}
+          </template>
+        </q-btn>
+        <CustomBanner v-if="profileUpdatedSuccessfully || error" :text="bannerText"
+          :success="profileUpdatedSuccessfully" :error="error">
+          <template v-slot:details v-if="error && initialState.isDevEnvironment && apiError">
+            <APIErrorDetails class="q-mt-md" :apiError="apiError"></APIErrorDetails>
+          </template>
+        </CustomBanner>
+      </form>
+    </template>
+  </CustomWidget>
+</template>
+
+<script setup>
+import { ref, computed, onMounted, nextTick } from "vue";
+import { useI18n } from 'vue-i18n'
+import { useInitialStateStore } from "stores/initialState";
+
+import { api } from 'boot/axios'
+import { useFormUtils } from "src/composables/formUtils";
+
+import { default as CustomWidget } from "src/components/CustomWidget.vue";
+import { default as CustomInputPassword } from "src/components/CustomInputPassword.vue";
+import { default as CustomBanner } from "src/components/CustomBanner.vue";
+import { default as APIErrorDetails } from "components/APIErrorDetails.vue";
+
+const { t } = useI18n();
+const formUtils = useFormUtils();
+const initialState = useInitialStateStore();
+
+const loading = ref(false);
+const error = ref(false);
+
+const email = ref(null);
+const password = ref(null);
+const passwordRef = ref(null);
+
+const profileUpdatedSuccessfully = ref(false);
+const apiError = ref(null);
+
+const validator = ref({
+  hasErrors: false,
+  message: null
+});
+
+const bannerText = computed(() => {
+  if (profileUpdatedSuccessfully.value) {
+    return ("Profile has been successfully updated");
+  } else if (error.value) {
+    return ("Error loading data")
+  } else {
+    return (null);
+  }
+});
+
+const onResetForm = () => {
+  validator.value.hasErrors = false;
+  validator.value.message = null;
+  passwordRef.value.resetValidation();
+}
+
+const onGetProfile = () => {
+  loading.value = true;
+  error.value = false;
+  email.value = null;
+  password.value = null;
+  api.user.getProfile()
+    .then((success) => {
+      loading.value = false;
+      email.value = success.data.data.email;
+      nextTick(() => {
+        passwordRef.value?.focus();
+      });
+    })
+    .catch((error) => {
+      loading.value = false;
+      error.value = true;
+      apiError.value = error.customAPIErrorDetails;
+      nextTick(() => {
+        passwordRef.value?.focus();
+      });
+    });
+}
+
+
+const onValidateForm = () => {
+  onResetForm();
+  passwordRef.value.validate();
+  nextTick(() => {
+    if (!(passwordRef.value.hasError)) {
+      onSubmitForm();
+    }
+  });
+}
+
+const onSubmitForm = () => {
+  loading.value = true;
+  error.value = false;
+  profileUpdatedSuccessfully.value = false;
+  api.user
+    .updateProfile(email.value, password.value)
+    .then((success) => {
+      loading.value = false;
+      email.value = success.data.data.email;
+      password.value = null;
+      profileUpdatedSuccessfully.value = true;
+      nextTick(() => {
+        passwordRef.value?.focus();
+      });
+    })
+    .catch((error) => {
+      loading.value = false;
+      error.value = true;
+      apiError.value = error.customAPIErrorDetails;
+      nextTick(() => {
+        passwordRef.value?.focus();
+      });
+    });
+}
+
+onMounted(() => {
+  onGetProfile();
+});
+
+</script>
--- quasar-frontend/src/pages/ProfilePage.vue
+++ quasar-frontend/src/pages/ProfilePage.vue
@@ -11,52 +11,7 @@
     </div>
     <div class="row q-col-gutter-sm">
       <div class="col-lg-4 col-xl-4 col-12 flex">
-        <CustomWidget title="Personal information" caption="Update your data" icon="contact_mail">
-          <template v-slot:content>
-            <form @submit.prevent.stop="onValidateForm" autocorrect="off" autocapitalize="off" autocomplete="off"
-              spellcheck="false">
-              <q-input dense outlined ref="emailRef" v-model="email" type="email" name="email" :label="t('Email')"
-                readonly>
-                <template v-slot:prepend>
-                  <q-icon name="alternate_email" />
-                </template>
-              </q-input>
-              <q-input dense outlined class="q-mt-md" ref="passwordRef" v-model="password" name="password"
-                :type="visiblePassword ? 'text' : 'password'" :label="t('New password')" :disable="loading" autofo2cus
-                :error="remoteValidation.password.hasErrors"
-                :errorMessage="remoteValidation.password.message ? t(remoteValidation.password.message) : ''">
-                <template v-slot:prepend>
-                  <q-icon name="key" />
-                </template>
-                <template v-slot:append>
-                  <q-icon :name="visiblePassword ? 'visibility_off' : 'visibility'" class="cursor-pointer"
-                    @click="visiblePassword = !visiblePassword" />
-                </template>
-                <q-tooltip anchor="bottom right" self="top end">
-                  {{
-                    t(visiblePassword ? "Hide password" : "Show password")
-                  }}</q-tooltip>
-              </q-input>
-              <!--
-              <CustomInputPassword dense outlined class="q-mt-md" v-model="password" name="password"
-                :label="t('New password')" :disable="loading" autofocus></CustomInputPassword>
--->
-              <q-btn color="primary" size="md" :label="$t('Update profile')" no-caps class="full-width"
-                icon="account_circle" :disable="loading || !password" :loading="loading" type="submit">
-                <template v-slot:loading>
-                  <q-spinner-hourglass class="on-left" />
-                  {{ t('Update profile') }}
-                </template>
-              </q-btn>
-              <CustomBanner v-if="profileUpdated || loadingError" :text="bannerText" :success="profileUpdated"
-                :error="loadingError">
-                <template v-slot:details v-if="loadingError && initialState.isDevEnvironment && apiError">
-                  <APIErrorDetails class="q-mt-md" :apiError="apiError"></APIErrorDetails>
-                </template>
-              </CustomBanner>
-            </form>
-          </template>
-        </CustomWidget>
+        <ProfileFormWidget></ProfileFormWidget>
       </div>
       <div class="col-lg-8 col-xl-8 col-12 flex">
         <CustomWidget title="Your system stats" caption="Small resume of your platform usage" icon="analytics">
@@ -70,160 +25,14 @@
 </template>
 
 <script setup>
-import { ref, computed, onMounted, nextTick } from "vue";
-import { useQuasar } from "quasar";
-import { useI18n } from 'vue-i18n'
-import { useInitialStateStore } from "stores/initialState";
 
-import { api } from 'boot/axios'
+import { useI18n } from 'vue-i18n'
 
 import { default as CustomWidget } from "src/components/CustomWidget.vue";
-import { default as CustomInputPassword } from "src/components/CustomInputPassword.vue";
-import { default as CustomBanner } from "src/components/CustomBanner.vue";
+import { default as ProfileFormWidget } from "src/components/ProfileFormWidget.vue";
 import { default as SystemStats } from "src/components/SystemStats.vue";
-import { default as APIErrorDetails } from "components/APIErrorDetails.vue";
 
 const { t } = useI18n();
-
-const $q = useQuasar();
-const initialState = useInitialStateStore();
-
-const loading = ref(false);
-const loadingError = ref(false);
-
-const email = ref(null);
-const password = ref(null);
-
-const emailRef = ref(null);
-const passwordRef = ref(null);
-
-const visiblePassword = ref(false);
-
-const profileUpdated = ref(false);
-
-const bannerText = computed(() => {
-  if (profileUpdated.value) {
-    return ("Profile has been successfully updated");
-  } else if (loadingError.value) {
-    return ("Error loading data")
-  } else {
-    return (null);
-  }
-});
-const apiError = ref(null);
-
-const remoteValidation = ref({
-  password: {
-    hasErrors: false,
-    message: null
-  }
-});
-
-function onGetProfile() {
-  loading.value = true;
-  loadingError.value = false;
-  email.value = null;
-  password.value = null;
-  api.user.getProfile()
-    .then((success) => {
-      loading.value = false;
-      email.value = success.data.data.email;
-    })
-    .catch((error) => {
-      loading.value = false;
-      loadingError.value = true;
-      apiError.value = error.customAPIErrorDetails;
-    });
-}
-
-function onResetForm() {
-  remoteValidation.value.password.hasErrors = false;
-  remoteValidation.value.password.message = null;
-  passwordRef.value.resetValidation();
-}
-
-function onValidateForm() {
-  onResetForm();
-  passwordRef.value.validate();
-  nextTick(() => {
-    if (!(passwordRef.value.hasError)) {
-      onSubmitForm();
-    }
-  });
-}
-
-function onSubmitForm() {
-  loading.value = true;
-  loadingError.value = false;
-  profileUpdated.value = false;
-  api.user
-    .updateProfile(email.value, password.value)
-    .then((success) => {
-      password.value = null;
-      profileUpdated.value = true;
-      loading.value = false;
-      nextTick(() => {
-        passwordRef.value.focus();
-      });
-    })
-    .catch((error) => {
-      loading.value = false;
-      switch (error.response.status) {
-        case 400:
-          if (
-            error.response.data.invalidOrMissingParams.find(function (e) {
-              return e === "email";
-            })
-          ) {
-            $q.notify({
-              type: "negative",
-              message: t("API Error: missing email param"),
-            });
-            emailRef.value.focus();
-          } else if (
-            error.response.data.invalidOrMissingParams.find(function (e) {
-              return e === "password";
-            })
-          ) {
-            $q.notify({
-              type: "negative",
-              message: t("API Error: missing password param"),
-            });
-            passwordRef.value.focus();
-          } else {
-            $q.notify({
-              type: "negative",
-              message: t("API Error: invalid/missing param"),
-            });
-          }
-          break;
-        case 404:
-          remoteValidation.value.email.hasErrors = true;
-          remoteValidation.value.email.message = "Email not registered";
-          nextTick(() => {
-            emailRef.value.focus();
-          });
-          break;
-        default:
-          /*
-          $q.notify({
-            type: "negative",
-            message: t("API Error: fatal error"),
-            caption: t("API Error: fatal error details", { status: error.response.status, statusText: error.response.statusText })
-          });
-          showErrorBanner("Error while updating profile");
-          */
-          //loading.value = false;
-          loadingError.value = true;
-          apiError.value = error.customAPIErrorDetails;
-          break;
-      }
-    });
-}
-
-onMounted(() => {
-  onGetProfile();
-});
 
 </script>
 
@@ -241,7 +50,6 @@ onMounted(() => {
   background-position: bottom;
   filter: grayscale(100%);
   border-radius: 16px;
-
 }
 
 .my-profile-header-top-right-icon {
