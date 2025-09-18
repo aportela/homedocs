361d165d0c30a1c83f691be441b5590726080d77
alex <766f6964+github@gmail.com>
2025-09-18 16:27:08 +0200

    icon size changed, added details slot

--- quasar-frontend/src/components/CustomBanner.vue
+++ quasar-frontend/src/components/CustomBanner.vue
@@ -2,10 +2,11 @@
   <q-banner dense rounded class="q-my-md theme-default-q-banner"
     :class="{ 'theme-default-q-banner-success': success, 'theme-default-q-banner-error': error }">
     <template v-slot:avatar v-if="success || error">
-      <q-icon v-if="success" name="check" class="text-green" />
-      <q-icon v-else-if="error" name="error" class="text-red" />
+      <q-icon size="sm" v-if="success" name="check" class="text-green" />
+      <q-icon size="sm" v-else-if="error" name="error" class="text-red" />
     </template>
     {{ t(text) }}
+    <slot name="details"></slot>
   </q-banner>
 </template>
 
