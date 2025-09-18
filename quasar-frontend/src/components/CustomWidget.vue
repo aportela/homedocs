8a70e7d9733be5854e0e40640e9ce42973ef5598
alex <766f6964+github@gmail.com>
2025-09-18 14:41:22 +0200

    new custom widget container

---
+++ quasar-frontend/src/components/CustomWidget.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-card>
+    <q-item class="theme-default-q-card-section-header">
+      <q-item-section avatar>
+        <q-icon :name="icon"></q-icon>
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ t(title) }}</q-item-label>
+        <q-item-label caption>{{ t(caption) }}</q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-separator />
+    <q-card-section>
+      <slot name="content"></slot>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup>
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+
+const props = defineProps({
+  title: String,
+  caption: String,
+  icon: String
+});
+
+</script>
