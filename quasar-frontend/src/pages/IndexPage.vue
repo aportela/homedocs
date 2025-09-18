c9c29e4f5eb213b6ea100d6ddf41efb26ad08478
alex <766f6964+github@gmail.com>
2025-09-18 16:13:41 +0200

    flex removed for prevent full height on widgets

--- quasar-frontend/src/pages/IndexPage.vue
+++ quasar-frontend/src/pages/IndexPage.vue
@@ -1,19 +1,25 @@
 <template>
   <q-page>
     <div class="row q-col-gutter-sm">
-      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 flex">
-        <RecentDocuments></RecentDocuments>
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <RecentDocuments :expanded="expanded"></RecentDocuments>
       </div>
-      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 flex">
-        <TagCloud></TagCloud>
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <TagCloud :expanded="expanded"></TagCloud>
       </div>
     </div>
   </q-page>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import { useQuasar } from "quasar";
 
 import { default as RecentDocuments } from "components/RecentDocuments.vue";
 import { default as TagCloud } from "components/TagCloud.vue";
+
+const $q = useQuasar();
+
+const expanded = ref(!$q.screen.lt.md);
 
 </script>
