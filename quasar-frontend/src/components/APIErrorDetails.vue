5fc8df7457824aed69c5a152fd2e5c204bc0765c
alex <766f6964+github@gmail.com>
2025-09-18 23:38:53 +0200

    added required property

--- quasar-frontend/src/components/APIErrorDetails.vue
+++ quasar-frontend/src/components/APIErrorDetails.vue
@@ -1,9 +1,9 @@
 <template>
   <div v-if="apiError">
-    <p class="q-my-none">URL: {{ apiError.url }}</p>
-    <p class="q-my-none">Method: {{ apiError.method }}</p>
-    <p class="q-my-none">HTTP code: {{ apiError.httpCode }}</p>
-    <p class="q-my-none">Text: {{ apiError.httpStatus }}</p>
+    <p class="q-my-none" v-if="apiError.url">URL: {{ apiError.url }}</p>
+    <p class="q-my-none" v-if="apiError.method">Method: {{ apiError.method }}</p>
+    <p class="q-my-none" v-if="apiError.httpCode">HTTP code: {{ apiError.httpCode }}</p>
+    <p class="q-my-none" v-if="apiError.httpStatus">Text: {{ apiError.httpStatus }}</p>
     <p class="q-my-none" v-if="apiError.params.query">Query params: {{ apiError.params.query }}</p>
     <p class="q-my-none" v-if="apiError.params.data">Body params: {{ apiError.params.data }}</p>
   </div>
@@ -12,6 +12,10 @@
 <script setup>
 
 defineProps({
-  apiError: Object
+  apiError: {
+    type: Object,
+    required: true
+  }
 });
+
 </script>
