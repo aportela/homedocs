532093952b26fb25606d62310757a6ba34431f41
alex <766f6964+github@gmail.com>
2025-09-18 16:53:55 +0200

    fixed pinia readonly properties

--- quasar-frontend/src/stores/initialState.js
+++ quasar-frontend/src/stores/initialState.js
@@ -4,37 +4,40 @@ import { api } from "boot/axios";
 export const useInitialStateStore = defineStore("initialState", {
   state: () => ({
     initialState: {
+      environment: 'production',
       allowSignUp: false,
-      maxUploadFileSize: 1,
+      maxUploadFileSize: 0,
       session: {
         id: null,
         email: null,
-      },
-    },
+      }
+    }
   }),
   getters: {
     isSignUpAllowed: (state) => state.initialState.allowSignUp,
     maxUploadFileSize: (state) => state.initialState.maxUploadFileSize,
     session: (state) => state.initialState.session,
+    isDevEnvironment: (state) => state.initialState.environment == 'development',
   },
   actions: {
     async load() {
       try {
         const success = await api.common.initialState();
         this.initialState.allowSignUp = success.data.initialState.allowSignUp;
-        this.initialState.maxUploadFileSize =
-          success.data.initialState.maxUploadFileSize;
-        this.session.id = success.data.initialState.session.id;
-        this.session.email = success.data.initialState.session.email;
+        this.initialState.maxUploadFileSize = success.data.initialState.maxUploadFileSize || 20;
+        this.initialState.session.id = success.data.initialState.session.id;
+        this.initialState.session.email = success.data.initialState.session.email;
+        this.initialState.environment = success.data.initialState.environment == "production" ? 'production' : 'development';
       } catch (error) {
-        console.error(error.response);
+        console.error(error);
       }
     },
     set(initialState) {
       this.initialState.allowSignUp = initialState.allowSignUp;
       this.initialState.maxUploadFileSize = initialState.maxUploadFileSize;
-      this.session.id = initialState.session.id;
-      this.session.email = initialState.session.email;
+      this.initialState.session.id = initialState.session.id;
+      this.initialState.session.email = initialState.session.email;
+      this.initialState.environment = initialState.environment == "production" ? 'production' : 'development';
     },
   },
 });
