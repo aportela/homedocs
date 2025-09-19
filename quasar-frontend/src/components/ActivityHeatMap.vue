0af09d0880b99dcde189c6139eb95b7972c6e118
alex <766f6964+github@gmail.com>
2025-09-19 12:02:36 +0200

    fixed locale/theme changes, added showbuttons prop, code cleanups

--- quasar-frontend/src/components/ActivityHeatMap.vue
+++ quasar-frontend/src/components/ActivityHeatMap.vue
@@ -1,18 +1,22 @@
 <template>
-  <div id="cal-heatmap"></div>
-  <!--
-  <q-btn-group flat dense class="q-mt-sm">
-    <q-btn icon="arrow_left" @click.prevent="cal.previous()">Previous</q-btn>
-    <q-btn icon-right="arrow_right" @click.prevent="cal.next()">Next</q-btn>
-  </q-btn-group>
-  -->
+  <div id="cal-heatmap-container" class="calContainerRef">
+    <div id="cal-heatmap"></div>
+    <div class="q-mb-md q-gutter-sm q-py-sm" v-if="showNavigationButtons">
+      <q-btn icon="arrow_left" :disabled="leftButtonDisabled" size="md" color="primary"
+        @click.prevent="onLeftButtonClicked">Previous</q-btn>
+      <q-btn icon-right="arrow_right" :disabled="rightButtonDisabled" size="md" color="primary"
+        @click.prevent="onRightButtonClicked">Next</q-btn>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, computed, onMounted, watch } from "vue";
 import { useRouter } from "vue-router";
 import { useI18n } from "vue-i18n";
+import { Dark } from "quasar";
 
+import { i18n } from "boot/i18n";
 import { api } from "boot/axios";
 
 import CalHeatmap from "cal-heatmap";
@@ -24,12 +28,99 @@ const router = useRouter();
 
 const { t } = useI18n();
 
-const activityHeatMapData = ref([]);
+const props = defineProps({
+  showNavigationButtons: {
+    type: Boolean,
+    required: false,
+    default: true
+  }
+});
 
-const cal = new CalHeatmap();
-
-const loadingError = ref(false);
 const loading = ref(false);
+const error = ref(false);
+
+const leftButtonDisabled = ref(false);
+const rightButtonDisabled = ref(false);
+
+const currentLocale = computed(() => i18n.global.locale.value.substring(0, 2));
+
+const calDefaultOptions = {
+  itemSelector: '#cal-heatmap',
+};
+
+const calOptions = ref({
+  date: {
+    locale: currentLocale.value,
+    start: new Date(new Date().setFullYear(new Date().getFullYear() - 1)),
+    end: new Date(),
+    max: new Date(),
+  },
+  range: 13, // visibility: 12 months
+  scale: {
+    color: {
+      scheme: 'greens',
+      type: 'threshold',
+      domain: null,
+    },
+  },
+  domain: {
+    type: 'month',
+    gutter: 4,
+    label: { text: 'MMM', textAlign: 'start', position: 'top' },
+  },
+  subDomain: { type: 'ghDay', radius: 2, width: 11, height: 11, gutter: 4 },
+  theme: Dark.isActive ? "dark" : "light"
+});
+
+const calDefaultPlugins = [
+  [
+    Tooltip,
+    {
+      text: function (date, value, dayjsDate) {
+        return (value
+          ? `${value} ${t(" change/s on date ")} ${dayjsDate.format('dddd, MMMM D, YYYY')}`
+          : `${t("No activity on date ")} ${dayjsDate.format('dddd, MMMM D, YYYY')}`);
+      },
+    },
+  ],
+  [
+    CalendarLabel,
+    {
+      width: 30,
+      textAlign: 'start',
+      text: () => dayjs.weekdaysShort().map((d, i) => (i % 2 == 0 ? '' : d)),
+      padding: [25, 0, 0, 0],
+    },
+  ],
+];
+
+let cal = new CalHeatmap(calDefaultOptions);
+
+// UGLY-HACK
+// official dynamic cal-heatmap theme toggle is not supported (https://cal-heatmap.com/docs/options/theme)
+watch(() => Dark.isActive, val => {
+  cal.destroy();
+  cal = new CalHeatmap(calDefaultOptions);
+  onCalRefresh();
+});
+
+// UGLY-HACK
+// day/month labels not updated on i18n changes
+watch(() => currentLocale.value, val => {
+  cal.destroy();
+  cal = new CalHeatmap(calDefaultOptions);
+  onCalRefresh();
+});
+
+/*
+// TODO: change cal-heatmap-container max-width style for avoiding scrollbar appearing on button navigation
+cal.on('resize', (newW, newH, oldW, oldH) => {
+  console.log(
+    `Calendar has been resized from ${oldW}x${oldH} to ${newW}x${newH}`
+  );
+
+});
+*/
 
 cal.on('click', (event, timestamp, value) => {
   if (value > 0) {
@@ -42,83 +133,68 @@ cal.on('click', (event, timestamp, value) => {
   }
 });
 
-function refresh() {
-  activityHeatMapData.value = [];
+cal.on('minDateReached', () => {
+  leftButtonDisabled.value = true;
+});
+
+cal.on('minDateNotReached', () => {
+  leftButtonDisabled.value = false;
+});
+
+cal.on('maxDateReached', () => {
+  rightButtonDisabled.value = true;
+});
+
+cal.on('maxDateNotReached', () => {
+  rightButtonDisabled.value = false;
+});
+
+const onCalRefresh = (data, scaleDomain) => {
+  if (data) {
+    calOptions.value.data = data;
+  }
+  if (scaleDomain) {
+    calOptions.value.scale.color.domain = scaleDomain;
+  }
+  calOptions.value.date.locale = currentLocale.value;
+  calOptions.value.theme = Dark.isActive ? "dark" : "light";
+  cal.paint(
+    calOptions.value,
+    calDefaultPlugins
+  );
+};
+
+const onLeftButtonClicked = () => {
+  cal.previous();
+};
+
+const onRightButtonClicked = () => {
+  cal.next();
+};
+
+
+const refresh = () => {
   loading.value = true;
-  loadingError.value = false;
+  error.value = false;
   api.stats.getActivityHeatMapData()
     .then((success) => {
-      activityHeatMapData.value = success.data.heatmap;
-      const maxValue = activityHeatMapData.value.reduce((max, obj) => (obj.count > max ? obj.count : max), -Infinity)
-      const counts = activityHeatMapData.value.map(d => d.count);
-      let ddomain = [...new Set(counts)];
-      ddomain.unshift(0);
-      ddomain.sort(function (a, b) { return a - b; });
+      const counts = success.data.heatmap.map(d => d.count);
       //const min = Math.min(...counts);
       //const max = Math.max(...counts);
-      cal.paint(
-        {
-          data: {
-            source: activityHeatMapData.value,
-            x: "date",
-            y: "count",
-            type: "json",
-          },
-          date: {
-            start: new Date(new Date().setFullYear(new Date().getFullYear() - 1)),
-            end: new Date(),
-            max: new Date(),
-          },
-          range: 13,
-          scale: {
-            color: {
-              scheme: 'greens',
-              type: 'threshold',
-              //domain: [0, min, 2, 5, 10, max], // TODO: create dynamic domain from values
-              domain: ddomain,
-            },
-          },
-          domain: {
-            type: 'month',
-            gutter: 4,
-            label: { text: 'MMM', textAlign: 'start', position: 'top' },
-          },
-          subDomain: { type: 'ghDay', radius: 2, width: 11, height: 11, gutter: 4 },
-          itemSelector: '#cal-heatmap',
-        },
-        [
-          [
-            Tooltip,
-            {
-              text: function (date, value, dayjsDate) {
-                return (value
-                  ? `${value} ${t(" change/s on date ")} ${dayjsDate.format('dddd, MMMM D, YYYY')}`
-                  : `${t("No activity on date ")} ${dayjsDate.format('dddd, MMMM D, YYYY')}`);
-              },
-            },
-          ],
-          [
-            CalendarLabel,
-            {
-              width: 30,
-              textAlign: 'start',
-              text: () => dayjs.weekdaysShort().map((d, i) => (i % 2 == 0 ? '' : d)),
-              padding: [25, 0, 0, 0],
-            },
-          ],
-        ]
-      );
-
+      let scaleDomain = [...new Set(counts)];
+      scaleDomain.unshift(0);
+      scaleDomain.sort(function (a, b) { return a - b; });
+      onCalRefresh({
+        source: success.data.heatmap,
+        x: "date",
+        y: "count",
+        type: "json",
+      }, scaleDomain);
       loading.value = false;
     })
     .catch((error) => {
       loading.value = false;
-      loadingError.value = true;
-      $q.notify({
-        type: "negative",
-        message: t("API Error: fatal error"),
-        caption: t("API Error: fatal error details", { status: error.response.status, statusText: error.response.statusText })
-      });
+      error.value = true;
     });
 }
 
@@ -129,8 +205,13 @@ onMounted(() => {
 </script>
 
 <style scoped>
+#cal-heatmap-container {
+  max-width: 882px;
+  overflow-x: auto;
+}
+
 #cal-heatmap {
-  width: 810px;
+  width: 870px;
   height: 136px;
 }
 </style>
