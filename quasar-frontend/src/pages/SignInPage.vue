a473edc2c7fb0ed96ad688d841698fd73b79401e
alex <766f6964+github@gmail.com>
2025-09-21 00:35:47 +0200

    use new custom error banner & profile reactive object

--- quasar-frontend/src/pages/SignInPage.vue
+++ quasar-frontend/src/pages/SignInPage.vue
@@ -9,40 +9,38 @@
           </q-avatar>
           <h4 class="q-mt-sm q-mb-md text-h4 text-weight-bolder">{{
             t(!!savedEmail ? "Glad to see you again!" : "Welcome aboard!")
-            }}</h4>
+          }}</h4>
           <div class="text-color-secondary">{{
             t(!!savedEmail ? "Let's get back to organizing." : "Let's start organizing.")
-          }}
+            }}
           </div>
         </q-card-section>
         <q-card-section>
-          <q-input dense outlined ref="emailRef" v-model="email" type="email" name="email" :label="t('Email')"
-            :disable="loading" :autofocus="!savedEmail" :rules="formUtils.requiredFieldRules" lazy-rules
+          <q-input dense outlined ref="emailRef" v-model="profile.email" type="email" name="email" :label="t('Email')"
+            :disable="state.loading" :autofocus="!savedEmail" :rules="formUtils.requiredFieldRules" lazy-rules
             :error="validator.email.hasErrors"
             :errorMessage="validator.email.message ? t(validator.email.message) : ''">
             <template v-slot:prepend>
               <q-icon name="alternate_email" />
             </template>
           </q-input>
-          <CustomInputPassword dense outlined ref="passwordRef" class="q-mt-md" v-model="password" name="password"
-            :label="t('Password')" :disable="loading" :autofocus="!!savedEmail" :rules="formUtils.requiredFieldRules"
-            lazy-rules :error="validator.password.hasErrors"
+          <CustomInputPassword dense outlined ref="passwordRef" class="q-mt-md" v-model="profile.password"
+            name="password" :label="t('Password')" :disable="state.loading" :autofocus="!!savedEmail"
+            :rules="formUtils.requiredFieldRules" lazy-rules :error="validator.password.hasErrors"
             :errorMessage="validator.password.message ? t(validator.password.message) : ''">
           </CustomInputPassword>
         </q-card-section>
         <q-card-section>
           <q-btn color="primary" size="md" :label="$t('Sign in')" no-caps class="full-width" icon="account_circle"
-            :disable="loading || (!(email && password))" :loading="loading" type="submit">
+            :disable="state.loading || (!(profile.email && profile.password))" :loading="state.loading" type="submit">
             <template v-slot:loading>
               <q-spinner-hourglass class="on-left" />
               {{ t('Sign in') }}
             </template>
           </q-btn>
-          <CustomBanner v-if="error && errorMessage" :text="errorMessage" :error="true">
-            <template v-slot:details v-if="error && initialState.isDevEnvironment && apiError">
-              <APIErrorDetails class="q-mt-md" :apiError="apiError"></APIErrorDetails>
-            </template>
-          </CustomBanner>
+          <CustomErrorBanner v-if="state.loadingError && state.errorMessage" :text="state.errorMessage"
+            :apiError="state.apiError">
+          </CustomErrorBanner>
         </q-card-section>
         <q-card-section class="text-center q-pt-none" v-if="signUpAllowed">
           <div>
@@ -66,7 +64,7 @@
 
 <script setup>
 
-import { ref, nextTick, computed } from "vue";
+import { ref, reactive, nextTick, computed } from "vue";
 import { LocalStorage } from "quasar";
 import { useRouter } from "vue-router";
 import { useI18n } from 'vue-i18n'
@@ -80,8 +78,8 @@ import { default as SwitchLanguageButton } from "components/SwitchLanguageButton
 import { default as GitHubButton } from "components/GitHubButton.vue"
 import { GITHUB_PROJECT_URL } from "src/constants"
 import { default as CustomInputPassword } from "src/components/CustomInputPassword.vue";
-import { default as CustomBanner } from "src/components/CustomBanner.vue";
-import { default as APIErrorDetails } from "components/APIErrorDetails.vue";
+import { default as CustomErrorBanner } from "components/CustomErrorBanner.vue";
+
 
 const { t } = useI18n();
 
@@ -93,12 +91,14 @@ const initialState = useInitialStateStore();
 
 const signUpAllowed = computed(() => initialState.isSignUpAllowed === true);
 
-const loading = ref(false);
-const error = ref(false);
-const errorMessage = ref(null);
-const apiError = ref(null);
+const state = reactive({
+  loading: false,
+  loadingError: false,
+  errorMessage: null,
+  apiError: null
+});
 
-const validator = ref({
+const validator = reactive({
   email: {
     hasErrors: false,
     message: null
@@ -111,49 +111,50 @@ const validator = ref({
 
 const savedEmail = LocalStorage.getItem("email");
 
-const email = ref(savedEmail || null);
+const profile = reactive(
+  {
+    email: savedEmail || null,
+    password: null
+  }
+);
+
 const emailRef = ref(null);
-const password = ref(null);
 const passwordRef = ref(null);
 
 const onResetForm = () => {
-  validator.value.email.hasErrors = false;
-  validator.value.email.message = null;
-  validator.value.password.hasErrors = false;
-  validator.value.password.message = null;
-  emailRef.value.resetValidation();
-  passwordRef.value.resetValidation();
+  validator.email.hasErrors = false;
+  validator.email.message = null;
+  validator.password.hasErrors = false;
+  validator.password.message = null;
+  emailRef.value?.resetValidation();
+  passwordRef.value?.resetValidation();
 }
 
 const onValidateForm = () => {
   onResetForm();
-  emailRef.value.validate();
-  passwordRef.value.validate();
-  nextTick(() => {
-    if (!(emailRef.value.hasError || passwordRef.value.hasError)) {
-      onSubmitForm();
-    }
-  });
+  emailRef.value?.validate();
+  passwordRef.value?.validate();
+  if (!(emailRef.value?.hasError || passwordRef.value?.hasError)) {
+    onSubmitForm();
+  }
 }
 
 const onSubmitForm = () => {
-  loading.value = true;
-  error.value = false;
-  errorMessage.value = null;
-  apiError.value = null;
+  state.loading = true;
+  state.loadingError = false;
+  state.errorMessage = null;
+  state.apiError = null;
   api.user
-    .signIn(email.value, password.value)
+    .signIn(profile.email, profile.password)
     .then((successResponse) => {
-      LocalStorage.set("email", email.value);
-      loading.value = false;
+      LocalStorage.set("email", profile.email);
+      state.loading = false;
       router.push({
         name: "index",
       });
     })
     .catch((errorResponse) => {
-      loading.value = false;
-      error.value = true;
-      apiError.value = errorResponse.customAPIErrorDetails;
+      state.apiError = errorResponse.customAPIErrorDetails;
       switch (errorResponse.response.status) {
         case 400:
           if (
@@ -161,7 +162,8 @@ const onSubmitForm = () => {
               return e === "email";
             })
           ) {
-            errorMessage.value = "API Error: missing email param";
+            state.loadingError = true;
+            state.errorMessage = "API Error: missing email param";
             nextTick(() => {
               emailRef.value?.focus();
             });
@@ -170,35 +172,39 @@ const onSubmitForm = () => {
               return e === "password";
             })
           ) {
-            errorMessage.value = "API Error: missing password param";
+            state.loadingError = true;
+            state.errorMessage = "API Error: missing password param";
             nextTick(() => {
               passwordRef.value?.focus();
             });
           } else {
-            errorMessage.value = "API Error: invalid/missing param";
+            state.loadingError = true;
+            state.errorMessage = "API Error: invalid/missing param";
             nextTick(() => {
               emailRef.value?.focus();
             });
           }
           break;
         case 404:
-          validator.value.email.hasErrors = true;
-          validator.value.email.message = "Email not registered";
+          validator.email.hasErrors = true;
+          validator.email.message = "Email not registered";
           nextTick(() => {
             emailRef.value?.focus();
           });
           break;
         case 401:
-          validator.value.password.hasErrors = true;
-          validator.value.password.message = "Invalid password";
+          validator.password.hasErrors = true;
+          validator.password.message = "Invalid password";
           nextTick(() => {
             passwordRef.value?.focus();
           });
           break;
         default:
-          errorMessage.value = "API Error: fatal error";
+          state.loadingError = true;
+          state.errorMessage = "API Error: fatal error";
           break;
       }
+      state.loading = false;
     });
 }
 
