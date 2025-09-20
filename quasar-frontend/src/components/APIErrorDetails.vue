d8874e4c6db6b23f6351b5ccbbe0d8cd89962997
alex <766f6964+github@gmail.com>
2025-09-20 23:38:05 +0200

    hide separator if no params found

--- quasar-frontend/src/components/APIErrorDetails.vue
+++ quasar-frontend/src/components/APIErrorDetails.vue
@@ -5,7 +5,7 @@
       <dt v-if="apiError.url">URL: <strong>{{ apiError.url }}</strong></dt>
       <dt v-if="apiError.httpCode">HTTP code: <strong>{{ apiError.httpCode }} <span v-if="apiError.httpStatus">({{
         apiError.httpStatus }})</span></strong></dt>
-      <q-separator class="q-my-md"></q-separator>
+      <q-separator class="q-my-md" v-if="apiError.params.query || apiError.params.data"></q-separator>
       <dt v-if="apiError.params.query">Query params: <strong>{{ apiError.params.query }}</strong></dt>
       <dt v-if="apiError.params.data">Body params: <strong>{{ apiError.params.data }}</strong></dt>
     </dl>
