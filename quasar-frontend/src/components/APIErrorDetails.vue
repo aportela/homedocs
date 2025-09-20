9136734e5f68397d79204709cceb147ca1ba3f90
alex <766f6964+github@gmail.com>
2025-09-20 21:49:42 +0200

    changed status text

--- quasar-frontend/src/components/APIErrorDetails.vue
+++ quasar-frontend/src/components/APIErrorDetails.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <dl>
-      <dt v-if="apiError.url">URL: {{ apiError.url }}</dt>
-      <dt v-if="apiError.method">Method: {{ apiError.method }}</dt>
-      <dt v-if="apiError.httpCode">HTTP code: {{ apiError.httpCode }}</dt>
-      <dt v-if="apiError.httpStatus">Text: {{ apiError.httpStatus }}</dt>
-      <dt v-if="apiError.params.query">Query params: {{ apiError.params.query }}</dt>
-      <dt v-if="apiError.params.data">Body params: {{ apiError.params.data }}</dt>
+      <dt v-if="apiError.method">Method: <strong>{{ apiError.method }}</strong></dt>
+      <dt v-if="apiError.url">URL: <strong>{{ apiError.url }}</strong></dt>
+      <dt v-if="apiError.httpCode">HTTP code: <strong>{{ apiError.httpCode }} <span v-if="apiError.httpStatus">({{
+        apiError.httpStatus }})</span></strong></dt>
+      <q-separator class="q-my-md"></q-separator>
+      <dt v-if="apiError.params.query">Query params: <strong>{{ apiError.params.query }}</strong></dt>
+      <dt v-if="apiError.params.data">Body params: <strong>{{ apiError.params.data }}</strong></dt>
     </dl>
   </div>
 </template>
