47cbafc66f122bea3903c3ba3e8b6cbdb93d2e59
alex <766f6964+github@gmail.com>
2025-09-23 10:56:36 +0200

    testing new hack for restore previous search results on history back

--- quasar-frontend/src/App.vue
+++ quasar-frontend/src/App.vue
@@ -5,6 +5,15 @@
 <script>
 import { defineComponent } from 'vue'
 
+
+/*
+window.onpopstate = function (event) {
+  if(event?.state?.current?.endsWith("advanced-search")) {
+  // TODO: UGLY HACK for checking if return to search page from history => relaunch previous search ?
+  }
+}
+*/
+
 export default defineComponent({
   name: 'App'
 })
