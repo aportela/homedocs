9df034247895410ff8228cbbd8b3d4e873aeece6
alex <766f6964+github@gmail.com>
2025-09-23 09:58:13 +0200

    added document title support, changed close button style, removed old code

--- quasar-frontend/src/components/NotesPreviewModal.vue
+++ quasar-frontend/src/components/NotesPreviewModal.vue
@@ -1,14 +1,15 @@
 <template>
   <q-dialog v-model="visible" @hide="emit('close')">
-    <q-card style="width: 700px; max-width: 80vw;">
+    <q-card style="width: 1280px; max-width: 80vw;">
       <q-card-section class="row items-center q-p-none">
-        <div class="text-h6">{{ t("Document notes") }}</div>
+        <div class="q-card-notes-dialog-header" v-if="title">{{ t("Document title") }}: <span>{{ title }}</span>
+        </div>
+        <div class="q-card-notes-dialog-header" v-else>{{ t("Document notes") }}</div>
         <q-space />
-        <q-btn icon="close" flat round dense v-close-popup />
+        <q-btn icon="close" flat round dense v-close-popup aria-label="Close modal" />
       </q-card-section>
       <q-card-section class="q-pt-none scroll" style="max-height: 50vh">
-        <div v-for="note in notes" :key="note.id"
-          class="cursor-pointer q-pa-sm q-mb-md relative-position white-space-pre-line"
+        <div v-for="note in notes" :key="note.id" class="q-pa-sm q-mb-md relative-position white-space-pre-line"
           style="border: 1px solid rgba(0, 0, 0, 0.12); border-radius: 4px;">
           <div style="font-size: 12px; color: rgba(0, 0, 0, 0.6); margin-left: 0px; margin-bottom: 4px;">
             {{ note.createdOn }} ({{ timeAgo(note.createdOnTimestamp * 1000) }})</div>
@@ -23,7 +24,7 @@
       </q-card-section>
       <q-card-section class="q-pt-none">
         <q-card-actions align="right">
-          <q-btn outline v-close-popup :label="t('Close')" icon="close" />
+          <q-btn color="primary" v-close-popup :label="t('Close')" icon="close" aria-label="Close modal" />
         </q-card-actions>
       </q-card-section>
     </q-card>
@@ -39,6 +40,11 @@ const { t } = useI18n();
 const { timeAgo } = useFormatDates();
 
 const props = defineProps({
+  title: {
+    type: String,
+    required: false,
+    default: ""
+  },
   notes: {
     type: Array,
     required: true
@@ -55,12 +61,6 @@ const props = defineProps({
   }
 });
 
-const currentIndex = ref(props.index + 1 || 1);
-const totalNotes = ref(props.notes ? props.notes.length : 0);
-if (currentIndex.value > totalNotes.value) {
-  currentIndex.value = 1;
-}
-
 const emit = defineEmits(['close']);
 
 const visible = ref(true);
@@ -68,6 +68,16 @@ const visible = ref(true);
 </script>
 
 <style scoped>
+.q-card-notes-dialog-header {
+  font-size: 1.2em;
+  font-weight: bold;
+}
+
+.q-card-notes-dialog-header span {
+  font-weight: normal;
+}
+
+
 .collapsed {
   overflow: hidden;
   -webkit-line-clamp: var(--max-lines);
