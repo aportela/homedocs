5755e084f973db2e8a23331e1fe2d7a97a27326d
alex <766f6964+github@gmail.com>
2025-09-24 12:13:36 +0200

    testing new re-auth dialog

---
+++ quasar-frontend/src/components/Dialogs/ReAuthDialog.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-dialog v-model="visible" @hide="onClose">
+    <q-card>
+      <q-card-section class="q-p-none">
+        <slot name="header"></slot>
+      </q-card-section>
+      <q-separator class="q-mb-md"></q-separator>
+      <q-card-section class="q-p-none">
+        <slot name="body"></slot>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useI18n } from "vue-i18n";
+
+
+const emit = defineEmits(['close', 'hide', 'success', 'error']);
+
+const visible = ref(true);
+
+const onClose = () => {
+  visible.value = false;
+  emit('close');
+}
+
+</script>
