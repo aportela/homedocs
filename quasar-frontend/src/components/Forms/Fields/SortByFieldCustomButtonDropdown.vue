3ad294d9b754d8a56e8323b43ea7848ee21846d8
alex <766f6964+github@gmail.com>
2025-09-25 12:11:55 +0200

    new custom button dropdown for sort by field (on small screens)

---
+++ quasar-frontend/src/components/Forms/Fields/SortByFieldCustomButtonDropdown.vue
@@ -0,0 +1,64 @@
+<template>
+  <q-btn-dropdown v-bind="attrs" :label="currentLabel">
+    <q-list>
+      <q-item :clickable="completeOptions.field != current.field && completeOptions.order != current.order"
+        v-close-popup v-for="(option, index) in completeOptions" :key="index" @click="onClick(option)">
+        <q-item-section avatar>
+          <q-icon :name="option.order == 'ASC' ? 'keyboard_double_arrow_up' : 'keyboard_double_arrow_down'" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ t(option.label) }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-btn-dropdown>
+</template>
+
+<script setup>
+
+import { computed, reactive, useAttrs } from "vue";
+import { useI18n } from "vue-i18n";
+
+const attrs = useAttrs();
+
+const { t } = useI18n();
+
+const emit = defineEmits(['change'])
+
+const props = defineProps({
+  options: {
+    type: Array,
+    required: true
+  },
+  current: {
+    type: Object,
+    required: true
+  }
+});
+
+const currentLabel = computed(() => {
+  return (t("Order by: none"))
+});
+
+const onClick = (option) => {
+  console.log(option);
+  emit("change", { field: option.field, order: option.order })
+};
+
+const completeOptions = reactive([]);
+
+props.options?.forEach((option) => {
+  completeOptions.push({
+    field: option.field,
+    label: option.label,
+    order: "ASC"
+  });
+  completeOptions.push({
+    field: option.field,
+    label: option.label,
+    order: "DESC"
+  })
+});
+
+
+</script>
