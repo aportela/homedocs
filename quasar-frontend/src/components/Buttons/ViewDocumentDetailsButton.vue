a4be44104a122c9d8052baa7934e6b292d9e00e0
alex <766f6964+github@gmail.com>
2025-09-25 22:17:52 +0200

    use native class attribute

--- quasar-frontend/src/components/Buttons/ViewDocumentDetailsButton.vue
+++ quasar-frontend/src/components/Buttons/ViewDocumentDetailsButton.vue
@@ -1,6 +1,5 @@
 <template>
-  <q-chip class="full-width theme-default-q-chip shadow-1" :class="qChipClass" :clickable="count > 0 && !disable"
-    @click.stop.prevent="onClick">
+  <q-chip class="theme-default-q-chip shadow-1" :clickable="count > 0 && !disable" @click.stop.prevent="onClick">
     <q-avatar class="theme-default-q-avatar" :class="{ 'text-white bg-blue-6': count > 0 }">{{ count }}</q-avatar>
     {{ t(label, { count: count }) }}
     <q-tooltip v-if="toolTip && count > 0">{{ t(toolTip) }}</q-tooltip>
@@ -33,10 +32,6 @@ const props = defineProps({
   toolTip: {
     type: String,
     required: false,
-  },
-  qChipClass: {
-    type: String,
-    required: false
   }
 });
 
