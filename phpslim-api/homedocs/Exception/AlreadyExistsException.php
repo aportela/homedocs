da4682c0ac71f54b91110f861045a97fc3fa218a
alex <766f6964+github@gmail.com>
2025-09-26 23:58:09 +0200

    warning fixed

--- phpslim-api/homedocs/Exception/AlreadyExistsException.php
+++ phpslim-api/homedocs/Exception/AlreadyExistsException.php
@@ -9,7 +9,7 @@ namespace HomeDocs\Exception;
  */
 class AlreadyExistsException extends \Exception
 {
-    public function __construct(string $message = "", int $code = 0, \Exception $previous = null)
+    public function __construct(string $message = "", int $code = 0, ?\Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
