d56ede09f3b7fd05dff7817250fcf89f0f6e5bd7
alex <766f6964+github@gmail.com>
2025-09-27 01:24:49 +0200

    added properties & constructor

--- phpslim-api/homedocs/DocumentHistoryOperation.php
+++ phpslim-api/homedocs/DocumentHistoryOperation.php
@@ -8,4 +8,13 @@ class DocumentHistoryOperation
 {
     const OPERATION_ADD_DOCUMENT = 1;
     const OPERATION_UPDATE_DOCUMENT = 2;
+
+    public ?int $operationTimestamp;
+    public ?int $operationType;
+
+    public function __construct(?int $operationTimestamp = null, ?int $operationType = null, ?string $name = null, ?int $size = null, ?string $hash = null, ?int $createdOnTimestamp = null)
+    {
+        $this->operationTimestamp = $operationTimestamp;
+        $this->operationType = $operationType;
+    }
 }
