704e2d6312467df7af65f3170bfab582b02630c7
alex <766f6964+github@gmail.com>
2025-09-26 10:44:29 +0200

    added reauth form inside this dialog

--- quasar-frontend/src/components/Dialogs/ReAuthDialog.vue
+++ quasar-frontend/src/components/Dialogs/ReAuthDialog.vue
@@ -1,12 +1,20 @@
 <template>
-  <q-dialog v-model="visible" persistent @hide="onClose">
-    <q-card style="width: 512px; max-width: 80vw;">
+  <q-dialog v-model="dialogModel" persistent @hide="onHide">
+    <q-card class="q-card-reauth-dialog">
       <q-card-section class="q-p-none">
-        <slot name="header"></slot>
+        <div class="q-card-notes-dialog-header max-width-90">
+          {{ t("Session lost... re-auth required") }}
+        </div>
       </q-card-section>
       <q-separator class="q-mb-md"></q-separator>
       <q-card-section class="q-p-none">
-        <slot name="body"></slot>
+        <SignInForm :show-extra-bottom="false" @success="onSuccess">
+          <template v-slot:slogan>
+            <h4 class="q-mt-sm q-mb-md text-h4 text-weight-bolder">{{ t("Oooops") }}</h4>
+            <div class="text-color-secondary">
+              {{ t("Please enter again your credentials") }}</div>
+          </template>
+        </SignInForm>
       </q-card-section>
     </q-card>
   </q-dialog>
@@ -14,14 +22,31 @@
 
 <script setup>
 import { ref } from "vue";
+import { useI18n } from "vue-i18n";
 
-const emit = defineEmits(['close', 'hide', 'success', 'error']);
+import { default as SignInForm } from "src/components/Forms/SignInForm.vue"
 
-const visible = ref(true);
+const { t } = useI18n();
 
-const onClose = () => {
-  visible.value = false;
+const emit = defineEmits(['close', 'success']);
+
+const dialogModel = ref(true);
+
+const onHide = () => {
+  dialogModel.value = false;
   emit('close');
 }
 
+const onSuccess = () => {
+  dialogModel.value = false;
+  emit('success');
+};
+
 </script>
+
+<style lang="css" scoped>
+.q-card-reauth-dialog {
+  width: 512px;
+  max-width: 80vw;
+}
+</style>
