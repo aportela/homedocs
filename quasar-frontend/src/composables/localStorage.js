b52a227e3c045d677d5f3300c7504b1a78b58f9e
alex <766f6964+github@gmail.com>
2025-09-30 14:57:41 +0200

    new local storage composable

---
+++ quasar-frontend/src/composables/localStorage.js
@@ -0,0 +1,33 @@
+import { LocalStorage } from "quasar";
+
+export function useLocalStorage() {
+
+  const get = (key, defaultValue = null) => {
+    try {
+      const savedValue = LocalStorage.getItem(key);
+      return savedValue === null ? defaultValue : savedValue;
+    } catch (error) {
+      console.error("Error accessing localStorage:", error);
+      return defaultValue;
+    }
+  };
+
+  const set = (key, value) => {
+    try {
+      LocalStorage.setItem(key, value);
+    } catch (error) {
+      console.error("Error accessing localStorage:", error);
+    }
+  };
+
+  const alwaysOpenUploadDialog = {
+    get() {
+      return get("alwaysOpenUploadDialog", true);
+    },
+    set(value) {
+      set("alwaysOpenUploadDialog", !!value);
+    }
+  };
+
+  return { alwaysOpenUploadDialog };
+}
