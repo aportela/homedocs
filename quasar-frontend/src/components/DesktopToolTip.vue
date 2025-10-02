7f2cf9f9118c16ca58c0125d4538670cbae50b15
alex <766f6964+github@gmail.com>
2025-10-03 01:15:04 +0200

    new tooltip (desktop only) component

---
+++ quasar-frontend/src/components/DesktopToolTip.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-tooltip v-if="isDesktop" :delay="delay" :anchor="anchor" :self="self">
+    <slot></slot>
+  </q-tooltip>
+</template>
+
+<script setup>
+
+import { computed } from "vue";
+import { useQuasar } from "quasar";
+
+const $q = useQuasar()
+
+const isDesktop = computed(() => $q.screen.gt.sm);
+
+const props = defineProps({
+  delay: {
+    type: Number,
+    required: false,
+    default: 0,
+    validator(value) {
+      if (value < 0) {
+        console.warn('Invalid (negative) delay value');
+        return false
+      }
+      return true
+    },
+  },
+  anchor: {
+    type: String,
+    required: false,
+    default: 'top middle',
+  },
+  self: {
+    type: String,
+    required: false,
+    default: 'bottom middle',
+  },
+})
+
+</script>
