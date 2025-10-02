989fc62964328c9502c90f05fba620c00e416568
alex <766f6964+github@gmail.com>
2025-10-02 23:09:08 +0200

    new isDisabled computed property, better error control

--- quasar-frontend/src/components/Forms/DocumentDetailsAttachments.vue
+++ quasar-frontend/src/components/Forms/DocumentDetailsAttachments.vue
@@ -2,16 +2,19 @@
   <q-list class="bg-transparent q-pa-sm">
     <q-item class="transparent-background text-color-primary q-pa-none">
       <q-item-section v-show="hasAttachments">
-        <q-input type="search" icon="search" outlined dense clearable :disable="disable || !hasAttachments"
+        <q-input type="search" icon="search" outlined dense clearable :disable="isDisabled || !hasAttachments"
           v-model.trim="searchText" @update:model-value="onSearchTextChanged" :label="t('Filter by text on file name')"
           :placeholder="t('type text search condition')"></q-input>
       </q-item-section>
       <q-item-section side>
         <q-btn size="md" :label="t('Add attachment')" icon="add" class="bg-blue text-white full-width"
-          :disable="disable" @click.stop="onAddAttachment"></q-btn>
+          :disable="isDisabled" @click.stop="onAddAttachment"></q-btn>
       </q-item-section>
     </q-item>
     <q-separator class="q-my-md" />
+    <CustomErrorBanner v-if="state.loadingError && state.errorMessage" :text="state.errorMessage"
+      :apiError="state.apiError" class="q-mt-lg">
+    </CustomErrorBanner>
     <div v-if="hasAttachments" class="q-list-attachments-container scroll">
       <div v-for="attachment, attachmentIndex in attachments" :key="attachment.id">
         <q-item class="q-pa-xs bg-transparent" v-show="!hiddenIds.includes(attachment.id)" clickable
@@ -29,15 +32,15 @@
           </q-item-section>
           <q-item-section side middle class="q-mr-sm q-item-section-attachment-actions">
             <q-chip size="md" square class="theme-default-q-chip shadow-1 full-width text-center"
-              :class="{ 'cursor-not-allowed': disable }" :clickable="!disable"
+              :class="{ 'cursor-not-allowed': isDisabled }" :clickable="!isDisabled"
               @click.stop.prevent="onRemoveAttachmentAtIndex(attachmentIndex)">
               <q-avatar class="theme-default-q-avatar text-white bg-blue-6" icon="delete" />
               {{ t("Remove") }}
             </q-chip>
             <q-chip size="md" square class="theme-default-q-chip shadow-1 full-width"
               v-if="allowPreview(attachment.name)"
-              :class="{ 'cursor-not-allowed': disable || !allowPreview(attachment.name) }"
-              :clickable="!disable && allowPreview(attachment.name)"
+              :class="{ 'cursor-not-allowed': isDisabled || !allowPreview(attachment.name) }"
+              :clickable="!isDisabled && allowPreview(attachment.name)"
               @click.stop.prevent="onPreviewAttachment(attachmentIndex)">
               <q-avatar class="theme-default-q-avatar text-white bg-blue-6" icon="preview" />
               {{ t("Preview") }}
@@ -49,21 +52,23 @@
           class="q-my-sm" />
       </div>
     </div>
-    <CustomBanner v-else-if="!disable" warning text="No document attachments found" class="q-ma-none">
+    <CustomBanner v-else-if="!isDisabled" warning text="No document attachments found" class="q-ma-none">
     </CustomBanner>
   </q-list>
 
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref, reactive, computed, onMounted, onBeforeUnmount } from "vue";
 import { useI18n } from "vue-i18n";
 import { format } from "quasar";
 
 import { api, bgDownload } from "src/boot/axios";
+import { bus } from "src/boot/bus";
 import { useFileUtils } from "src/composables/fileUtils"
 import { useDocument } from "src/composables/document"
 
+import { default as CustomErrorBanner } from "src/components/Banners/CustomErrorBanner.vue";
 import { default as CustomBanner } from "src/components/Banners/CustomBanner.vue"
 
 const { t } = useI18n();
@@ -85,6 +90,15 @@ const props = defineProps({
     default: false
   }
 });
+
+const state = reactive({
+  loading: false,
+  loadingError: false,
+  errorMessage: null,
+  apiError: null
+});
+
+const isDisabled = computed(() => props.disable || state.loading);
 
 const attachments = computed({
   get() {
@@ -119,20 +133,31 @@ const onRemoveAttachmentAtIndex = (index) => {
   // orphaned elements are uploaded to server, but not associated (until document saved)
   // so we must remove them
   if (attachments.value[index].orphaned) {
-    // TODO
-    //loading.value = true;
+    state.loading = true;
+    state.loadingError = false;
+    state.errorMessage = null;
+    state.apiError = null;
     api.document.
       removeFile(attachments.value[index].id)
       .then((successResponse) => {
+        state.loading = false;
         attachments.value = attachments.value.filter((_, i) => i !== index);
-        // TODO
-        //loading.value = false;
-        //state.loading = false;
       })
       .catch((errorResponse) => {
-        // TODO
-        //loading.value = false;
-        //state.loading = false;
+        state.loading = false;
+        state.loadingError = true;
+        state.apiError = errorResponse.customAPIErrorDetails;
+        switch (errorResponse.response.status) {
+          case 401:
+            state.errorMessage = "Auth session expired, requesting new...";
+            bus.emit("reAuthRequired", { emitter: "DocumentDetailsAttachments.onRemoveAttachmentAtIndex" });
+            break;
+          default:
+            state.errorMessage = "API Error: fatal error";
+            break;
+        }
+        state.loading = false;
+
       });
   } else {
     attachments.value = attachments.value.filter((_, i) => i !== index);
@@ -152,6 +177,20 @@ const onDownload = (url, fileName) => {
       // TODO
     });
 }
+
+onMounted(() => {
+  bus.on("reAuthSucess", (msg) => {
+    if (msg.to?.includes("DocumentDetailsAttachments.onRemoveAttachmentAtIndex")) {
+      // TODO: save old selected index to remove & re-submit removeFile api call
+      //onRemoveAttachmentAtIndex(savedIndex);
+    }
+  });
+});
+
+onBeforeUnmount(() => {
+  bus.off("reAuthSucess");
+});
+
 </script>
 
 <style scoped>
