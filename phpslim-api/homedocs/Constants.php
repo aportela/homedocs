4c57824891685f5837d0e389d3e4791306465ad9
alex <766f6964+github@gmail.com>
2025-10-03 01:22:21 +0200

    new constants class added

---
+++ phpslim-api/homedocs/Constants.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HomeDocs;
+
+class Constants
+{
+    const UUID_V4_LENGTH = 36;
+    const MAX_DOCUMENT_TITLE_LENGTH = 128;
+    const MAX_DOCUMENT_DESCRIPTION_LENGTH = 8192;
+    const MAX_DOCUMENT_TAG_LENGTH = 32;
+    const MAX_DOCUMENT_NOTE_BODY_LENGTH = 16384;
+}
