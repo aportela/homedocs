8c25747a9281b84f76dcd7b30d648e9001ef6cbc
alex <766f6964+github@gmail.com>
2025-10-04 16:42:22 +0200

    create store before exporting

--- quasar-frontend/src/boot/inititalState.js
+++ quasar-frontend/src/boot/inititalState.js
@@ -1,9 +1,10 @@
 import { boot } from 'quasar/wrappers'
 import { useInitialStateStore } from "src/stores/initialState";
 
+const initialState = useInitialStateStore();
+
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(async (/* { app, router, ... } */) => {
-  const initialState = useInitialStateStore();
   initialState.load();
 })
