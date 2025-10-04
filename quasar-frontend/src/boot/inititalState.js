6045a5d850a2a44a19c88bb4a23dc3a8570ff7f7
alex <766f6964+github@gmail.com>
2025-10-04 18:07:50 +0200

    old code removed

--- quasar-frontend/src/boot/inititalState.js
+++ quasar-frontend/src/boot/inititalState.js
@@ -1,10 +1,7 @@
 import { boot } from 'quasar/wrappers'
-import { useInitialStateStore } from "src/stores/initialState";
 import { useAPI } from "src/composables/useAPI";
 
 const { api } = useAPI();
-
-const initialState = useInitialStateStore();
 
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
