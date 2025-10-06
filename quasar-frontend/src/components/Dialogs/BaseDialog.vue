9a5aa5b967a07c2b6e420eecd76925171d14aea4
alex <766f6964+github@gmail.com>
2025-10-06 15:31:25 +0200

    added width props & slots changes

--- quasar-frontend/src/components/Dialogs/BaseDialog.vue
+++ quasar-frontend/src/components/Dialogs/BaseDialog.vue
@@ -1,24 +1,26 @@
 <template>
   <q-dialog v-model="visible" @hide="onHide">
-    <q-card class="q-card-base-dialog">
-      <q-card-section class="q-py-sm row self-center q-dialog-header">
+    <q-card class="q-card-base-dialog" :style="{ width: width, minWidth: minWidth, maxWidth: maxWidth }">
+      <q-card-section class="q-py-xs row self-center q-dialog-header">
         <slot name="header">
           <div class="col flex items-center">
-            <slot name="title">Title</slot>
+            <slot name="header-left"></slot>
           </div>
           <q-space />
           <div>
-            <q-btn icon="close" flat round dense v-close-popup aria-label="Close modal" />
+            <slot name="header-right"></slot>
+            <q-btn icon="close" flat round dense v-close-popup aria-label="Close modal" v-if="showHeaderCloseButton" />
           </div>
         </slot>
       </q-card-section>
-      <q-card-section class="q-py-sm q-dialog-body">
+      <q-card-section class="q-pa-none q-dialog-body">
         <slot name="body"></slot>
       </q-card-section>
-      <q-card-section class="q-py-sm q-dialog-footer">
+      <q-card-section class="q-pa-none q-dialog-footer">
         <slot name="footer">
           <q-card-actions align="right">
             <slot name="actions">
+              <slot name="actions-prepend"></slot>
               <q-btn color="primary" size="md" no-caps @click.stop="onHide" icon="close" :label="t('Close')" />
             </slot>
           </q-card-actions>
@@ -36,6 +38,29 @@ import { useI18n } from "vue-i18n";
 const { t } = useI18n();
 
 const emit = defineEmits(['close']);
+
+const props = defineProps({
+  showHeaderCloseButton: {
+    type: Boolean,
+    required: false,
+    default: true
+  },
+  width: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  minWidth: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  maxWidth: {
+    type: String,
+    required: false,
+    default: ""
+  }
+});
 
 const visible = ref(true);
 
