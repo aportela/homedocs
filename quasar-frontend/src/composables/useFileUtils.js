81d642f7b529bedb05f3750053446eb4d7d11420
alex <766f6964+github@gmail.com>
2025-10-07 01:09:08 +0200

    code cleanups

--- quasar-frontend/src/composables/useFileUtils.js
+++ quasar-frontend/src/composables/useFileUtils.js
@@ -1,15 +1,14 @@
 export function useFileUtils() {
   const allowPreview = (filename) => {
-    return (!!filename?.match(/.(jpg|jpeg|png|gif|mp3)$/i));
+    return !!filename?.match(/.(jpg|jpeg|png|gif|mp3)$/i);
   };
 
   const isImage = (filename) => {
-    if (filename)
-      return (!!filename?.match(/.(jpg|jpeg|png|gif)$/i));
+    if (filename) return !!filename?.match(/.(jpg|jpeg|png|gif)$/i);
   };
 
   const isAudio = (filename) => {
-    return (!!filename?.match(/.(mp3)$/i));
+    return !!filename?.match(/.(mp3)$/i);
   };
 
   return { allowPreview, isImage, isAudio };
