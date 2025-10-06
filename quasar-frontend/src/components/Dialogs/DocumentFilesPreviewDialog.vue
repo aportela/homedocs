418d192a2613de70694c5a3d8cbcf57320bb15b1
alex <766f6964+github@gmail.com>
2025-10-06 15:36:36 +0200

    use BaseDialog component, added responsive item actions

--- quasar-frontend/src/components/Dialogs/DocumentFilesPreviewDialog.vue
+++ quasar-frontend/src/components/Dialogs/DocumentFilesPreviewDialog.vue
@@ -1,24 +1,15 @@
 <template>
-  <q-dialog v-model="dialogModel" @hide="onHide">
-    <q-card class="q-card-attachments-dialog">
-      <q-card-section class="row q-p-none">
-        <div class="q-card-attachments-dialog-header col" v-if="documentTitle">{{ t("Document title")
-          }}: <router-link :to="{ name: 'document', params: { id: documentId } }" class="text-decoration-hover">{{
-            documentTitle
-            }}</router-link>
-        </div>
-        <div class="q-card-attachments-dialog-header col" v-else>{{ t("Document attachments") }}</div>
-        <q-space />
-        <div>
-          <q-chip size="md" square class="gt-sm theme-default-q-chip" v-if="!state.loading && !state.loadingError">
-            <q-avatar class="theme-default-q-avatar">{{ attachments.length }}</q-avatar>
-            {{ t("Total files", { count: attachments.length }) }}
-          </q-chip>
-          <q-btn icon="close" flat round dense v-close-popup aria-label="Close modal" :disable="state.loading" />
-        </div>
-      </q-card-section>
-      <q-separator class="q-mb-md"></q-separator>
-      <q-card-section class="q-pt-none scroll attachments-scrolled-container">
+  <BaseDialog @close="onClose" width="1280px" max-width="80vw">
+    <template v-slot:header-left>
+      <div class="q-card-attachments-dialog-header col" v-if="documentTitle">{{ t("Document title")
+      }}: <router-link :to="{ name: 'document', params: { id: documentId } }" class="text-decoration-hover">{{
+          documentTitle
+        }}</router-link>
+      </div>
+      <div class="q-card-attachments-dialog-header col" v-else>{{ t("Document attachments") }}</div>
+    </template>
+    <template v-slot:body>
+      <div class="q-pt-none scroll attachments-scrolled-container">
         <div v-if="state.loading"></div>
         <div v-else-if="state.loadingError">
           <CustomErrorBanner :text="state.errorMessage || 'Error loading data'" :api-error="state.apiError">
@@ -28,49 +19,48 @@
           <CustomBanner warning text="This document has no associated attachments."></CustomBanner>
         </div>
         <q-list v-else>
-          <div v-for="attachment, index in attachments" :key="attachment.id">
-            <!-- TODO: clickable to preview if allowed or download -->
-            <q-item class="transparent-background text-color-primary q-pa-sm" clickable :href="attachment.url"
-              @click.stop.prevent="onDownload(attachment.url, attachment.name)">
-              <q-item-section top>
-                <q-item-label>
-                  <span class="text-weight-bold">{{ t("Name") }}:</span> {{ attachment.name }}
-                </q-item-label>
-                <q-item-label>
-                  <span class="text-weight-bold">{{ t("Size") }}:</span> {{ attachment.humanSize }}</q-item-label>
-                <q-item-label>
-                  <span class="text-weight-bold">{{ t('Uploaded on') }}:</span> {{ attachment.createdOn }} ({{
-                    timeAgo(attachment.createdOnTimestamp)
-                  }})</q-item-label>
+          <div v-for="attachment, index in attachments" :key="attachment.id" class="border-bottom-except-last-item">
+            <q-item class="transparent-background text-color-primary q-pa-sm" clickable
+              @click="onDownload(attachment.url, attachment.name)">
+              <q-item-section>
+                <div class="row full-width">
+                  <div class="col-xl-10 col-lg-10 col-md-9 col-sm-12 col-xs-12">
+                    <q-item-label>
+                      <span class="text-weight-bold">{{ t("Name") }}:</span> {{ attachment.name }}
+                    </q-item-label>
+                    <q-item-label>
+                      <span class="text-weight-bold">{{ t("Size") }}:</span> {{ attachment.humanSize }}</q-item-label>
+                    <q-item-label>
+                      <span class="text-weight-bold">{{ t('Uploaded on') }}:</span> {{ attachment.createdOn }} ({{
+                        timeAgo(attachment.createdOnTimestamp)
+                      }})</q-item-label>
+                  </div>
+                  <div class="col-xl-2 col-lg-2 col-md-3 col-sm-12 col-xs-12">
+                    <q-btn align="left" size="md" color="primary" class="q-mt-sm full-width"
+                      v-if="allowPreview(attachment.name)" :disable="state.loading" icon="save" :label="t('Download')"
+                      no-caps></q-btn>
+                    <q-btn align="left" size="md" color="primary" class="q-mt-sm full-width"
+                      v-if="allowPreview(attachment.name)" :disable="state.loading" icon="preview" :label="t('Preview')"
+                      no-caps @click.stop.prevent="onFilePreview(index)"></q-btn>
+                  </div>
+                </div>
               </q-item-section>
-              <q-item-section top side>
-                <q-btn size="md" color="primary" class="q-mt-sm" v-if="allowPreview(attachment.name)"
-                  :disable="state.loading" icon="preview" :label="t('Preview')" no-caps
-                  @click.stop.prevent="onFilePreview(index)"></q-btn>
-              </q-item-section>
-              <DesktopToolTip>{{ t("Click to download") }}</DesktopToolTip>
             </q-item>
-            <q-separator v-if="index !== attachments.length - 1" class="q-my-xs" />
           </div>
         </q-list>
-      </q-card-section>
-      <q-separator class="q-my-sm"></q-separator>
-      <q-card-section class="q-pt-none">
-        <q-card-actions align="right">
-          <CustomBanner v-if="downloadBanner.visible" :success="downloadBanner.success" :error="downloadBanner.error">
-            <template v-slot:text>{{ downloadBanner.text }}</template>
-          </CustomBanner>
-          <q-space></q-space>
-          <q-btn color="primary" :disable="state.loading" v-close-popup :label="t('Close')" icon="close"
-            aria-label="Close modal" />
-        </q-card-actions>
-      </q-card-section>
-    </q-card>
-  </q-dialog>
+      </div>
+    </template>
+    <template v-slot:actions-prepend>
+      <CustomBanner v-if="downloadBanner.visible" :success="downloadBanner.success" :error="downloadBanner.error">
+        <template v-slot:text>{{ downloadBanner.text }}</template>
+      </CustomBanner>
+      <q-space></q-space>
+    </template>
+  </BaseDialog>
 </template>
 
 <script setup>
-import { ref, reactive, computed, onMounted, onBeforeUnmount } from "vue";
+import { reactive, computed, onMounted, onBeforeUnmount } from "vue";
 import { useI18n } from "vue-i18n";
 import { date, format } from "quasar";
 import { useBus } from "src/composables/useBus";
@@ -79,7 +69,7 @@ import { useFileUtils } from "src/composables/useFileUtils"
 import { useAxios } from "src/composables/useAxios";
 import { useAPI } from "src/composables/useAPI";
 
-import { default as DesktopToolTip } from "src/components/DesktopToolTip.vue";
+import { default as BaseDialog } from "src/components/Dialogs/BaseDialog.vue";
 import { default as CustomErrorBanner } from "src/components/Banners/CustomErrorBanner.vue";
 import { default as CustomBanner } from "src/components/Banners/CustomBanner.vue";
 
@@ -114,8 +104,6 @@ const state = reactive({
 const attachments = reactive([]);
 const hasAttachments = computed(() => attachments?.length > 0);
 
-const dialogModel = ref(true);
-
 const onFilePreview = (index) => {
   bus.emit("showDocumentFilePreviewDialog", { document: { id: props.documentId, title: props.documentTitle, attachments: attachments }, currentIndex: index });
 };
@@ -132,6 +120,8 @@ const onDownload = (url, fileName) => {
   downloadBanner.success = false;
   downloadBanner.error = false;
   downloadBanner.text = null;
+  console.log(fileName);
+  console.log(url);
   bgDownload(url, fileName)
     .then((successResponse) => {
       downloadBanner.success = true;
@@ -186,7 +176,7 @@ const onRefresh = (documentId) => {
   }
 };
 
-const onHide = () => {
+const onClose = () => {
   emit('close');
 };
 
@@ -206,21 +196,11 @@ onBeforeUnmount(() => {
 </script>
 
 <style lang="css" scoped>
-.q-card-attachments-dialog {
-  width: 1280px;
-  max-width: 80vw;
-}
-
-.q-card-attachments-dialog-header {
-  font-size: 1.2em;
-  font-weight: bold;
-}
-
-.q-card-attachments-dialog-header span {
-  font-weight: normal;
+.border-bottom-except-last-item:not(:last-child) {
+  border-bottom: 1px solid rgba(0, 0, 0, 0.12);
 }
 
 .attachments-scrolled-container {
-  max-height: 50vh
+  max-height: 50vh;
 }
 </style>
