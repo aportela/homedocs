d85eea7d809b00652cbedce46e8b8899e956b1f5
alex <766f6964+github@gmail.com>
2025-10-06 16:44:35 +0200

    use new BaseDialog component

--- quasar-frontend/src/components/Dialogs/ReAuthDialog.vue
+++ quasar-frontend/src/components/Dialogs/ReAuthDialog.vue
@@ -1,52 +1,36 @@
 <template>
-  <q-dialog v-model="dialogModel" persistent @hide="onHide">
-    <q-card class="q-card-reauth-dialog">
-      <q-card-section class="q-p-none">
-        <div class="q-card-notes-dialog-header">
-          {{ t("Session lost... re-auth required") }}
-        </div>
-      </q-card-section>
-      <q-separator class="q-mb-md"></q-separator>
-      <q-card-section class="q-p-none">
-        <SignInForm :show-extra-bottom="false" @success="onSuccess">
-          <template v-slot:slogan>
-            <h4 class="q-mt-sm q-mb-md text-h4 text-weight-bolder">{{ t("Oooops") }}</h4>
-            <div class="text-color-secondary">
-              {{ t("Please enter again your credentials") }}</div>
-          </template>
-        </SignInForm>
-      </q-card-section>
-    </q-card>
-  </q-dialog>
+  <BaseDialog @close="onClose" width="512px" max-width="80vw" hide-footer persistent>
+    <template v-slot:header-left>
+      {{ t("Session lost... re-auth required") }}
+    </template>
+    <template v-slot:body>
+      <SignInForm :show-extra-bottom="false" @success="onSuccess">
+        <template v-slot:slogan>
+          <h4 class="q-mt-sm q-mb-md text-h4 text-weight-bolder">{{ t("Oooops") }}</h4>
+          <div class="text-color-secondary">
+            {{ t("Please enter again your credentials") }}</div>
+        </template>
+      </SignInForm>
+    </template>
+  </BaseDialog>
 </template>
 
 <script setup>
-import { ref } from "vue";
 import { useI18n } from "vue-i18n";
 
+import { default as BaseDialog } from "src/components/Dialogs/BaseDialog.vue"
 import { default as SignInForm } from "src/components/Forms/SignInForm.vue"
 
 const { t } = useI18n();
 
 const emit = defineEmits(['close', 'success']);
 
-const dialogModel = ref(true);
-
-const onHide = () => {
-  dialogModel.value = false;
+const onClose = () => {
   emit('close');
 }
 
 const onSuccess = () => {
-  dialogModel.value = false;
   emit('success');
 };
 
 </script>
-
-<style lang="css" scoped>
-.q-card-reauth-dialog {
-  width: 512px;
-  max-width: 80vw;
-}
-</style>
