7b1fd0bb8db84f6b237f7d2d826dc76e2852c035
alex <766f6964+github@gmail.com>
2025-10-17 22:39:37 +0200

    warning fixed

--- phpslim-api/tools/generate-demo-data.php
+++ phpslim-api/tools/generate-demo-data.php
@@ -30,13 +30,12 @@ if (count($missingExtensions) > 0) {
     if ($cmdLine->hasParam("count") && $cmdLine->hasParam("email") && $cmdLine->hasParam("password")) {
         $dbh = $container->get(\aportela\DatabaseWrapper\DB::class);
         $u = new \HomeDocs\User("", $cmdLine->getParamValue("email"), $cmdLine->getParamValue("password"));
-
+        $found = false;
         try {
             $u->get($dbh);
             $found = true;
         } catch (\HomeDocs\Exception\NotFoundException $e) {
         }
-
         if ($found) {
             //$c["logger"]->debug("Account exists -> update credentials");
             echo "User found, updating password...";
