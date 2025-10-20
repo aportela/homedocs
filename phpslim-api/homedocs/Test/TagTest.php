fb05a307a44065d88b7c53916fcebf6c1253c61b
alex <766f6964+github@gmail.com>
2025-10-20 15:28:37 +0200

    added more complete tag tests

--- phpslim-api/homedocs/Test/TagTest.php
+++ phpslim-api/homedocs/Test/TagTest.php
@@ -10,11 +10,40 @@ final class TagTest extends \HomeDocs\Test\BaseTest
 {
     public function testGetCloud(): void
     {
-        $this->assertIsArray(\HomeDocs\Tag::getCloud(self::$dbh));
+        $tag1 = sprintf("tag_%d", time());
+        $tag2 =  sprintf("tag_%d", time() + 1);
+        $this->createValidSession();
+        $d = new \HomeDocs\Document(\HomeDocs\Utils::uuidv4(), "document title", "document description", null, null, [$tag1, $tag2]);
+        $d->add(self::$dbh);
+        $tagCloud = \HomeDocs\Tag::getCloud(self::$dbh);
+        $d->delete(self::$dbh);
+        $this->assertTrue(count($tagCloud) >= 2);
+        $tags = array_values(array_filter($tagCloud, function ($obj) use ($tag1) {
+            return ($obj->tag == $tag1);
+        }));
+        $this->assertEquals(1, count($tags));
+        $this->assertEquals($tag1, $tags[0]->tag);
+        $this->assertEquals(1, $tags[0]->total);
+        $tags = array_values(array_filter($tagCloud, function ($obj) use ($tag2) {
+            return ($obj->tag == $tag2);
+        }));
+        $this->assertEquals(1, count($tags));
+        $this->assertEquals($tag2, $tags[0]->tag);
+        $this->assertEquals(1, $tags[0]->total);
     }
 
     public function testSearch(): void
     {
-        $this->assertIsArray(\HomeDocs\Tag::search(self::$dbh));
+
+        $tag1 = sprintf("tag_%d", time());
+        $tag2 =  sprintf("tag_%d", time() + 1);
+        $this->createValidSession();
+        $d = new \HomeDocs\Document(\HomeDocs\Utils::uuidv4(), "document title", "document description", null, null, [$tag1, $tag2]);
+        $d->add(self::$dbh);
+        $tags = \HomeDocs\Tag::search(self::$dbh);
+        $d->delete(self::$dbh);
+        $this->assertTrue(count($tags) >= 2);
+        $this->assertContains($tag1, $tags);
+        $this->assertContains($tag2, $tags);
     }
 }
