17c110579f0ace823d26845b95f5b80ebc1a36cd
alex <766f6964+github@gmail.com>
2025-10-22 18:39:23 +0200

    changed uuid database fields to fixed char(36)

--- phpslim-api/config/db-schema.php
+++ phpslim-api/config/db-schema.php
@@ -4,7 +4,7 @@ return (array(
     3 => array(
         '
             CREATE TABLE USER (
-                id VARCHAR(36) NOT NULL,
+                id CHAR(36) NOT NULL,
                 email VARCHAR(255) NOT NULL UNIQUE,
                 password_hash VARCHAR(60) NOT NULL,
                 created_on_timestamp INTEGER NOT NULL,
@@ -15,7 +15,7 @@ return (array(
             CREATE INDEX idx_user_email ON USER (email);
 
             CREATE TABLE DOCUMENT (
-                id VARCHAR(36) NOT NULL,
+                id CHAR(36) NOT NULL,
                 title VARCHAR(128) NOT NULL,
                 description VARCHAR(8192) NULL,
                 PRIMARY KEY (id)
@@ -24,7 +24,7 @@ return (array(
             CREATE INDEX idx_document_title ON DOCUMENT (title);
 
             CREATE TABLE DOCUMENT_TAG (
-                document_id VARCHAR(36) NOT NULL,
+                document_id CHAR(36) NOT NULL,
                 tag VARCHAR(32) NOT NULL,
                 PRIMARY KEY (document_id, tag),
                 FOREIGN KEY (document_id) REFERENCES DOCUMENT(id)
@@ -33,11 +33,11 @@ return (array(
             CREATE INDEX idx_document_tag_document_id ON DOCUMENT_TAG (document_id);
 
             CREATE TABLE ATTACHMENT (
-                id VARCHAR(36) NOT NULL,
-                sha1_hash VARCHAR(40) NOT NULL,
+                id CHAR(36) NOT NULL,
+                sha1_hash CHAR(40) NOT NULL,
                 name VARCHAR(256) NOT NULL,
                 size INTEGER NOT NULL,
-                created_by_user_id VARCHAR(36) NOT NULL,
+                created_by_user_id CHAR(36) NOT NULL,
                 created_on_timestamp INTEGER NOT NULL,
                 PRIMARY KEY (id),
                 FOREIGN KEY (created_by_user_id) REFERENCES USER(id)
@@ -46,8 +46,8 @@ return (array(
             CREATE INDEX idx_attachment_created_by_user_id_user_id ON ATTACHMENT (created_by_user_id);
 
             CREATE TABLE DOCUMENT_ATTACHMENT (
-                document_id VARCHAR(36) NOT NULL,
-                attachment_id VARCHAR(36) NOT NULL,
+                document_id CHAR(36) NOT NULL,
+                attachment_id CHAR(36) NOT NULL,
                 PRIMARY KEY (document_id, attachment_id),
                 FOREIGN KEY (document_id) REFERENCES DOCUMENT(id),
                 FOREIGN KEY (attachment_id) REFERENCES ATTACHMENT(id)
@@ -57,10 +57,10 @@ return (array(
             CREATE INDEX idx_document_attachment_attachment_id ON DOCUMENT_ATTACHMENT (attachment_id);
 
             CREATE TABLE DOCUMENT_NOTE (
-                note_id VARCHAR(36) NOT NULL,
-                document_id VARCHAR(36) NOT NULL,
+                note_id CHAR(36) NOT NULL,
+                document_id CHAR(36) NOT NULL,
                 created_on_timestamp INTEGER NOT NULL,
-                created_by_user_id VARCHAR(36) NOT NULL,
+                created_by_user_id CHAR(36) NOT NULL,
                 body VARCHAR(16384) NULL,
                 PRIMARY KEY (note_id),
                 FOREIGN KEY (document_id) REFERENCES DOCUMENT(id),
@@ -71,10 +71,10 @@ return (array(
             CREATE INDEX idx_document_note_user_id ON DOCUMENT_NOTE (created_by_user_id);
 
             CREATE TABLE DOCUMENT_HISTORY (
-                document_id VARCHAR(36) NOT NULL,
+                document_id CHAR(36) NOT NULL,
                 created_on_timestamp INTEGER NOT NULL,
                 operation_type INTEGER NOT NULL,
-                created_by_user_id VARCHAR(36) NOT NULL,
+                created_by_user_id CHAR(36) NOT NULL,
                 PRIMARY KEY (document_id, created_on_timestamp, operation_type, created_by_user_id),
                 FOREIGN KEY (document_id) REFERENCES DOCUMENT(id),
                 FOREIGN KEY (created_by_user_id) REFERENCES USER(id)
