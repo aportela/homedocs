57eb86849a01cecd4f7a809b3cf3aedef04a4ade
alex <766f6964+github@gmail.com>
2025-10-23 14:18:19 +0200

    allow customize date & datetime formats

--- quasar-frontend/src/components/Widgets/ProfileStorageSettingsWidget.vue
+++ quasar-frontend/src/components/Widgets/ProfileStorageSettingsWidget.vue
@@ -1,6 +1,10 @@
 <template>
   <BaseWidget title="Profile settings" caption="Customize how you interact with the platform." icon="settings">
     <template v-slot:content>
+      <p class="q-my-none"><q-input dense outlined clearable v-model.trim="dateFormatModel" :label="t('Date format')"
+          @update:modelValue="saveDateFormat" /></p>
+      <p class="q-my-md"><q-input dense outlined clearable v-model.trim="dateTimeFormatModel"
+          :label="t('Datetime format')" @update:modelValue="saveDateTimeFormat" /></p>
       <p class="q-my-none"><q-toggle v-model="visibilityCheck" checked-icon="check" color="green"
           :label="t('Always show uploading dialog after adding files')" unchecked-icon="clear" class="q-mr-md"
           @update:modelValue="saveVisibilityCheck" /></p>
@@ -21,7 +25,27 @@ import { default as BaseWidget } from "src/components/Widgets/BaseWidget.vue";
 
 const { t } = useI18n();
 
-const { alwaysOpenUploadDialog, showToolTips } = useLocalStorage();
+const { alwaysOpenUploadDialog, showToolTips, dateFormat, dateTimeFormat } = useLocalStorage();
+
+const dateFormatModel = ref(dateFormat.get());
+
+const saveDateFormat = (val) => {
+  if (val && val.length > 0) {
+    dateFormat.set(val);
+  } else {
+    dateFormat.remove();
+  }
+};
+
+const dateTimeFormatModel = ref(dateTimeFormat.get());
+
+const saveDateTimeFormat = (val) => {
+  if (val && val.length > 0) {
+    dateTimeFormat.set(val);
+  } else {
+    dateTimeFormat.remove();
+  }
+};
 
 const visibilityCheck = ref(alwaysOpenUploadDialog.get());
 
