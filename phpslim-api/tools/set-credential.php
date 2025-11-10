99161ec7e47a205c136dd512cdd9fd4537bd33b4
alex <766f6964+github@gmail.com>
2025-11-10 12:34:16 +0100

    use installer class && added email validation

--- phpslim-api/tools/set-credential.php
+++ phpslim-api/tools/set-credential.php
@@ -12,43 +12,59 @@ $containerBuilder->addDefinitions(__DIR__ . '../../config/container.php');
 // Build PHP-DI Container instance
 $container = $containerBuilder->build();
 
-echo "HomeDocs account manager" . PHP_EOL;
+echo "[-] HomeDocs account manager" . PHP_EOL;
 
 $logger = $container->get(\HomeDocs\Logger\InstallerLogger::class);
 
 $settings = $container->get('settings');
 
-$missingExtensions = array_diff($settings["phpRequiredExtensions"], get_loaded_extensions());
-if ($missingExtensions !== []) {
-    $missingExtensionsStr = implode(", ", $missingExtensions);
-    echo "Error: missing php extension/s: " . $missingExtensionsStr . PHP_EOL;
-    $logger->critical("Error: missing php extension/s: ", [$missingExtensionsStr]);
+$installer = new \HomeDocs\Installer($logger, $container);
+
+echo "[?] Checking php required extensions...";
+if ($installer->checkRequiredPHPExtensions()) {
+    echo " success!" . PHP_EOL;
 } else {
-    $cmdLine = new \HomeDocs\CmdLine("", ["email:", "password:"]);
-    if ($cmdLine->hasParam("email") && $cmdLine->hasParam("password")) {
-        echo "Setting account credentials..." . PHP_EOL;
-        $dbh = $container->get(\aportela\DatabaseWrapper\DB::class);
-        $found = false;
-        $u = new \HomeDocs\User("", $cmdLine->getParamValue("email"), $cmdLine->getParamValue("password"));
-        try {
-            $u->get($dbh);
-            $found = true;
-        } catch (\HomeDocs\Exception\NotFoundException) {
-        }
-        
-        if ($found) {
-            //$c["logger"]->debug("Account exists -> update credentials");
-            echo "User found, updating password...";
-            $u->update($dbh);
-            echo "ok!" . PHP_EOL;
-        } else {
-            //$c["logger"]->debug("Account not found -> adding credentials");
-            echo "User not found, creating account...";
-            $u->id = \HomeDocs\Utils::uuidv4();
-            $u->add($dbh);
-            echo "ok!" . PHP_EOL;
-        }
+    $missingPHPExtensions = $installer->getMissingPHPExtensions();
+    echo " error! - missing extensions: " . implode(",", $missingPHPExtensions) . PHP_EOL;
+    $logger->error("Missing php required extensions", $missingPHPExtensions);
+    exit(1);
+}
+
+echo "[?] Checking params...";
+$cmdLine = new \HomeDocs\CmdLine("", ["email:", "password:"]);
+if ($cmdLine->hasParam("email") && $cmdLine->hasParam("password")) {
+    $email = $cmdLine->getParamValue("email");
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo " error! - invalid email param: {$email}" . PHP_EOL;
+        $logger->error("Invalid email param", [$email]);
+        exit(1);
     } else {
-        echo "No required params found: --email <email> --password <secret>" . PHP_EOL;
+        echo " success!" . PHP_EOL;
     }
+    echo "[?] Setting account credentials...";
+    $dbh = $container->get(\aportela\DatabaseWrapper\DB::class);
+    $found = false;
+    $u = new \HomeDocs\User("", $cmdLine->getParamValue("email"), $cmdLine->getParamValue("password"));
+    try {
+        $u->get($dbh);
+        $found = true;
+    } catch (\HomeDocs\Exception\NotFoundException) {
+    }
+
+    if ($found) {
+        //$c["logger"]->debug("Account exists -> update credentials");
+        echo " user found, updating password...";
+        $u->update($dbh);
+        echo " success!" . PHP_EOL;
+    } else {
+        //$c["logger"]->debug("Account not found -> adding credentials");
+        echo " user not found, creating account...";
+        $u->id = \HomeDocs\Utils::uuidv4();
+        $u->add($dbh);
+        echo " success!" . PHP_EOL;
+    }
+    exit(0);
+} else {
+    echo " error! - No required params found: --email <email> --password <secret>" . PHP_EOL;
+    exit(1);
 }
