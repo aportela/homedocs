70d55ad70fc63391e8507a095eb9fe91f18b17ab
alex <766f6964+github@gmail.com>
2025-11-12 09:23:51 +0100

    new stable build release

--- phpslim-api/public/cliserver.php
+++ phpslim-api/public/cliserver.php
@@ -6,7 +6,7 @@
 
 // public/cliserver.php (router script)
 
-if (PHP_SAPI !== 'cli-server') {
+if (php_sapi_name() !== 'cli-server') {
     die('this is only for the php development server');
 }
 
@@ -14,9 +14,8 @@ if (is_file($_SERVER['DOCUMENT_ROOT'] . '/' . $_SERVER['SCRIPT_NAME'])) {
     // probably a static file...
     return false;
 }
-
 $_SERVER['SCRIPT_NAME'] = '/index.php';
 // if needed, fix also 'PATH_INFO' and 'PHP_SELF' variables here...
 
 // require the entry point
-require __DIR__ . '/index.php';
+require 'index.php';
