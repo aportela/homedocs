481b91bede54dcf763d60c74051784b62a350ff4
alex <766f6964+github@gmail.com>
2025-11-13 14:39:59 +0100

    warnings fixed  (php-stan)

--- quasar-frontend/public/cliserver.php
+++ quasar-frontend/public/cliserver.php
@@ -6,16 +6,19 @@
 
 // public/cliserver.php (router script)
 
-if (php_sapi_name() !== 'cli-server') {
-    die('this is only for the php development server');
+if (PHP_SAPI !== 'cli-server') {
+  die('this is only for the php development server');
 }
 
-if (is_file($_SERVER['DOCUMENT_ROOT'] . '/' . $_SERVER['SCRIPT_NAME'])) {
-    // probably a static file...
-    return false;
+$file = (is_string($_SERVER['DOCUMENT_ROOT']) && is_string($_SERVER['SCRIPT_NAME'])) ? $_SERVER['DOCUMENT_ROOT'] . '/' . $_SERVER['SCRIPT_NAME'] : null;
+
+if ($file !== null && is_file($file)) {
+  // probably a static file...
+  return false;
 }
+
 $_SERVER['SCRIPT_NAME'] = '/index.php';
 // if needed, fix also 'PATH_INFO' and 'PHP_SELF' variables here...
 
 // require the entry point
-require 'index.php';
+require __DIR__ . '/index.php';
