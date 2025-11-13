3e1a8311e57c5864945f837b78fe04cc89033dfe
alex <766f6964+github@gmail.com>
2025-11-13 14:41:02 +0100

    new quasar web frontend stable build release

--- phpslim-api/public/cliserver.php
+++ phpslim-api/public/cliserver.php
@@ -7,14 +7,14 @@
 // public/cliserver.php (router script)
 
 if (PHP_SAPI !== 'cli-server') {
-    die('this is only for the php development server');
+  die('this is only for the php development server');
 }
 
 $file = (is_string($_SERVER['DOCUMENT_ROOT']) && is_string($_SERVER['SCRIPT_NAME'])) ? $_SERVER['DOCUMENT_ROOT'] . '/' . $_SERVER['SCRIPT_NAME'] : null;
 
 if ($file !== null && is_file($file)) {
-    // probably a static file...
-    return false;
+  // probably a static file...
+  return false;
 }
 
 $_SERVER['SCRIPT_NAME'] = '/index.php';
