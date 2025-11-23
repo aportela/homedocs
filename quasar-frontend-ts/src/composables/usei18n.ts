5ed1012fdf72605cbe53f020ccb7ae6d80b9ee57
alex <766f6964+github@gmail.com>
2025-11-23 22:14:48 +0100

    i18n migrated to TypeScript

--- quasar-frontend-ts/src/composables/usei18n.ts
+++ quasar-frontend-ts/src/composables/usei18n.ts
@@ -3,19 +3,38 @@ import messages from "src/i18n";
 import { Lang } from "quasar";
 import { useLocalStorage } from "./useLocalStorage";
 
+export type MessageLanguages = keyof typeof messages;
+// Type-define 'en-US' as the master schema for the resource
+export type MessageSchema = (typeof messages)['en-US'];
+
+// See https://vue-i18n.intlify.dev/guide/advanced/typescript.html#global-resource-schema-type-definition
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+declare module 'vue-i18n' {
+  // define the locale messages schema
+  export interface DefineLocaleMessage extends MessageSchema { }
+
+  // define the datetime format schema
+  export interface DefineDateTimeFormat { }
+
+  // define the number format schema
+  export interface DefineNumberFormat { }
+}
+/* eslint-enable @typescript-eslint/no-empty-object-type */
+
 
 const { locale } = useLocalStorage();
 
 const availableLocales = Object.keys(messages);
 
-const localeMappings = {
-  es: 'es-ES',
+const localeMappings: Record<'en' | 'es' | 'gl', string> = {
   en: 'en-EN',
+  es: 'es-ES',
   gl: 'gl-GL'
 };
 
-const browserLocale = Lang.getLocale();
-const normalizedBrowsedLocale = browserLocale.length >= 2 ? (localeMappings[browserLocale.substring(0, 2)] || browserLocale) : browserLocale;
+const browserLocale = Lang.getLocale() ?? "en";
+const key = browserLocale.substring(0, 2) as keyof typeof localeMappings;
+const normalizedBrowsedLocale = localeMappings[key] || browserLocale;
 
 const savedLocale = locale.get();
 
@@ -25,8 +44,8 @@ currentLocale = availableLocales.includes(currentLocale)
   ? currentLocale
   : "en-US";
 
-// Create I18n instance
-const i18n = createI18n({
+
+const i18n = createI18n<{ message: MessageSchema }, MessageLanguages>({
   locale: currentLocale,
   legacy: false, // you must set `false`, to use Composition API
   globalInjection: true,
