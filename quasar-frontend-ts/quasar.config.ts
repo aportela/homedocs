23920cd025881bf9f16abd739776835adb7dc2b2
alex <766f6964+github@gmail.com>
2025-11-23 21:33:14 +0100

    migrated remaining boot files to TypeScript

--- quasar-frontend-ts/quasar.config.ts
+++ quasar-frontend-ts/quasar.config.ts
@@ -12,7 +12,7 @@ export default defineConfig((ctx) => {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: ['serverEnvironment', 'i18n', 'axios', 'dark'],
+    boot: ['axios', 'serverEnvironment', 'router', 'i18n', 'dark'],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#css
     css: ['app.scss'],
--- quasar-frontend-ts/src/boot/axios.ts
+++ quasar-frontend-ts/src/boot/axios.ts
@@ -1,31 +1,8 @@
 import { defineBoot } from '#q-app/wrappers';
-import axios, { type AxiosInstance } from 'axios';
+import { useAxios } from 'src/composables/useAxios';
 
-declare module 'vue' {
-  interface ComponentCustomProperties {
-    $axios: AxiosInstance;
-    $api: AxiosInstance;
-  }
-}
-
-// Be careful when using SSR for cross-request state pollution
-// due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
-// "export default () => {}" function below (which runs individually
-// for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' });
+const { axiosInstance } = useAxios();
 
 export default defineBoot(({ app }) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
-
-  app.config.globalProperties.$axios = axios;
-  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
-  //       so you won't necessarily have to import axios in each vue file
-
-  app.config.globalProperties.$api = api;
-  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
-  //       so you can easily perform requests against your app's API
+  app.config.globalProperties.$api = axiosInstance;
 });
-
-export { api };
