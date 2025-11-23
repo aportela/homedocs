b8fb80a067797e2318d21f388f8a051caa5af6f5
alex <766f6964+github@gmail.com>
2025-11-23 10:39:57 +0100

    method not required anymore

--- quasar-frontend/src/layouts/MainLayout.vue
+++ quasar-frontend/src/layouts/MainLayout.vue
@@ -13,7 +13,7 @@
         <q-btn type="button" no-caps no-wrap align="left" outline :label="searchButtonLabel" icon="search"
           class="full-width no-caps theme-default-q-btn" @click.prevent="dialogs.fastSearch.visible = true">
           <DesktopToolTip anchor="bottom middle" self="top middle">{{ t("Click to open fast search")
-          }}</DesktopToolTip>
+            }}</DesktopToolTip>
         </q-btn>
         <!--
         <FastSearchSelector dense class="full-width"></FastSearchSelector>
@@ -83,10 +83,6 @@ const { bus } = useBus();
 const { currentTimestamp } = useFormatDates();
 
 const { alwaysOpenUploadDialog } = useLocalStorage();
-
-if (!session.isLoaded) {
-  session.load();
-}
 
 const dialogs = reactive({
   reauth: {
