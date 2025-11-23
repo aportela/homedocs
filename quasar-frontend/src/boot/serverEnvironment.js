6994ba34e8d629d3d3a6be926aeea542ee9b0f0b
alex <766f6964+github@gmail.com>
2025-11-23 11:36:51 +0100

    refactored initialState to serverEnvironment

--- quasar-frontend/src/boot/serverEnvironment.js
+++ quasar-frontend/src/boot/serverEnvironment.js
@@ -8,7 +8,7 @@ const { api } = useAPI();
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(async (/* { app, router, ... } */) => {
   try {
-    // on useAxios composable we declare a response interceptor that assign initialState (if found) for every response
+    // on useAxios composable we declare a response interceptor that assign serverEnvironment (if found) for every response
     await api.common.getServerEnvironment();
   } catch (error) {
     console.error(error);
