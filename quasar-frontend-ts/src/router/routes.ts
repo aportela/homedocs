73b8c3c866e61e25f80c943d27db52136cf4a1bc
alex <766f6964+github@gmail.com>
2025-11-23 18:21:24 +0100

    routes moved to TypeScript

--- quasar-frontend-ts/src/router/routes.ts
+++ quasar-frontend-ts/src/router/routes.ts
@@ -1,17 +1,85 @@
-import type { RouteRecordRaw } from 'vue-router';
-
-const routes: RouteRecordRaw[] = [
+const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    path: "/auth",
+    component: () => import("layouts/LoginRegisterLayout.vue"),
+    children: [
+      {
+        name: "login",
+        path: "login",
+        component: () => import("pages/LoginPage.vue"),
+      },
+      {
+        name: "register",
+        path: "register",
+        component: () => import("pages/RegisterPage.vue"),
+      },
+    ],
   },
+  {
+    path: "/",
+    name: "root",
+    redirect: "/index",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        name: "index",
+        path: "index",
+        component: () => import("pages/IndexPage.vue"),
+      },
+      {
+        name: "profile",
+        path: "profile",
+        component: () => import("pages/ProfilePage.vue"),
+      },
+      {
+        name: "newDocument",
+        path: "new-document",
+        component: () => import("pages/DocumentPage.vue"),
+      },
+      {
+        name: "document",
+        path: "document/:id",
+        component: () => import("pages/DocumentPage.vue"),
+      },
+      {
+        name: "advancedSearch",
+        path: "advanced-search",
+        component: () => import("pages/AdvancedSearchPage.vue"),
+        meta: { conditionsFilterExpanded: true, autoLaunchSearch: false },
+      },
+      {
+        name: "advancedSearchByTag",
+        path: "advanced-search/tag/:tag",
+        component: () => import("pages/AdvancedSearchPage.vue"),
+        meta: { conditionsFilterExpanded: false, autoLaunchSearch: true },
+      },
+      {
+        name: "advancedSearchByFixedCreationDate",
+        path: "advanced-search/fixed-creation-date/:fixedCreationDate",
+        component: () => import("pages/AdvancedSearchPage.vue"),
+        meta: { conditionsFilterExpanded: false, autoLaunchSearch: true },
+      },
+      {
+        name: "advancedSearchByFixedLastUpdate",
+        path: "advanced-search/fixed-last-update/:fixedLastUpdate",
 
+        component: () => import("pages/AdvancedSearchPage.vue"),
+        meta: { conditionsFilterExpanded: false, autoLaunchSearch: true },
+      },
+      {
+        name: "advancedSearchByFixedUpdatedOn",
+        path: "advanced-search/fixed-updated-on/:fixedUpdatedOn",
+        component: () => import("pages/AdvancedSearchPage.vue"),
+        meta: { conditionsFilterExpanded: false, autoLaunchSearch: true },
+      },
+    ],
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    path: "/:catchAll(.*)*",
+    name: "notFound",
+    component: () => import("layouts/ErrorNotFoundLayout.vue"),
   },
 ];
 
