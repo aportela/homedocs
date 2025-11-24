c7afc3ea114b07459e2c0a61d710ce242fd425eb
alex <766f6964+github@gmail.com>
2025-11-24 23:33:40 +0100

    store fixed

--- quasar-frontend-ts/src/components/Buttons/DarkModeButton.vue
+++ quasar-frontend-ts/src/components/Buttons/DarkModeButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-btn v-bind="attrs" :icon="currentDarkModeIcon" @click="toggle">
+  <q-btn v-bind="attrs" :icon="currentDarkModeIcon" @click="darkModeStore.toggle">
     <DesktopToolTip>{{ tooltip }}</DesktopToolTip>
     <slot></slot>
   </q-btn>
@@ -18,15 +18,15 @@ import { default as DesktopToolTip } from "src/components/DesktopToolTip.vue";
 const { t } = useI18n();
 const attrs = useAttrs();
 
-const { toggle, isActive } = useDarkModeStore();
+const darkModeStore = useDarkModeStore();
 
 const currentDarkModeIcon = computed(() => {
-  return (isActive ? "dark_mode" : "light_mode");
+  return (darkModeStore.isActive ? "dark_mode" : "light_mode");
 });
 
 const toolTipLight = computed(() => t("Switch to light mode"));
 const toolTipDark = computed(() => t("Switch to dark mode"));
 
-const tooltip = computed(() => isActive ? toolTipLight : toolTipDark);
+const tooltip = computed(() => darkModeStore.isActive ? toolTipLight : toolTipDark);
 
 </script>
