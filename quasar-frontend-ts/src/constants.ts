b43147847e21b2ca351a003316821fe8355b1f29
alex <766f6964+github@gmail.com>
2025-11-24 10:48:56 +0100

    added default locale const

--- quasar-frontend-ts/src/constants.ts
+++ quasar-frontend-ts/src/constants.ts
@@ -3,3 +3,5 @@ export const GITHUB_PROJECT_URL = "http://github.com/aportela/homedocs";
 export const LOCAL_STORAGE_NAMESPACE = "homedocs#";
 
 export const SERVER_API_BASE_PATH = "api3";
+
+export const DEFAULT_LOCALE = "en-US";
