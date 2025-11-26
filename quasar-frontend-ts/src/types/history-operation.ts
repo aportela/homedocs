cc689409e8cb0ceb43cd37599c10fef747d48408
alex <766f6964+github@gmail.com>
2025-11-26 17:02:39 +0100

    interface changed, added class

--- quasar-frontend-ts/src/types/history-operation.ts
+++ quasar-frontend-ts/src/types/history-operation.ts
@@ -1,10 +1,42 @@
+import { type DateTime as DateTimeInterface } from "src/types/date-time";
+import { uid } from "quasar";
+
+type HistoryOperationType = 1 | 2;
+
 interface HistoryOperation {
-  operationType: number;
+  id: string;
+  createdAt: DateTimeInterface;
+  //operationType: HistoryOperationType;
   label: string;
   icon: string;
-  createdOnTimestamp: number;
-  createdOn: string;
-  createdOnTimeAgo: string;
 };
 
-export { type HistoryOperation };
+class HistoryOperationClass implements HistoryOperation {
+  id: string;
+  createdAt: DateTimeInterface;
+  //operationType: HistoryOperationType;
+  label: string;
+  icon: string;
+
+  constructor(createdAt: DateTimeInterface, operationType: HistoryOperationType) {
+    this.id = uid();
+    //this.operationType = operationType;
+    switch (operationType) {
+      case 1:
+        this.label = "Document created";
+        this.icon = "post_add";
+        break;
+      case 2:
+        this.label = "Document updated";
+        this.icon = "edit_note";
+        break;
+      default:
+        this.label = "Unknown operation";
+        this.icon = "error";
+        break;
+    }
+    this.createdAt = createdAt;
+  }
+};
+
+export { type HistoryOperationType, type HistoryOperation, HistoryOperationClass };
