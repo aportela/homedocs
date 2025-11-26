b59b67feeeb81d4133b94212a3d6bdf05f46e28b
alex <766f6964+github@gmail.com>
2025-11-26 16:03:05 +0100

    code indents

--- quasar-frontend-ts/src/components/Dialogs/DocumentFilesPreviewDialog.vue
+++ quasar-frontend-ts/src/components/Dialogs/DocumentFilesPreviewDialog.vue
@@ -2,9 +2,9 @@
   <BaseDialog v-model="visible" @close="onClose" width="1280px" max-width="80vw">
     <template v-slot:header-left>
       <div v-if="documentTitle">{{ t("Document title")
-        }}: <router-link :to="{ name: 'document', params: { id: documentId } }" class="text-decoration-hover">{{
+      }}: <router-link :to="{ name: 'document', params: { id: documentId } }" class="text-decoration-hover">{{
           documentTitle
-          }}</router-link>
+        }}</router-link>
       </div>
       <div v-else>{{ t("Document attachments") }}</div>
     </template>
