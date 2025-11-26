2c5e05825f95ba0b2b62876d965a95abcb627182
alex <766f6964+github@gmail.com>
2025-11-26 15:40:26 +0100

    updated Attachment interface & added AttachmentClass

--- quasar-frontend-ts/src/types/attachment.ts
+++ quasar-frontend-ts/src/types/attachment.ts
@@ -1,13 +1,35 @@
+import { format } from "quasar";
+
 import { type DateTime as DateTimeInterface } from "src/types/date-time";
 
 interface Attachment {
   id: string;
   name: string;
-  size: number | null;
   hash: string | null;
-  humanSize: string | null;
+  size: number;
+  humanSize: string;
   createdAt: DateTimeInterface;
   orphaned: boolean;
 };
 
-export { type Attachment };
+class AttachmentClass implements Attachment {
+  id: string;
+  name: string;
+  hash: string | null;
+  size: number;
+  humanSize: string;
+  createdAt: DateTimeInterface;
+  orphaned: boolean;
+
+  constructor(id: string, name: string, hash: string | null, size: number, createdAt: DateTimeInterface, orphaned: boolean) {
+    this.id = id;
+    this.name = name;
+    this.hash = hash;
+    this.size = size;
+    this.humanSize = format.humanStorageSize(size);
+    this.createdAt = createdAt;
+    this.orphaned = orphaned;
+  }
+};
+
+export { type Attachment, AttachmentClass };
