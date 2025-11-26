9ab101f9924e7a583a8e1c6fe8a13383a578c906
alex <766f6964+github@gmail.com>
2025-11-27 00:09:19 +0100

    new separated widgets for stats

---
+++ quasar-frontend-ts/src/components/Widgets/SystemStatsCountTotalAttachmentsWidget.vue
@@ -0,0 +1,63 @@
+<template>
+  <SystemStatsWidgetBase :loading="state.ajaxRunning" :error="state.ajaxErrors" icon="attachment"
+    header-label="Total attachments" :total="total" :error-message="state.ajaxErrorMessage"
+    :api-error-details="state.ajaxAPIErrorDetails" />
+</template>
+
+<script setup lang="ts">
+import { ref, reactive, onMounted, onBeforeUnmount } from "vue";
+
+import { bus } from "src/composables/useBus";
+import { api } from "src/composables/useAPI";
+import { type AjaxState as AjaxStateInterface, defaultAjaxState } from "src/types/ajax-state";
+import { type GetTotalAttachmentsStatsResponse as GetTotalAttachmentsStatsResponseInterface } from "src/types/api-responses";
+import { default as SystemStatsWidgetBase } from "src/components/Widgets/SystemStatsWidgetBase.vue";
+
+const state: AjaxStateInterface = reactive({ ...defaultAjaxState });
+
+const total = ref<number>(0);
+
+const onRefresh = () => {
+  if (!state.ajaxRunning) {
+    Object.assign(state, defaultAjaxState);
+    state.ajaxRunning = true;
+    api.stats.attachmentCount()
+      .then((successResponse: GetTotalAttachmentsStatsResponseInterface) => {
+        total.value = successResponse.data.count;
+      })
+      .catch((errorResponse) => {
+        state.ajaxErrors = true;
+        if (errorResponse.isAPIError) {
+          state.ajaxAPIErrorDetails = errorResponse.customAPIErrorDetails;
+          switch (errorResponse.response.status) {
+            case 401:
+              state.ajaxErrorMessage = "Auth session expired, requesting new...";
+              bus.emit("reAuthRequired", { emitter: "SystemStatsCountTotalAttachmentsWidget" });
+              break;
+            default:
+              state.ajaxErrorMessage = "API Error: fatal error";
+              break;
+          }
+        } else {
+          state.ajaxErrorMessage = `Uncaught exception: ${errorResponse}`;
+          console.error(errorResponse);
+        }
+      }).finally(() => {
+        state.ajaxRunning = false;
+      });
+  }
+}
+
+onMounted(() => {
+  onRefresh();
+  bus.on("reAuthSucess", (msg) => {
+    if (msg.to?.includes("SystemStatsCountTotalAttachmentsWidget")) {
+      onRefresh();
+    }
+  });
+});
+
+onBeforeUnmount(() => {
+  bus.off("reAuthSucess");
+});
+</script>
---
+++ quasar-frontend-ts/src/components/Widgets/SystemStatsWidgetBase.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-card class="q-ma-sm">
+    <q-card-section class="text-center text-h6 theme-default-q-card-section-header">
+      <q-icon :name="loading ? 'settings' : error ? 'error' : icon"
+        :class="{ 'text-red': error, 'animation-spin': loading }" />
+      {{ t(headerLabel) }}
+    </q-card-section>
+    <q-card-section class="text-center text-h4" v-if="!error">{{ total }}</q-card-section>
+    <q-card-section v-else>
+      <CustomErrorBanner :text="errorMessage || 'Error loading data'" :api-error-details="apiErrorDetails">
+      </CustomErrorBanner>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup lang="ts">
+
+import { useI18n } from "vue-i18n";
+
+import { type APIErrorDetails as APIErrorDetailsInterface } from "src/types/api-error-details";
+import { default as CustomErrorBanner } from "src/components/Banners/CustomErrorBanner.vue";
+
+const { t } = useI18n();
+
+interface SystemStatsCountWidgetBaseProps {
+  loading: boolean;
+  error: boolean;
+  icon: string;
+  headerLabel: string;
+  total: number;
+  errorMessage: string | null;
+  apiErrorDetails: APIErrorDetailsInterface | null;
+};
+
+defineProps<SystemStatsCountWidgetBaseProps>();
+
+</script>
