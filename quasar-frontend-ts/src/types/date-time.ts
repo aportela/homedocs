144154b89ba51b13b7a4eb1ea56775b49a6e5c09
alex <766f6964+github@gmail.com>
2025-11-26 21:22:28 +0100

    extends DateClass

--- quasar-frontend-ts/src/types/date-time.ts
+++ quasar-frontend-ts/src/types/date-time.ts
@@ -1,34 +1,24 @@
-import { type Date as DateInterface } from "./date";
-import { dateFormat as localStorageDateFormat, dateTimeFormat as localStorageDateTimeFormat } from "src/composables/useLocalStorage";
-import { dateHuman, fullDateTimeHuman, timeAgo } from "src/composables/useFormatDates";
+import { type Date as DateInterface, DateClass } from "./date";
+import { dateTimeFormat as localStorageDateTimeFormat } from "src/composables/useLocalStorage";
+import { fullDateTimeHuman, } from "src/composables/useFormatDates";
 import { type Ti18NFunction } from "./i18n";
 
 interface DateTime extends DateInterface {
   dateTime: string | null,
 };
 
-class DateTimeClass implements DateTime {
-  timestamp: number | null;
-  // TODO: refactor to formattedDate
-  date: string | null;
+class DateTimeClass extends DateClass implements DateTime {
   // TODO: refactor to formattedDateTime
   dateTime: string | null;
-  timeAgo: string | null;
 
   constructor(t: Ti18NFunction, timestamp: number | null) {
+    super(t, timestamp);
     if (timestamp) {
-      this.timestamp = timestamp;
-      this.date = dateHuman(timestamp, localStorageDateFormat.get());
       this.dateTime = fullDateTimeHuman(timestamp, localStorageDateTimeFormat.get());
-      const returnedTimeAgo = timeAgo(timestamp);
-      this.timeAgo = t(returnedTimeAgo.label, returnedTimeAgo.count != null ? { count: returnedTimeAgo.count } : {});
     } else {
-      this.timestamp = null;
-      this.date = null;
       this.dateTime = null;
-      this.timeAgo = null;
     }
   }
-}
+};
 
 export { type DateTime, DateTimeClass }
