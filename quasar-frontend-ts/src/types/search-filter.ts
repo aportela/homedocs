8cecb1ba486e8fbc9381b9183ab248ba14584c7f
alex <766f6964+github@gmail.com>
2025-11-28 00:17:28 +0100

    new interfaces & classes

--- quasar-frontend-ts/src/types/search-filter.ts
+++ quasar-frontend-ts/src/types/search-filter.ts
@@ -5,14 +5,53 @@ interface SearchOnTextEntitiesFilter {
   attachmentsFilename: string | null;
 };
 
+class SearchOnTextEntitiesFilterClass implements SearchOnTextEntitiesFilter {
+  title: string | null;
+  description: string | null;
+  notesBody: string | null;
+  attachmentsFilename: string | null;
+
+  constructor(title: string | null, description: string | null, notesBody: string | null, attachmentsFilename: string | null) {
+    this.title = title;
+    this.description = description;
+    this.notesBody = notesBody;
+    this.attachmentsFilename = attachmentsFilename;
+  }
+}
+
+interface SearchDatesFilter {
+  createdAt: any;
+  lastUpdateAt: any;
+  updatedAt: any;
+};
+
+class SearchDatesFilterClass implements SearchDatesFilter {
+  createdAt: any;
+  lastUpdateAt: any;
+  updatedAt: any;
+
+  constructor(createdAt: any, lastUpdateAt: any, updatedAt: any) {
+    this.createdAt = createdAt;
+    this.lastUpdateAt = lastUpdateAt;
+    this.updatedAt = updatedAt;
+  }
+}
+
 interface SearchFilter {
   text: SearchOnTextEntitiesFilter;
   tags: string[];
-  dates: {
-    createdAt: any;
-    lastUpdateAt: any;
-    updatedAt: any;
-  },
+  dates: SearchDatesFilter;
 };
 
-export { type SearchOnTextEntitiesFilter, type SearchFilter };
+class SearchFilterClass implements SearchFilter {
+  text: SearchOnTextEntitiesFilter;
+  tags: string[];
+  dates: SearchDatesFilter;
+
+  constructor(text: SearchOnTextEntitiesFilter, tags: string[], dates: SearchDatesFilter) {
+    this.text = text;
+    this.tags = tags;
+    this.dates = dates;
+  }
+}
+export { type SearchOnTextEntitiesFilter, SearchOnTextEntitiesFilterClass, type SearchDatesFilter, SearchDatesFilterClass, type SearchFilter, SearchFilterClass };
