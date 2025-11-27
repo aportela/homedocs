2e5638798dd1dfd09fa4e6f0be38d78aeadfc87c
alex <766f6964+github@gmail.com>
2025-11-27 14:39:18 +0100

    import consts from date-filters

--- quasar-frontend-ts/src/composables/useDateFilter.ts
+++ quasar-frontend-ts/src/composables/useDateFilter.ts
@@ -1,23 +1,11 @@
 import { computed, reactive, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import { date } from "quasar";
+import { availableSelectOptions, dateMask } from "src/types/date-filters";
 
 export function useDateFilter() {
   const { t } = useI18n();
 
-  const availableSelectOptions = [
-    { labelKey: "Any date", value: 0 },
-    { labelKey: "Today", value: 1 },
-    { labelKey: "Yesterday", value: 2 },
-    { labelKey: "Last 7 days", value: 3 },
-    { labelKey: "Last 15 days", value: 4 },
-    { labelKey: "Last 31 days", value: 5 },
-    { labelKey: "Last 365 days", value: 6 },
-    { labelKey: "Fixed date", value: 7 },
-    { labelKey: "From date", value: 8 },
-    { labelKey: "To date", value: 9 },
-    { labelKey: "Between dates", value: 10 },
-  ];
 
   const dateFilterTypeOptions = computed(() =>
     availableSelectOptions.map((option) => ({
@@ -25,8 +13,6 @@ export function useDateFilter() {
       label: t(option.labelKey),
     })),
   );
-
-  const dateMask = "YYYY/MM/DD";
 
   const getFormattedDate = (daysToSubtract = 0) => {
     return date.formatDate(
