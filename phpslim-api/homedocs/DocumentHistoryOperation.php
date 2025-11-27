124dd67883cef64a643800c4e5b4d0a804bc690d
alex <766f6964+github@gmail.com>
2025-11-27 23:55:37 +0100

    php-cs-fixer changes

--- phpslim-api/homedocs/DocumentHistoryOperation.php
+++ phpslim-api/homedocs/DocumentHistoryOperation.php
@@ -9,8 +9,12 @@ class DocumentHistoryOperation
 // TODO: enum ???
 {
     public const OPERATION_NONE = 0;
+
     public const OPERATION_ADD_DOCUMENT = 1;
+
     public const OPERATION_UPDATE_DOCUMENT = 2;
 
-    public function __construct(public int $createdAtTimestamp, public int $operationType) {}
+    public function __construct(public int $createdAtTimestamp, public int $operationType)
+    {
+    }
 }
--- phpslim-api/homedocs/Note.php
+++ phpslim-api/homedocs/Note.php
@@ -6,5 +6,7 @@ namespace HomeDocs;
 
 class Note
 {
-    public function __construct(public ?string $id = null, public ?int $createdAtTimestamp = null, public ?string $body = null) {}
+    public function __construct(public ?string $id = null, public ?int $createdAtTimestamp = null, public ?string $body = null)
+    {
+    }
 }
