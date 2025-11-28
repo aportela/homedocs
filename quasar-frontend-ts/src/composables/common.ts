9eefc885482adcaa617465cee0bd11e31c5a8a30
alex <766f6964+github@gmail.com>
2025-11-28 19:13:05 +0100

    added method for checking browser pdf preview support

--- quasar-frontend-ts/src/composables/common.ts
+++ quasar-frontend-ts/src/composables/common.ts
@@ -2,4 +2,23 @@ const escapeRegExp = (string) => {
   return string.replace(/[.*+?^=!:${}()|\[\]\/\\]/g, "\\$&");
 };
 
-export { escapeRegExp };
+const isPdfSupportedInIframe = (): Promise<boolean> => {
+  return new Promise<boolean>((resolve) => {
+    const iframe = document.createElement('iframe');
+    iframe.style.display = 'none';
+    // credits: https://www.emcken.dk/programming/2024/01/12/very-small-pdf-for-testing/
+    const base64PDF: string = "data:application/pdf;base64,JVBERi0xLjQKMSAwIG9iago8PC9UeXBlIC9DYXRhbG9nCi9QYWdlcyAyIDAgUgo+PgplbmRvYmoK MiAwIG9iago8PC9UeXBlIC9QYWdlcwovS2lkcyBbMyAwIFJdCi9Db3VudCAxCj4+CmVuZG9iagoz IDAgb2JqCjw8L1R5cGUgL1BhZ2UKL1BhcmVudCAyIDAgUgovTWVkaWFCb3ggWzAgMCA1OTUgODQy XQovQ29udGVudHMgNSAwIFIKL1Jlc291cmNlcyA8PC9Qcm9jU2V0IFsvUERGIC9UZXh0XQovRm9u dCA8PC9GMSA0IDAgUj4+Cj4+Cj4+CmVuZG9iago0IDAgb2JqCjw8L1R5cGUgL0ZvbnQKL1N1YnR5 cGUgL1R5cGUxCi9OYW1lIC9GMQovQmFzZUZvbnQgL0hlbHZldGljYQovRW5jb2RpbmcgL01hY1Jv bWFuRW5jb2RpbmcKPj4KZW5kb2JqCjUgMCBvYmoKPDwvTGVuZ3RoIDUzCj4+CnN0cmVhbQpCVAov RjEgMjAgVGYKMjIwIDQwMCBUZAooRHVtbXkgUERGKSBUagpFVAplbmRzdHJlYW0KZW5kb2JqCnhy ZWYKMCA2CjAwMDAwMDAwMDAgNjU1MzUgZgowMDAwMDAwMDA5IDAwMDAwIG4KMDAwMDAwMDA2MyAw MDAwMCBuCjAwMDAwMDAxMjQgMDAwMDAgbgowMDAwMDAwMjc3IDAwMDAwIG4KMDAwMDAwMDM5MiAw MDAwMCBuCnRyYWlsZXIKPDwvU2l6ZSA2Ci9Sb290IDEgMCBSCj4+CnN0YXJ0eHJlZgo0OTUKJSVF T0YK";
+    iframe.src = base64PDF;
+    document.body.appendChild(iframe);
+    iframe.onload = () => {
+      document.body.removeChild(iframe);
+      resolve(true);
+    };
+    iframe.onerror = () => {
+      document.body.removeChild(iframe);
+      resolve(false);
+    };
+  });
+};
+
+export { escapeRegExp, isPdfSupportedInIframe };
