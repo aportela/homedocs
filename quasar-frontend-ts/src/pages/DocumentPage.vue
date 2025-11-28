260680009398fc869eb2190a1de78b771e1e4da6
alex <766f6964+github@gmail.com>
2025-11-28 22:23:01 +0100

    warnings fixed

--- quasar-frontend-ts/src/pages/DocumentPage.vue
+++ quasar-frontend-ts/src/pages/DocumentPage.vue
@@ -13,13 +13,13 @@ import { default as DocumentForm } from "src/components/Forms/DocumentForm.vue"
 const router = useRouter();
 const currentRoute = useRoute();
 
-const documentId = ref(currentRoute.name == "document" ? currentRoute.params?.id || null : null);
+const documentId = ref<string | null>(currentRoute.name == "document" ? String(currentRoute.params?.id) || null : null);
 
 router.beforeEach((to) => {
   if (to.name == "newDocument") {
     documentId.value = null;
   } else if (to.name == "document" && to.params.id) {
-    documentId.value = to.params.id;
+    documentId.value = String(to.params.id);
   } else {
     // TODO: error
   }
