9be3f3fbaa31382a7415b3560e660c33a8d6f366
alex <766f6964+github@gmail.com>
2025-11-28 01:43:48 +0100

    added interface & class

--- quasar-frontend-ts/src/composables/useDateFilter.ts
+++ quasar-frontend-ts/src/composables/useDateFilter.ts
@@ -1,167 +1,226 @@
 import { computed, reactive, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import { date } from "quasar";
-import { availableSelectOptions, dateMask } from "src/types/date-filters";
+import { type DateFilterOptionBaseType as DateFilterOptionBaseTypeInterface, availableSelectOptions, dateMask } from "src/types/date-filters";
+
+interface DateFilterTypeOption {
+  labelKey: string;
+  value: number;
+  label: string;
+}
+
+interface DateFilterInstance {
+  filterType: DateFilterTypeOption;
+  formattedDate: {
+    fixed: string | null;
+    from: string | null;
+    to: string | null;
+  };
+  timestamps: {
+    from: number | null;
+    to: number | null;
+  };
+  state: {
+    hasFrom: boolean;
+    hasTo: boolean;
+    hasFixed: boolean;
+    hasValue: boolean;
+    denyChanges: boolean;
+  };
+  // UGLY HACK to skip clearing/reseting values on filterType watchers
+  skipClearOnRecalc: {
+    from: boolean;
+    to: boolean;
+    fixed: boolean;
+  };
+};
+
+const getFormattedDate = (daysToSubtract = 0) => {
+  return date.formatDate(
+    date.addToDate(Date.now(), { days: daysToSubtract }),
+    dateMask,
+  );
+};
+
+class DateFilterInstanceClass implements DateFilterInstance {
+  filterType: DateFilterTypeOption;
+  formattedDate: {
+    fixed: string | null;
+    from: string | null;
+    to: string | null;
+  };
+  timestamps: {
+    from: number | null;
+    to: number | null;
+  };
+  state: {
+    hasFrom: boolean;
+    hasTo: boolean;
+    hasFixed: boolean;
+    hasValue: boolean;
+    denyChanges: boolean;
+  };
+  // UGLY HACK to skip clearing/reseting values on filterType watchers
+  skipClearOnRecalc: {
+    from: boolean;
+    to: boolean;
+    fixed: boolean;
+  };
+
+  constructor(filterType: DateFilterTypeOption) {
+    this.filterType = filterType; //dateFilterTypeOptions.value[0]!;
+    this.formattedDate = {
+      fixed: null,
+      from: null,
+      to: null,
+    };
+    this.timestamps = {
+      from: null,
+      to: null,
+    };
+    this.state = {
+      hasFrom: false,
+      hasTo: false,
+      hasFixed: false,
+      hasValue: false,
+      denyChanges: false,
+    };
+    // UGLY HACK to skip clearing/reseting values on filterType watchers
+    this.skipClearOnRecalc = {
+      from: false,
+      to: false,
+      fixed: false,
+    };
+  }
+
+  onRecalcDates() {
+    if (!this.skipClearOnRecalc.from) {
+      this.formattedDate.from = null;
+    }
+    if (!this.skipClearOnRecalc.to) {
+      this.formattedDate.to = null;
+    }
+    if (!this.skipClearOnRecalc.fixed) {
+      this.formattedDate.fixed = null;
+    }
+    this.skipClearOnRecalc.from = false;
+    this.skipClearOnRecalc.to = false;
+    this.skipClearOnRecalc.fixed = false;
+    this.timestamps.from = null;
+    this.timestamps.to = null;
+    // generate model/formatted (visible) dates
+    switch (this.filterType?.value) {
+      // NONE
+      case 0:
+        this.formattedDate.from = null;
+        this.formattedDate.to = null;
+        this.formattedDate.fixed = null;
+        break;
+      // TODAY
+      case 1:
+        this.formattedDate.fixed = getFormattedDate();
+        break;
+      // YESTERDAY
+      case 2:
+        this.formattedDate.fixed = getFormattedDate(-1);
+        break;
+      // LAST 7 DAYS
+      case 3:
+        this.formattedDate.from = getFormattedDate(-7);
+        this.formattedDate.to = getFormattedDate();
+        break;
+      // LAST 15 DAYS
+      case 4:
+        this.formattedDate.from = getFormattedDate(-15);
+        this.formattedDate.to = getFormattedDate();
+        break;
+      // LAST 31 DAYS
+      case 5:
+        this.formattedDate.from = getFormattedDate(-31);
+        this.formattedDate.to = getFormattedDate();
+        break;
+      // LAST 365 DAYS
+      case 6:
+        this.formattedDate.from = getFormattedDate(-365);
+        this.formattedDate.to = getFormattedDate();
+        break;
+      // FIXED DATE
+      case 7:
+        break;
+      // FROM DATE
+      case 8:
+        //this.formattedDate.from = getFormattedDate();
+        break;
+      // TO DATE
+      case 9:
+        //this.formattedDate.to = getFormattedDate();
+        break;
+      // BETWEEN DATES
+      case 10:
+        //this.formattedDate.from = getFormattedDate();
+        //this.formattedDate.to = getFormattedDate();
+        break;
+    }
+  };
+
+  onRecalcTimestamps() {
+    // generate API timestamps (real filters)
+    if (this.formattedDate.fixed) {
+      this.timestamps.from = Number(date.formatDate(
+        date.adjustDate(
+          date.extractDate(this.formattedDate.fixed, dateMask),
+          { hour: 0, minute: 0, second: 0, millisecond: 0 },
+        ),
+        "x", // timestamp in ms
+      ));
+      this.timestamps.to = Number(date.formatDate(
+        date.adjustDate(
+          date.extractDate(this.formattedDate.fixed, dateMask),
+          { hour: 23, minute: 59, second: 59, millisecond: 999 },
+        ),
+        "x", // timestamp in ms
+      ));
+    } else {
+      if (this.formattedDate.from) {
+        this.timestamps.from = Number(date.formatDate(
+          date.adjustDate(
+            date.extractDate(this.formattedDate.from, "YYYY/MM/DD"),
+            { hour: 0, minute: 0, second: 0, millisecond: 0 },
+          ),
+          "x", // timestamp in ms
+        ));
+      }
+      if (this.formattedDate.to) {
+        this.timestamps.to = Number(date.formatDate(
+          date.adjustDate(
+            date.extractDate(this.formattedDate.to, "YYYY/MM/DD"),
+            { hour: 23, minute: 59, second: 59, millisecond: 999 },
+          ),
+          "x", // timestamp in ms
+        ));
+      }
+    }
+  };
+};
 
 export function useDateFilter() {
   const { t } = useI18n();
 
-
   const dateFilterTypeOptions = computed(() =>
-    availableSelectOptions.map((option) => ({
+    availableSelectOptions.map((option: DateFilterOptionBaseTypeInterface) => ({
       ...option,
       label: t(option.labelKey),
     })),
   );
 
-  const getFormattedDate = (daysToSubtract = 0) => {
-    return date.formatDate(
-      date.addToDate(Date.now(), { days: daysToSubtract }),
-      dateMask,
-    );
-  };
-
   const getDateFilterInstance = () => {
-    const dateFilter = reactive({
-      filterType: dateFilterTypeOptions.value[0],
-      formattedDate: {
-        fixed: null,
-        from: null,
-        to: null,
-      },
-      timestamps: {
-        from: null,
-        to: null,
-      },
-      state: {
-        hasFrom: false,
-        hasTo: false,
-        hasFixed: false,
-        hasValue: false,
-        denyChanges: false,
-      },
-      // UGLY HACK to skip clearing/reseting values on filterType watchers
-      skipClearOnRecalc: {
-        from: false,
-        to: false,
-        fixed: false,
-      },
-    });
-
-    const onRecalcDates = () => {
-      if (!dateFilter.skipClearOnRecalc.from) {
-        dateFilter.formattedDate.from = null;
-      }
-      if (!dateFilter.skipClearOnRecalc.to) {
-        dateFilter.formattedDate.to = null;
-      }
-      if (!dateFilter.skipClearOnRecalc.fixed) {
-        dateFilter.formattedDate.fixed = null;
-      }
-      dateFilter.skipClearOnRecalc.from = false;
-      dateFilter.skipClearOnRecalc.to = false;
-      dateFilter.skipClearOnRecalc.fixed = false;
-      dateFilter.timestamps.from = null;
-      dateFilter.timestamps.to = null;
-      // generate model/formatted (visible) dates
-      switch (dateFilter.filterType?.value) {
-        // NONE
-        case 0:
-          dateFilter.formattedDate.from = null;
-          dateFilter.formattedDate.to = null;
-          dateFilter.formattedDate.fixed = null;
-          break;
-        // TODAY
-        case 1:
-          dateFilter.formattedDate.fixed = getFormattedDate();
-          break;
-        // YESTERDAY
-        case 2:
-          dateFilter.formattedDate.fixed = getFormattedDate(-1);
-          break;
-        // LAST 7 DAYS
-        case 3:
-          dateFilter.formattedDate.from = getFormattedDate(-7);
-          dateFilter.formattedDate.to = getFormattedDate();
-          break;
-        // LAST 15 DAYS
-        case 4:
-          dateFilter.formattedDate.from = getFormattedDate(-15);
-          dateFilter.formattedDate.to = getFormattedDate();
-          break;
-        // LAST 31 DAYS
-        case 5:
-          dateFilter.formattedDate.from = getFormattedDate(-31);
-          dateFilter.formattedDate.to = getFormattedDate();
-          break;
-        // LAST 365 DAYS
-        case 6:
-          dateFilter.formattedDate.from = getFormattedDate(-365);
-          dateFilter.formattedDate.to = getFormattedDate();
-          break;
-        // FIXED DATE
-        case 7:
-          break;
-        // FROM DATE
-        case 8:
-          //dateFilter.formattedDate.from = getFormattedDate();
-          break;
-        // TO DATE
-        case 9:
-          //dateFilter.formattedDate.to = getFormattedDate();
-          break;
-        // BETWEEN DATES
-        case 10:
-          //dateFilter.formattedDate.from = getFormattedDate();
-          //dateFilter.formattedDate.to = getFormattedDate();
-          break;
-      }
-    };
-
-    const onRecalcTimestamps = () => {
-      // generate API timestamps (real filters)
-      if (dateFilter.formattedDate.fixed) {
-        dateFilter.timestamps.from = Number(date.formatDate(
-          date.adjustDate(
-            date.extractDate(dateFilter.formattedDate.fixed, dateMask),
-            { hour: 0, minute: 0, second: 0, millisecond: 0 },
-          ),
-          "x", // timestamp in ms
-        ));
-        dateFilter.timestamps.to = Number(date.formatDate(
-          date.adjustDate(
-            date.extractDate(dateFilter.formattedDate.fixed, dateMask),
-            { hour: 23, minute: 59, second: 59, millisecond: 999 },
-          ),
-          "x", // timestamp in ms
-        ));
-      } else {
-        if (dateFilter.formattedDate.from) {
-          dateFilter.timestamps.from = Number(date.formatDate(
-            date.adjustDate(
-              date.extractDate(dateFilter.formattedDate.from, "YYYY/MM/DD"),
-              { hour: 0, minute: 0, second: 0, millisecond: 0 },
-            ),
-            "x", // timestamp in ms
-          ));
-        }
-        if (dateFilter.formattedDate.to) {
-          dateFilter.timestamps.to = Number(date.formatDate(
-            date.adjustDate(
-              date.extractDate(dateFilter.formattedDate.to, "YYYY/MM/DD"),
-              { hour: 23, minute: 59, second: 59, millisecond: 999 },
-            ),
-            "x", // timestamp in ms
-          ));
-        }
-      }
-    };
+    const dateFilter = reactive<DateFilterInstanceClass>(new DateFilterInstanceClass(dateFilterTypeOptions.value[0]!));
 
     watch(
       () => dateFilter.formattedDate.from,
       (value) => {
         dateFilter.state.hasValue = !!value;
-        onRecalcTimestamps();
+        dateFilter.onRecalcTimestamps();
       },
     );
 
@@ -169,7 +228,7 @@ export function useDateFilter() {
       () => dateFilter.formattedDate.to,
       (value) => {
         dateFilter.state.hasValue = !!value;
-        onRecalcTimestamps();
+        dateFilter.onRecalcTimestamps();
       },
     );
 
@@ -177,7 +236,7 @@ export function useDateFilter() {
       () => dateFilter.formattedDate.fixed,
       (value) => {
         dateFilter.state.hasValue = !!value;
-        onRecalcTimestamps();
+        dateFilter.onRecalcTimestamps();
       },
     );
 
@@ -185,7 +244,7 @@ export function useDateFilter() {
     // so we are watching changes on first option label and when this label translation changes
     // we translate again the label assigned to the model
     watch(
-      () => dateFilterTypeOptions.value[0].label,
+      () => dateFilterTypeOptions.value[0]!.label,
       () => {
         dateFilter.filterType.label = t(dateFilter.filterType.labelKey);
       },
@@ -206,10 +265,10 @@ export function useDateFilter() {
             filterType.value,
           );
         } else {
-          dateFilter.filterType = dateFilterTypeOptions.value[0];
+          dateFilter.filterType = dateFilterTypeOptions.value[0]!;
         }
-        onRecalcDates();
-        onRecalcTimestamps();
+        dateFilter.onRecalcDates();
+        dateFilter.onRecalcTimestamps();
       },
     );
 
@@ -217,8 +276,8 @@ export function useDateFilter() {
   };
 
   return {
-    dateMask,
     dateFilterTypeOptions,
     getDateFilterInstance,
   };
 }
+export { type DateFilterInstance, DateFilterInstanceClass };
