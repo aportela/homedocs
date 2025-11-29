7db4333e4b79c2007d614c092ab976d0182cb9aa
alex <766f6964+github@gmail.com>
2025-11-29 01:44:54 +0100

    new classes for document search filter

---
+++ phpslim-api/homedocs/DocumentSearchDatesFilter.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HomeDocs;
+
+class DocumentSearchDatesFilter
+{
+    public readonly int $createdAtFromTimestamp;
+    public readonly int $createdAtToTimestamp;
+    public readonly int $lastUpdateAtFromTimestamp;
+    public readonly int $lastUpdateAtToTimestamp;
+    public readonly int $updatedAtFromTimestamp;
+    public readonly int $updatedAtToTimestamp;
+
+    public function __construct(array $routeParams = [])
+    {
+        $this->createdAtFromTimestamp = $this->getDateFilterFromParams($routeParams, "createdAt", "from") ?? 0;
+        $this->createdAtToTimestamp = $this->getDateFilterFromParams($routeParams, "createdAt", "to") ?? 0;
+        $this->lastUpdateAtFromTimestamp = $this->getDateFilterFromParams($routeParams, "lastUpdateAt", "from") ?? 0;
+        $this->lastUpdateAtToTimestamp = $this->getDateFilterFromParams($routeParams, "lastUpdateAt", "to") ?? 0;
+        $this->updatedAtFromTimestamp = $this->getDateFilterFromParams($routeParams, "updatedAt", "from") ?? 0;
+        $this->updatedAtToTimestamp = $this->getDateFilterFromParams($routeParams, "updatedAt", "to") ?? 0;
+    }
+
+    private function getDateFilterFromParams(array $params = [], string $dateType, $timestampType): int|null
+    {
+        if (
+            array_key_exists("filter", $params) &&
+            is_array($params["filter"]) &&
+            array_key_exists("dates", $params["filter"]) &&
+            is_array($params["filter"]["dates"]) &&
+            array_key_exists($dateType, $params["filter"]["dates"]) &&
+            is_array($params["filter"]["dates"][$dateType]) &&
+            array_key_exists("timestamps", $params["filter"]["dates"][$dateType]) &&
+            is_array($params["filter"]["dates"][$dateType]["timestamps"]) &&
+            array_key_exists($timestampType, $params["filter"]["dates"][$dateType]["timestamps"]) &&
+            is_numeric($params["filter"]["dates"][$dateType]["timestamps"][$timestampType])
+        ) {
+            return (intval($params["filter"]["dates"][$dateType]["timestamps"][$timestampType]));
+        } else {
+            return (null);
+        }
+    }
+}
---
+++ phpslim-api/homedocs/DocumentSearchTextFilter.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HomeDocs;
+
+class DocumentSearchTextFilter
+{
+    public readonly ?string $title;
+    public readonly ?string $description;
+    public readonly ?string $notesBody;
+    public readonly ?string $attachmentsFilename;
+
+    public function __construct(array $routeParams = [])
+    {
+        $this->title = $this->getTextFilterFromParams($routeParams, "title");
+        $this->description = $this->getTextFilterFromParams($routeParams, "description");
+        $this->notesBody = $this->getTextFilterFromParams($routeParams, "notesBody");
+        $this->attachmentsFilename = $this->getTextFilterFromParams($routeParams, "attachmentsFilename");
+    }
+
+    private function getTextFilterFromParams(array $routeParams, string $textFilterType): string | null
+    {
+        return (
+            array_key_exists("filter", $routeParams) &&
+            is_array($routeParams["filter"]) &&
+            array_key_exists("text", $routeParams["filter"]) &&
+            is_array(($routeParams["filter"]["text"])) &&
+            array_key_exists($textFilterType, $routeParams["filter"]["text"]) &&
+            is_string($routeParams["filter"]["text"][$textFilterType])
+            ?
+            $routeParams["filter"]["text"][$textFilterType]
+            :
+            null
+        );
+    }
+}
