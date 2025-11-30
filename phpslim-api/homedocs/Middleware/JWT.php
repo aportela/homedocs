a66bd58c2c52f7dfb106553e6fa1f26dbebe1e4c
alex <766f6964+github@gmail.com>
2025-11-30 22:23:57 +0100

    replaced jwt headers with Authorization bearer header

--- phpslim-api/homedocs/Middleware/JWT.php
+++ phpslim-api/homedocs/Middleware/JWT.php
@@ -37,42 +37,31 @@ class JWT
      */
     public function __invoke(\Psr\Http\Message\ServerRequestInterface $serverRequest, \Psr\Http\Server\RequestHandlerInterface $requestHandler): \Psr\Http\Message\ResponseInterface
     {
-        $clientHeaderJWT = $serverRequest->hasHeader("HOMEDOCS-JWT") ? $serverRequest->getHeader("HOMEDOCS-JWT")[0] : null;
-        // user not logged (or session lost) && jwt auth header found => re-auth with jwt
-        if (!\HomeDocs\UserSession::isLogged() && !empty($clientHeaderJWT)) {
-            // try decoding jwt data
-            $jwt = new \HomeDocs\JWT($this->logger, $this->passphrase);
-            $decoded = $jwt->decode($clientHeaderJWT);
-            if (property_exists($decoded, "data") && is_object($decoded->data) && property_exists($decoded->data, "userId") && is_string($decoded->data->userId) && property_exists($decoded->data, "email") && is_string($decoded->data->email)) {
-                $this->logger->notice("JWT valid data decoded", [print_r($decoded->data, true)]);
-                $user = new \HomeDocs\User($decoded->data->userId);
-                if ($user->exists($this->dbh)) {
-                    \HomeDocs\UserSession::set($decoded->data->userId, $decoded->data->email);
-                } else {
-                    throw new \HomeDocs\Exception\NotFoundException("userId");
-                }
-            } else {
-                throw new \HomeDocs\Exception\InvalidParamsException("jwt");
-            }
-
-            $response = $requestHandler->handle($serverRequest);
-            return $response->withHeader("HOMEDOCS-JWT", $clientHeaderJWT);
+        // extract the Bearer Token from the Authorization header
+        $authorizationHeader = $serverRequest->getHeader('Authorization');
+        if (empty($authorizationHeader)) {
+            throw new \HomeDocs\Exception\UnauthorizedException();
         } else {
-            $response = $requestHandler->handle($serverRequest);
-            if (empty($clientHeaderJWT) && \HomeDocs\UserSession::isLogged()) {
-                $payload = [
-                    "userId" => $_SESSION["userId"] ?? null,
-                    "email" => $_SESSION["email"] ?? null,
-                ];
+            $bearerToken = trim(preg_replace('/Bearer\s/', '', $authorizationHeader[0]));
+            // user not logged (or session lost) && jwt auth bearer header found => re-validate session with jwt
+            if (!\HomeDocs\UserSession::isLogged() && !empty($bearerToken)) {
+                // try decoding jwt data
                 $jwt = new \HomeDocs\JWT($this->logger, $this->passphrase);
-                $clientHeaderJWT = $jwt->encode($payload);
+                $decoded = $jwt->decode($bearerToken);
+                if (property_exists($decoded, "data") && is_object($decoded->data) && property_exists($decoded->data, "userId") && is_string($decoded->data->userId) && property_exists($decoded->data, "email") && is_string($decoded->data->email)) {
+                    $this->logger->notice("JWT valid data decoded", [print_r($decoded->data, true)]);
+                    $user = new \HomeDocs\User($decoded->data->userId);
+                    if ($user->exists($this->dbh)) {
+                        \HomeDocs\UserSession::set($decoded->data->userId, $decoded->data->email);
+                    } else {
+                        throw new \HomeDocs\Exception\NotFoundException("userId");
+                    }
+                } else {
+                    throw new \HomeDocs\Exception\InvalidParamsException("jwt");
+                }
             }
-
-            if ($clientHeaderJWT && \HomeDocs\UserSession::isLogged()) {
-                return $response->withHeader("HOMEDOCS-JWT", $clientHeaderJWT);
-            } else {
-                return ($response);
-            }
+            $response = $requestHandler->handle($serverRequest);
+            return ($response);
         }
     }
 }
