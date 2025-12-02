7b9cf4603716c07c1db7e533e07980e56100b7f2
alex <766f6964+github@gmail.com>
2025-12-02 10:35:39 +0100

    refactored axios instance property

--- quasar-frontend/src/boot/axios.ts
+++ quasar-frontend/src/boot/axios.ts
@@ -2,5 +2,5 @@ import { defineBoot } from '#q-app/wrappers';
 import { axiosInstance } from 'src/composables/axios';
 
 export default defineBoot(({ app }) => {
-  app.config.globalProperties.$api = axiosInstance;
+  app.config.globalProperties.$axios = axiosInstance;
 });
