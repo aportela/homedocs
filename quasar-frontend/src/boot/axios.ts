2c8dbd54a34d37de91df1f15fec515dff85695fe
alex <766f6964+github@gmail.com>
2025-12-04 14:57:08 +0100

    added module declaration

--- quasar-frontend/src/boot/axios.ts
+++ quasar-frontend/src/boot/axios.ts
@@ -1,6 +1,25 @@
 import { defineBoot } from '#q-app/wrappers';
-import { axiosInstance } from 'src/composables/axios';
+import axios, { type AxiosInstance } from 'axios';
+
+declare module 'vue' {
+  interface ComponentCustomProperties {
+    $axios: AxiosInstance;
+    $api: AxiosInstance;
+  }
+}
+
+// Be careful when using SSR for cross-request state pollution
+// due to creating a Singleton instance here;
+// If any client changes this (global) instance, it might be a
+// good idea to move this instance creation inside of the
+// "export default () => {}" function below (which runs individually
+// for each client)
 
 export default defineBoot(({ app }) => {
-  app.config.globalProperties.$axios = axiosInstance;
+  // for use inside Vue files (Options API) through this.$axios and this.$api
+
+  app.config.globalProperties.$axios = axios;
+  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
+  //       so you won't necessarily have to import axios in each vue file
+
 });
