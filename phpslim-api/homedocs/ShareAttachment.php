22d5b4b4ed0728c0ed399e5dcdd8d1cb6e633c74
alex <766f6964+github@gmail.com>
2025-12-11 19:18:54 +0100

    added attachmentId param on delete/update methods

--- phpslim-api/homedocs/ShareAttachment.php
+++ phpslim-api/homedocs/ShareAttachment.php
@@ -62,74 +62,51 @@ class ShareAttachment
         }
     }
 
-    public function incrementAccessCount(\aportela\DatabaseWrapper\DB $db): bool
+    public function update(\aportela\DatabaseWrapper\DB $db, string $attachmentId): bool
     {
-        return ($db->execute(
-            "
-                UPDATE SHARED_ATTACHMENT
-                SET
-                    access_count = access_count + 1
-                WHERE
-                    id = :id
-                AND
-                    enabled = 1
-                AND
-                    (
-                        access_limit = 0
-                        OR
-                        access_count < access_limit
-                    )
-                AND
-                    (
-                        etime = 0
-                        OR
-                        etime < :current_timestamp
-                    )
-            ",
-            [
-                new \aportela\DatabaseWrapper\Param\StringParam(":id", $this->id),
-                new \aportela\DatabaseWrapper\Param\IntegerParam(":current_timestamp", intval(microtime(true) * 1000)),
-            ]
-        ));
+        if (! empty($attachmentId) && mb_strlen($attachmentId) === 36) {
+            return ($db->execute(
+                "
+                    UPDATE SHARED_ATTACHMENT
+                    SET
+                        etime = :etime,
+                        access_limit = :access_limit,
+                        enabled = :enabled
+                    WHERE
+                        attachment_id = :attachment_id
+                    AND
+                        cuid = :cuid
+                ",
+                [
+                    new \aportela\DatabaseWrapper\Param\StringParam(":attachment_id", $attachmentId),
+                    new \aportela\DatabaseWrapper\Param\StringParam(":cuid", \HomeDocs\UserSession::getUserId()),
+                    new \aportela\DatabaseWrapper\Param\IntegerParam(":etime", $this->expiresAtTimestamp > 0 ? $this->expiresAtTimestamp : 0),
+                    new \aportela\DatabaseWrapper\Param\IntegerParam(":access_limit", $this->accessLimit > 0 ? $this->accessLimit : 0),
+                    new \aportela\DatabaseWrapper\Param\IntegerParam(":enabled", $this->enabled ? 1 : 0),
+                ]
+            ));
+        } else {
+            throw new \HomeDocs\Exception\InvalidParamsException("attachmentId");
+        }
     }
 
-    public function update(\aportela\DatabaseWrapper\DB $db): bool
+    public function delete(\aportela\DatabaseWrapper\DB $db, string $attachmentId): bool
     {
-        return ($db->execute(
-            "
-                UPDATE SHARED_ATTACHMENT
-                SET
-                    etime = :etime,
-                    access_limit = :access_limit,
-                    enabled = :enabled
-                WHERE
-                    id = :id
-                AND
-                    cuid = :cuid
-            ",
-            [
-                new \aportela\DatabaseWrapper\Param\StringParam(":id", $this->id),
-                new \aportela\DatabaseWrapper\Param\StringParam(":cuid", \HomeDocs\UserSession::getUserId()),
-                new \aportela\DatabaseWrapper\Param\IntegerParam(":etime", $this->expiresAtTimestamp > 0 ? $this->expiresAtTimestamp : 0),
-                new \aportela\DatabaseWrapper\Param\IntegerParam(":access_limit", $this->accessLimit > 0 ? $this->accessLimit : 0),
-                new \aportela\DatabaseWrapper\Param\IntegerParam(":enabled", $this->enabled ? 1 : 0),
-            ]
-        ));
-    }
-
-    public function delete(\aportela\DatabaseWrapper\DB $db): bool
-    {
-        return ($db->execute(
-            "
-                DELETE
-                FROM SHARED_ATTACHMENT
-                WHERE id = :id AND cuid = :cuid
-            ",
-            [
-                new \aportela\DatabaseWrapper\Param\StringParam(":id", $this->id),
-                new \aportela\DatabaseWrapper\Param\StringParam(":cuid", \HomeDocs\UserSession::getUserId())
-            ]
-        ));
+        if (! empty($attachmentId) && mb_strlen($attachmentId) === 36) {
+            return ($db->execute(
+                "
+                    DELETE
+                    FROM SHARED_ATTACHMENT
+                    WHERE attachment_id = :attachment_id AND cuid = :cuid
+                ",
+                [
+                    new \aportela\DatabaseWrapper\Param\StringParam(":attachment_id", $attachmentId),
+                    new \aportela\DatabaseWrapper\Param\StringParam(":cuid", \HomeDocs\UserSession::getUserId())
+                ]
+            ));
+        } else {
+            throw new \HomeDocs\Exception\InvalidParamsException("attachmentId");
+        }
     }
 
     public function get(\aportela\DatabaseWrapper\DB $db, string|null $attachmentId)
@@ -202,5 +179,36 @@ class ShareAttachment
     public function isExpired()
     {
         return ($this->expiresAtTimestamp > 0 && $this->expiresAtTimestamp < intval(microtime(true) * 1000));
+    }
+
+    public function incrementAccessCount(\aportela\DatabaseWrapper\DB $db): bool
+    {
+        return ($db->execute(
+            "
+                UPDATE SHARED_ATTACHMENT
+                SET
+                    access_count = access_count + 1
+                WHERE
+                    id = :id
+                AND
+                    enabled = 1
+                AND
+                    (
+                        access_limit = 0
+                        OR
+                        access_count < access_limit
+                    )
+                AND
+                    (
+                        etime = 0
+                        OR
+                        etime < :current_timestamp
+                    )
+            ",
+            [
+                new \aportela\DatabaseWrapper\Param\StringParam(":id", $this->id),
+                new \aportela\DatabaseWrapper\Param\IntegerParam(":current_timestamp", intval(microtime(true) * 1000)),
+            ]
+        ));
     }
 }
