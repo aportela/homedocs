4ac36469b1d21af6bc62429be409978f43540a8a
alex <766f6964+github@gmail.com>
2025-12-15 16:58:11 +0100

    changed state interface struct, added expiration getters & actions

--- quasar-frontend/src/stores/session.ts
+++ quasar-frontend/src/stores/session.ts
@@ -1,5 +1,8 @@
 import { defineStore, acceptHMRUpdate } from 'pinia';
+import { AxiosError } from 'axios';
 import { createStorageEntry } from 'src/composables/localStorage';
+import { type GetNewAccessTokenResponse as GetNewAccessTokenResponseInterface } from 'src/types/apiResponses';
+import { api } from 'src/composables/api';
 
 const localStorageShowToolTips = createStorageEntry<boolean>('showToolTips', false);
 
@@ -21,74 +24,116 @@ const localStorageSearchDialogResultsPage = createStorageEntry<number>(
 );
 
 interface State {
-  tokens: {
-    access: string | null;
-  };
-  other: {
-    toolTips: boolean;
-    alwaysOpenUploadDialog: boolean;
-    dateFormat: string;
-    dateTimeFormat: string;
-    searchDialogResultsPage: number;
+  session: {
+    accessToken: {
+      token: string | null;
+      expiresAtTimestamp: number | null;
+    };
+    other: {
+      toolTips: boolean;
+      alwaysOpenUploadDialog: boolean;
+      dateFormat: string;
+      dateTimeFormat: string;
+      searchDialogResultsPage: number;
+    };
   };
 }
 
 export const useSessionStore = defineStore('session', {
   state: (): State => ({
-    tokens: {
-      access: null,
-    },
-    other: {
-      toolTips: localStorageShowToolTips.get(),
-      alwaysOpenUploadDialog: localStorageAlwaysOpenUploadDialog.get(),
-      dateFormat: localStorageDateFormat.get(),
-      dateTimeFormat: localStorageDateTimeFormat.get(),
-      searchDialogResultsPage: localStorageSearchDialogResultsPage.get(),
+    session: {
+      accessToken: {
+        token: null,
+        expiresAtTimestamp: null,
+      },
+      other: {
+        toolTips: localStorageShowToolTips.get(),
+        alwaysOpenUploadDialog: localStorageAlwaysOpenUploadDialog.get(),
+        dateFormat: localStorageDateFormat.get(),
+        dateTimeFormat: localStorageDateTimeFormat.get(),
+        searchDialogResultsPage: localStorageSearchDialogResultsPage.get(),
+      },
     },
   }),
   getters: {
-    hasAccessToken: (state: State): boolean => state.tokens.access !== null,
-    accessToken: (state: State): string | null => state.tokens.access,
-    toolTipsEnabled: (state: State): boolean => state.other.toolTips,
-    openUploadDialog: (state: State): boolean => state.other.alwaysOpenUploadDialog,
-    savedDateFormat: (state: State): string => state.other.dateFormat,
-    savedDateTimeFormat: (state: State): string => state.other.dateTimeFormat,
-    searchDialogResultsPage: (state: State): number => state.other.searchDialogResultsPage,
+    hasAccessToken: (state: State): boolean =>
+      state.session.accessToken.token !== null &&
+      state.session.accessToken.expiresAtTimestamp !== null,
+    accessToken: (state: State): string | null => state.session.accessToken.token,
+    accessTokenExpirationTimestamp: (state): number | null =>
+      state.session.accessToken.expiresAtTimestamp,
+    toolTipsEnabled: (state: State): boolean => state.session.other.toolTips,
+    openUploadDialog: (state: State): boolean => state.session.other.alwaysOpenUploadDialog,
+    savedDateFormat: (state: State): string => state.session.other.dateFormat,
+    savedDateTimeFormat: (state: State): string => state.session.other.dateTimeFormat,
+    searchDialogResultsPage: (state: State): number => state.session.other.searchDialogResultsPage,
   },
   actions: {
-    setAccessToken(token: string): void {
-      this.tokens.access = token;
+    async refreshAccessToken(): Promise<boolean> {
+      try {
+        const successResponse: GetNewAccessTokenResponseInterface =
+          await api.auth.renewAccessToken();
+        this.setAccessToken(
+          successResponse.data.accessToken.token,
+          successResponse.data.accessToken.expiresAtTimestamp,
+        );
+        return true;
+      } catch (e: unknown) {
+        if (e instanceof AxiosError) {
+          // TODO: replace with e.response?.status ???
+          if (e.status !== 401) {
+            // 401 is the "normal" error response if we do not have a previous refresh token
+            console.error('Invalid error http response code', e.status);
+          }
+        } else {
+          console.error('Invalid error response', e);
+        }
+        return false;
+      }
+    },
+    accessTokenExpiresBeforeInterval(interval: number) {
+      if (this.session.accessToken.expiresAtTimestamp) {
+        const currentTimestamp = Math.round(Date.now() / 1000);
+        return this.session.accessToken.expiresAtTimestamp - currentTimestamp <= interval;
+      } else {
+        return false;
+      }
+    },
+    setAccessToken(token: string, expiresAtTimestamp: number): void {
+      this.session.accessToken.token = token;
+      this.session.accessToken.expiresAtTimestamp = expiresAtTimestamp;
     },
     removeAccessToken(): void {
-      this.tokens.access = null;
+      this.session.accessToken.token = null;
+      this.session.accessToken.expiresAtTimestamp = null;
     },
     toggleToolTips(enabled: boolean) {
-      this.other.toolTips = enabled;
-      localStorageShowToolTips.set(this.other.toolTips);
+      this.session.other.toolTips = enabled;
+      localStorageShowToolTips.set(this.session.other.toolTips);
     },
     setOpenUploadDialog(value: boolean) {
-      this.other.alwaysOpenUploadDialog = value;
-      localStorageAlwaysOpenUploadDialog.set(this.other.alwaysOpenUploadDialog);
+      this.session.other.alwaysOpenUploadDialog = value;
+      localStorageAlwaysOpenUploadDialog.set(this.session.other.alwaysOpenUploadDialog);
     },
     setDateFormat(value: string) {
-      this.other.dateFormat = value;
-      localStorageDateFormat.set(this.other.dateFormat);
+      this.session.other.dateFormat = value;
+      localStorageDateFormat.set(this.session.other.dateFormat);
     },
     removeDateFormat() {
-      this.other.dateFormat = 'YYYY/MM/DD';
+      this.session.other.dateFormat = 'YYYY/MM/DD';
       localStorageDateFormat.remove();
     },
     setDateTimeFormat(value: string) {
-      this.other.dateTimeFormat = value;
-      localStorageDateTimeFormat.set(this.other.dateTimeFormat);
+      this.session.other.dateTimeFormat = value;
+      localStorageDateTimeFormat.set(this.session.other.dateTimeFormat);
     },
     removeDateTimeFormat() {
-      this.other.dateTimeFormat = 'YYYY/MM/DD HH:mm:ss';
+      this.session.other.dateTimeFormat = 'YYYY/MM/DD HH:mm:ss';
       localStorageDateTimeFormat.remove();
     },
     setSearchDialogResultsPage(value: number) {
-      this.other.searchDialogResultsPage = value;
-      localStorageSearchDialogResultsPage.set(this.other.searchDialogResultsPage);
+      this.session.other.searchDialogResultsPage = value;
+      localStorageSearchDialogResultsPage.set(this.session.other.searchDialogResultsPage);
     },
   },
 });
