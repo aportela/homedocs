72bfe6ab0086075ae392257d52617f5f1b0fe20a
alex <766f6964+github@gmail.com>
2025-12-15 16:59:01 +0100

    added auto-refresh access token before expiration

--- quasar-frontend/src/boot/router.ts
+++ quasar-frontend/src/boot/router.ts
@@ -1,27 +1,33 @@
 import { defineBoot } from '#q-app/wrappers';
-import { type GetNewAccessTokenResponse as GetNewAccessTokenResponseInterface } from 'src/types/apiResponses';
 import { useSessionStore } from 'src/stores/session';
-import { api } from 'src/composables/api';
-import { AxiosError } from 'axios';
 
 const sessionStore = useSessionStore();
+const accessTokenCheckInterval = 300; // check every 5 min (300 seconds)
 
 export default defineBoot(async ({ router }) => {
+  // at startup, if a refresh token is available, we generate a new access token
   if (!sessionStore.hasAccessToken) {
-    try {
-      const successResponse: GetNewAccessTokenResponseInterface = await api.auth.renewAccessToken();
-      sessionStore.setAccessToken(successResponse.data.accessToken);
-    } catch (e: unknown) {
-      if (e instanceof AxiosError) {
-        if (e.status !== 401) {
-          // 401 is the "normal" error response if we do not have a previous refresh token
-          console.error('Invalid error http response code', e.status);
-        }
-      } else {
-        console.error('Invalid error response', e);
-      }
-    }
+    await sessionStore.refreshAccessToken();
   }
+
+  const refreshAccessTokenIfNeeded = async (): Promise<boolean> => {
+    if (
+      sessionStore.hasAccessToken &&
+      sessionStore.accessTokenExpiresBeforeInterval(accessTokenCheckInterval)
+    ) {
+      return await sessionStore.refreshAccessToken();
+    } else {
+      return false;
+    }
+  };
+
+  // create interval for checking && auto-refresh access token
+  setInterval(() => {
+    refreshAccessTokenIfNeeded()
+      .then(() => {})
+      .catch(() => {});
+  }, accessTokenCheckInterval * 1000);
+
   router.beforeEach((to, _from, next) => {
     if (!to.matched.length) {
       next({ name: 'notFound' });
@@ -34,7 +40,6 @@ export default defineBoot(async ({ router }) => {
         next();
       }
     } else {
-      // TODO: session.hasRefreshToken
       if (to.name === 'login' || to.name === 'register') {
         next();
       } else {
