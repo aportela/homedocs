1c5e3058b946b2fc3d18aecbec0676f5f927b666
alex <766f6964+github@gmail.com>
2025-12-16 21:08:10 +0100

    new quasar web frontend build release

--- phpslim-api/public/assets/axios-BsUr4QQe.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./index-B4QQtCX7.js";import{a as i}from"./index-CG2WSiiR.js";const t=a(({app:o})=>{o.config.globalProperties.$axios=i});export{t as default};
---
+++ phpslim-api/public/assets/dark-rId0wxkv.js
@@ -0,0 +1,1 @@
+import{d as o}from"./index-B-UYWWpq.js";import{u as r}from"./darkMode-DJpuHzA4.js";const a=o(()=>{r()});export{a as default};
