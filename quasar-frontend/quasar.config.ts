9e37d77e3f3fa202d01ab5769758ca195c48020b
alex <766f6964+github@gmail.com>
2026-08-10 14:06:33 +0200

    quasar property filenameBasedRouting disabled

--- quasar-frontend/quasar.config.ts
+++ quasar-frontend/quasar.config.ts
@@ -44,7 +44,7 @@ export default defineConfig((ctx) => {
       },
 
       // https://v2.quasar.dev/quasar-cli-vite/page-routing-with-vue-router#filename-based-routing
-      filenameBasedRouting: true,
+      filenameBasedRouting: false,
 
       vueRouterMode: 'hash', // available values: 'hash', 'history'
       // vueRouterBase,
